6b7e2cfb29baedc498238284eb20d88bdfa8258f
foxit <jun_fang@foxitsoftware.com>
2014-07-07 14:06:56 -0700

    fix a crash issue in _CMapLookupCallback
    BUG=382242
    R=palmer@chromium.org
    
    Review URL: https://codereview.chromium.org/341333004

--- core/src/fpdfapi/fpdf_parser/fpdf_parser_parser.cpp
+++ core/src/fpdfapi/fpdf_parser/fpdf_parser_parser.cpp
@@ -1383,6 +1383,7 @@ CPDF_Object* CPDF_Parser::ParseIndirectObjectAt(CPDF_IndirectObjects* pObjList, 
     m_Syntax.RestorePos(SavedPos);
     if (pObj && !objnum) {
         pObj->m_ObjNum = real_objnum;
+        pObj->m_GenNum = gennum;
     }
     return pObj;
 }
