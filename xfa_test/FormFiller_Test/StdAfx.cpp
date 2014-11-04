9d9f6c347f56b07ec519c93097066a7eb541263d
Bo Xu <bo_xu@foxitsoftware.com>
2014-11-03 18:38:41 -0800

    Remove xfa_test for delivery

--- xfa_test/FormFiller_Test/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	ReaderVC.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
