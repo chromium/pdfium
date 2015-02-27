a310e001c5e7182aa32aca02c1cf930e2f0e0e13
Tom Sepez <tsepez@chromium.org>
2015-02-27 13:03:07 -0800

    Merge to XFA: Pull in gmock for standalone pdfium builds.
    Original Review URL: https://codereview.chromium.org/955513009
    TBR=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/965873002

--- DEPS
+++ DEPS
@@ -2,6 +2,9 @@ deps = {
   "build/gyp":
     "https://chromium.googlesource.com/external/gyp",
 
+  "testing/gmock":
+     "https://chromium.googlesource.com/external/googlemock.git@29763965ab52f24565299976b936d1265cb6a271",
+
   "testing/gtest":
      "https://chromium.googlesource.com/external/googletest.git@8245545b6dc9c4703e6496d1efd19e975ad2b038",
 
---
+++ fpdfsdk/src/fpdfformfill_embeddertest.cpp
@@ -0,0 +1,27 @@
+// Copyright 2015 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+#include "../../fpdfsdk/include/fpdfformfill.h"
+#include "../../testing/embedder_test.h"
+#include "../../testing/embedder_test_mock_delegate.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using testing::_;
+using testing::Return;
+
+class FPDFFormFillEmbeddertest : public EmbedderTest {
+};
+
+TEST_F(FPDFFormFillEmbeddertest, FirstTest) {
+  EmbedderTestMockDelegate mock;
+  EXPECT_CALL(mock, Alert(_, _, _, _)).Times(0);
+  EXPECT_CALL(mock, UnsupportedHandler(_)).Times(0);
+  SetDelegate(&mock);
+
+  EXPECT_TRUE(OpenDocument("testing/resources/hello_world.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  EXPECT_NE(nullptr, page);
+}
