0fff5ef23382f8c13d079dae41b1664999d9c2a7
Tom Sepez <tsepez@chromium.org>
2015-04-17 16:03:41 -0700

    Remove unused FX_NEW definition.
    R=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1096853002

--- core/include/fxcrt/fx_memory.h
+++ core/include/fxcrt/fx_memory.h
@@ -24,7 +24,6 @@ void	FXMEM_DefaultFree(void* pointer, int flags);
 #ifdef __cplusplus
 }
 
-#define FX_NEW new
 #define FX_NEW_VECTOR(Pointer, Class, Count) (Pointer = new Class[Count])
 #define FX_DELETE_VECTOR(Pointer, Class, Count) delete[] Pointer
 
