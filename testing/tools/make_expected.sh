3fc30f0778ff0bfb1c61e7994e6472a34bdc98bd
Tom Sepez <tsepez@chromium.org>
2015-07-30 16:48:28 -0700

    Add simple script to regenerate expected results.
    (I've been doing this by hand for far too long).
    
    R=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1264873005 .

---
+++ testing/tools/make_expected.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Script to generate expected result files.
+#
+set -e
+while (( "$#" )); do
+  INFILE="$1"
+  echo $INFILE | grep -qs ' ' && echo space in filename detected && exit 1
+  out/Debug/pdfium_test --png $INFILE
+  RESULTS="$INFILE.*.png"
+  for RESULT in $RESULTS ; do
+      EXPECTED=`echo -n $RESULT | sed 's/[.]pdf[.]/_expected.pdf./'`
+      mv $RESULT $EXPECTED
+  done
+  shift
+done
