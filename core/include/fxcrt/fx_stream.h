780cee82236d1b3b0f9b01a22424e4b8ec9a6f12
Tom Sepez <tsepez@chromium.org>
2015-07-31 16:00:05 -0700

    Merge to XFA: Kill FX_HFILE
    (cherry picked from commit c8eeed31f217d99a706b0cbf5e4ce0bcc12beb64)
    Review URL: https://codereview.chromium.org/1259123008 .
    
    R=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1256103006 .

--- core/include/fxcrt/fx_stream.h
+++ core/include/fxcrt/fx_stream.h
@@ -15,9 +15,6 @@ FX_BOOL FX_GetNextFile(void* handle, CFX_ByteString& filename, FX_BOOL& bFolder)
 FX_BOOL FX_GetNextFile(void* handle, CFX_WideString& filename, FX_BOOL& bFolder);
 void FX_CloseFolder(void* handle);
 FX_WCHAR FX_GetFolderSeparator();
-typedef struct FX_HFILE_ {
-    void* pData;
-}* FX_HFILE;
 #if _FXM_PLATFORM_ == _FXM_PLATFORM_WINDOWS_
 #define FX_FILESIZE			int32_t
 #else
@@ -43,18 +40,6 @@ typedef struct FX_HFILE_ {
 #define FX_FILEMODE_Write		0
 #define FX_FILEMODE_ReadOnly	1
 #define FX_FILEMODE_Truncate	2
-FX_HFILE	FX_File_Open(const CFX_ByteStringC& fileName, FX_DWORD dwMode);
-FX_HFILE	FX_File_Open(const CFX_WideStringC& fileName, FX_DWORD dwMode);
-void		FX_File_Close(FX_HFILE hFile);
-FX_FILESIZE	FX_File_GetSize(FX_HFILE hFile);
-FX_FILESIZE	FX_File_GetPosition(FX_HFILE hFile);
-FX_FILESIZE	FX_File_SetPosition(FX_HFILE hFile, FX_FILESIZE pos);
-size_t		FX_File_Read(FX_HFILE hFile, void* pBuffer, size_t szBuffer);
-size_t		FX_File_ReadPos(FX_HFILE hFile, void* pBuffer, size_t szBuffer, FX_FILESIZE pos);
-size_t		FX_File_Write(FX_HFILE hFile, const void* pBuffer, size_t szBuffer);
-size_t		FX_File_WritePos(FX_HFILE hFile, const void* pBuffer, size_t szBuffer, FX_FILESIZE pos);
-FX_BOOL		FX_File_Flush(FX_HFILE hFile);
-FX_BOOL		FX_File_Truncate(FX_HFILE hFile, FX_FILESIZE szFile);
 FX_BOOL		FX_File_Exist(const CFX_ByteStringC& fileName);
 FX_BOOL		FX_File_Exist(const CFX_WideStringC& fileName);
 FX_BOOL		FX_File_Delete(const CFX_ByteStringC& fileName);
