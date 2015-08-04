7f6b084fbdde785240f2509ae3617a17937f97ba
Lei Zhang <thestig@chromium.org>
2015-08-04 14:23:05 -0700

    Add a license header for testing/tools/make_expected.sh.
    R=tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/1259963005 .

--- testing/tools/make_expected.sh
+++ testing/tools/make_expected.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
+# Copyright 2015 PDFium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
 # Script to generate expected result files.
 #
 set -e
