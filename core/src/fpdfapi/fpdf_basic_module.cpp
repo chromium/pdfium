f372567fdb84df1c8b2cd3735575d6d9881e7871
Tom Sepez <tsepez@chromium.org>
2015-08-14 12:39:55 -0700

    CPDF_ModuleMgr::m_SecurityHandlerMap only used by dead code.
    R=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1289693008 .

--- core/src/fpdfapi/fpdf_basic_module.cpp
+++ core/src/fpdfapi/fpdf_basic_module.cpp
@@ -36,34 +36,6 @@ CPDF_ModuleMgr::CPDF_ModuleMgr() : m_pCodecModule(nullptr) {}
 
 CPDF_ModuleMgr::~CPDF_ModuleMgr() {}
 
-void CPDF_ModuleMgr::SetDownloadCallback(
-    FX_BOOL (*callback)(const FX_CHAR* module_name)) {
-  m_pDownloadCallback = callback;
-}
-FX_BOOL CPDF_ModuleMgr::DownloadModule(const FX_CHAR* module_name) {
-  if (m_pDownloadCallback == NULL) {
-    return FALSE;
-  }
-  return m_pDownloadCallback(module_name);
-}
-void CPDF_ModuleMgr::NotifyModuleAvailable(const FX_CHAR* module_name) {
-  if (FXSYS_strcmp(module_name, kAddinNameCJK) == 0) {
-    m_pPageModule->NotifyCJKAvailable();
-  }
-}
-void CPDF_ModuleMgr::RegisterSecurityHandler(
-    const FX_CHAR* filter,
-    CPDF_SecurityHandler* (*CreateHandler)(void* param),
-    void* param) {
-  if (CreateHandler == NULL) {
-    m_SecurityHandlerMap.RemoveKey(filter);
-  } else {
-    m_SecurityHandlerMap.SetAt(filter, (void*)CreateHandler);
-  }
-  if (param) {
-    m_SecurityHandlerMap.SetAt(FX_BSTRC("_param_") + filter, param);
-  }
-}
 void CPDF_ModuleMgr::SetPrivateData(void* module_id,
                                     void* pData,
                                     PD_CALLBACK_FREEDATA callback) {
@@ -71,19 +43,6 @@ void CPDF_ModuleMgr::SetPrivateData(void* module_id,
 }
 void* CPDF_ModuleMgr::GetPrivateData(void* module_id) {
   return m_privateData.GetPrivateData(module_id);
-}
-CPDF_SecurityHandler* CPDF_ModuleMgr::CreateSecurityHandler(
-    const FX_CHAR* filter) {
-  CPDF_SecurityHandler* (*CreateHandler)(void*) = NULL;
-  if (!m_SecurityHandlerMap.Lookup(filter, (void*&)CreateHandler)) {
-    return NULL;
-  }
-  if (CreateHandler == NULL) {
-    return NULL;
-  }
-  void* param = NULL;
-  m_SecurityHandlerMap.Lookup(FX_BSTRC("_param_") + filter, param);
-  return CreateHandler(param);
 }
 ICodec_FaxModule* CPDF_ModuleMgr::GetFaxModule() {
   return m_pCodecModule ? m_pCodecModule->GetFaxModule() : NULL;
