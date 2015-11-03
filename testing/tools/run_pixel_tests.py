dccfe5736818f94f7e084105e6d2ab6d5d3a9941
Dan Sinclair <dsinclair@chromium.org>
2015-11-03 11:30:00 -0500

    Merge to XFA: Revert "One test runner to rule them all."
    This reverts commit a1215ba51a235fb7abcb995f0e768ea0176d9275.
    
    Revert "Return result of the test runner."
    
    This reverts commit d1579c9b92b7f9c1d9e0fac1ecb8e3cb23875fca.
    
    Reverting the new test_runner code as there appears to be something string
    happening on the non-Linux boxes on XFA.
    
    It looks like it ran the font_size test twice, once with the .in and once with
    the .pdf. The .in was suppressed, the .pdf faild and wasn't suppressed.
    
    Rendering PDF file /Volumes/data/b/build/slave/mac_xfa/build/pdfium/out/Debug/gen/pdfium/testing/pixel/font_size.pdf.
    Rendered 1 pages.
    Skipped 0 bad pages.
    Checking /Volumes/data/b/build/slave/mac_xfa/build/pdfium/out/Debug/gen/pdfium/testing/pixel/font_size.pdf.0.png
    FAILURE: font_size.in; Command '['/Volumes/data/b/build/slave/mac_xfa/build/pdfium/out/Debug/pdfium_diff', '/Volumes/data/b/build/slave/mac_xfa/build/pdfium/testing/resources/pixel/font_size_expected.pdf.0.png', '/Volumes/data/b/build/slave/mac_xfa/build/pdfium/out/Debug/gen/pdfium/testing/pixel/font_size.pdf.0.png']' returned non-zero exit status 1
    font_size.in is suppressed, found in SUPPRESSIONS_mac file
    
    Rendering PDF file /Volumes/data/b/build/slave/mac_xfa/build/pdfium/out/Debug/gen/pdfium/testing/pixel/font_size.pdf.
    Rendered 1 pages.
    Skipped 0 bad pages.
    Checking /Volumes/data/b/build/slave/mac_xfa/build/pdfium/out/Debug/gen/pdfium/testing/pixel/font_size.pdf.0.png
    FAILURE: font_size.pdf; Command '['/Volumes/data/b/build/slave/mac_xfa/build/pdfium/out/Debug/pdfium_diff', '/Volumes/data/b/build/slave/mac_xfa/build/pdfium/testing/resources/pixel/font_size_expected.pdf.0.png', '/Volumes/data/b/build/slave/mac_xfa/build/pdfium/out/Debug/gen/pdfium/testing/pixel/font_size.pdf.0.png']' returned non-zero exit status 1
    
    TBR=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1410743008 .
    
    (cherry picked from commit e067f7bab4317704de27917c1befa8f57a0e78d7)
    
    Review URL: https://codereview.chromium.org/1421133005 .

--- testing/tools/run_pixel_tests.py
+++ testing/tools/run_pixel_tests.py
@@ -3,13 +3,96 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import optparse
+import os
+import re
+import subprocess
 import sys
 
-import test_runner
+import common
+import pngdiffer
+import suppressor
+
+# Nomenclature:
+#   x_root - "x"
+#   x_filename - "x.ext"
+#   x_path - "path/to/a/b/c/x.ext"
+#   c_dir - "path/to/a/b/c"
+
+def generate_and_test(input_filename, source_dir, working_dir,
+                      fixup_path, pdfium_test_path, image_differ):
+  input_root, _ = os.path.splitext(input_filename)
+  input_path = os.path.join(source_dir, input_root + '.in')
+  pdf_path = os.path.join(working_dir, input_root + '.pdf')
+
+  # Remove any existing generated images from previous runs.
+  actual_images = image_differ.GetActualFiles(
+      input_filename, source_dir, working_dir)
+  for image in actual_images:
+    if os.path.exists(image):
+      os.remove(image)
+
+  try:
+    sys.stdout.flush()
+    subprocess.check_call(
+        [sys.executable, fixup_path, '--output-dir=' + working_dir, input_path])
+    subprocess.check_call([pdfium_test_path, '--png', pdf_path])
+  except subprocess.CalledProcessError as e:
+    print "FAILURE: " + input_filename + "; " + str(e)
+    return False
+  if image_differ.HasDifferences(input_filename, source_dir, working_dir):
+    print "FAILURE: " + input_filename
+    return False
+  return True
+
 
 def main():
-  runner = test_runner.TestRunner('pixel')
-  return runner.Run()
+  parser = optparse.OptionParser()
+  parser.add_option('--build-dir', default=os.path.join('out', 'Debug'),
+                    help='relative path from the base source directory')
+  options, args = parser.parse_args()
+  finder = common.DirectoryFinder(options.build_dir)
+  fixup_path = finder.ScriptPath('fixup_pdf_template.py')
+  source_dir = finder.TestingDir(os.path.join('resources', 'pixel'))
+  pdfium_test_path = finder.ExecutablePath('pdfium_test')
+  if not os.path.exists(pdfium_test_path):
+    print "FAILURE: Can't find test executable '%s'" % pdfium_test_path
+    print "Use --build-dir to specify its location."
+    return 1
+  working_dir = finder.WorkingDir(os.path.join('testing', 'pixel'))
+  if not os.path.exists(working_dir):
+    os.makedirs(working_dir)
+
+  test_suppressor = suppressor.Suppressor(finder)
+  image_differ = pngdiffer.PNGDiffer(finder)
+
+  input_files = []
+  if len(args):
+    for file_name in args:
+      input_files.append(file_name.replace(".pdf", ".in"))
+  else:
+    input_files = os.listdir(source_dir)
+
+  failures = []
+  input_file_re = re.compile('^[a-zA-Z0-9_.]+[.]in$')
+  for input_filename in input_files:
+    if input_file_re.match(input_filename):
+      input_path = os.path.join(source_dir, input_filename)
+      if os.path.isfile(input_path):
+        if test_suppressor.IsSuppressed(input_filename):
+          continue
+        if not generate_and_test(input_filename, source_dir, working_dir,
+                                 fixup_path, pdfium_test_path, image_differ):
+          failures.append(input_path)
+
+  if failures:
+    failures.sort()
+    print '\n\nSummary of Failures:'
+    for failure in failures:
+      print failure
+    return 1
+  return 0
+
 
 if __name__ == '__main__':
   sys.exit(main())
