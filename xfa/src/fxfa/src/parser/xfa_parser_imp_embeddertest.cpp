3d632f023a2e1c4a29512fca3dfe723f4f474c59
Wei Li <weili@chromium.org>
2015-11-09 12:41:44 -0800

    fix an embedder test in XFA branch.
    The merge https://codereview.chromium.org/1411403012/ broken build since there are one more embedder test in XFA branch. Fix this test in this CL.
    
    R=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1415163008 .

--- xfa/src/fxfa/src/parser/xfa_parser_imp_embeddertest.cpp
+++ xfa/src/fxfa/src/parser/xfa_parser_imp_embeddertest.cpp
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "../../../../../testing/embedder_test.h"
+#include "testing/embedder_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 class XFAParserImpEmbeddertest : public EmbedderTest {};
 
 TEST_F(XFAParserImpEmbeddertest, Bug_216) {
-    EXPECT_TRUE(OpenDocument("testing/resources/bug_216.pdf"));
-    FPDF_PAGE page = LoadPage(0);
-    EXPECT_NE(nullptr, page);
-    UnloadPage(page);
+  EXPECT_TRUE(OpenDocument("bug_216.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  EXPECT_NE(nullptr, page);
+  UnloadPage(page);
 }
