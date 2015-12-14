6e698d13cde3391eaf745e90649a6bec1c72b0b7
Lei Zhang <thestig@chromium.org>
2015-12-14 15:15:19 -0800

    Merge to XFA: Fix a memory leak in FPDFPageFuncEmbeddertest.
    TBR=dsinclair@chromium.org
    
    Review URL: https://codereview.chromium.org/1514283002 .
    
    (cherry picked from commit 1407c979a3ba8599546468d461f5403f317bddd3)
    
    Review URL: https://codereview.chromium.org/1525013002 .

--- core/src/fpdfapi/fpdf_render/fpdf_render_pattern_embeddertest.cpp
+++ core/src/fpdfapi/fpdf_render/fpdf_render_pattern_embeddertest.cpp
@@ -11,6 +11,7 @@ TEST_F(FPDFRenderPatternEmbeddertest, LoadError_547706) {
   // Test shading where object is a dictionary instead of a stream.
   EXPECT_TRUE(OpenDocument("bug_547706.pdf"));
   FPDF_PAGE page = LoadPage(0);
-  RenderPage(page);
+  FPDF_BITMAP bitmap = RenderPage(page);
+  FPDFBitmap_Destroy(bitmap);
   UnloadPage(page);
 }
