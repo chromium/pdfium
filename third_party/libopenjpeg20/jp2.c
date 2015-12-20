b8574446d346bf1a026a208550e8da35501f97f0
Jun Fang <jun_fang@foxitsoftware.com>
2015-12-21 00:11:28 +0800

    Fix wrong rendering on greyscale images with index colorspace
    BUG=514514
    R=tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/1536223002 .

--- third_party/libopenjpeg20/jp2.c
+++ third_party/libopenjpeg20/jp2.c
@@ -1194,7 +1194,7 @@ static OPJ_BOOL opj_jp2_read_cmap(	opj_jp2_t * jp2,
 
 
 	for(i = 0; i < nr_channels; ++i) {
-		opj_read_bytes(p_cmap_header_data, &l_value, 2);			/* CMP^i */
+		opj_read_bytes_BE(p_cmap_header_data, &l_value, 2);			/* CMP^i */
 		p_cmap_header_data +=2;
 		cmap[i].cmp = (OPJ_UINT16) l_value;
 
