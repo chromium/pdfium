1ac47ebfc2d106e25c3e13062cae2132daa49348
Lei Zhang <thestig@chromium.org>
2015-12-21 11:04:44 -0800

    Merge to XFA: Fix JS seconds since epoch to date conversions.
    BUG=515137,564736
    TBR=ochang@chromium.org, tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/1533233002 .
    Review URL: https://codereview.chromium.org/1544493003 .
    
    (cherry picked from commit b426e3edde040089b70d1a223c83b90957aa571d)
    (cherry picked from commit bd35d484a4027775f19ff93e9143e0b270133d42)
    
    Review URL: https://codereview.chromium.org/1546443002 .

--- fpdfsdk/src/jsapi/fxjs_v8_embeddertest.cpp
+++ fpdfsdk/src/jsapi/fxjs_v8_embeddertest.cpp
@@ -2,11 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "core/include/fpdfapi/fpdf_parser.h"
-#include "fpdfsdk/include/jsapi/fxjs_v8.h"
-#include "testing/embedder_test.h"
+#include "testing/js_embedder_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/base/nonstd_unique_ptr.h"
 
 namespace {
 
@@ -14,51 +11,9 @@ const wchar_t kScript[] = L"fred = 7";
 
 }  // namespace
 
-class FXJSV8Embeddertest : public EmbedderTest {
- public:
-  FXJSV8Embeddertest()
-      : m_pArrayBufferAllocator(new FXJS_ArrayBufferAllocator) {
-    v8::Isolate::CreateParams params;
-    params.array_buffer_allocator = m_pArrayBufferAllocator.get();
-    m_pIsolate = v8::Isolate::New(params);
-  }
+class FXJSV8EmbedderTest : public JSEmbedderTest {};
 
-  ~FXJSV8Embeddertest() override { m_pIsolate->Dispose(); }
-
-  void SetUp() override {
-    EmbedderTest::SetExternalIsolate(m_pIsolate);
-    EmbedderTest::SetUp();
-
-    v8::Isolate::Scope isolate_scope(m_pIsolate);
-#ifdef PDF_ENABLE_XFA
-    v8::Locker locker(m_pIsolate);
-#endif  // PDF_ENABLE_XFA
-    v8::HandleScope handle_scope(m_pIsolate);
-    FXJS_PerIsolateData::SetUp(m_pIsolate);
-    FXJS_InitializeRuntime(m_pIsolate, nullptr, &m_pPersistentContext,
-                           &m_StaticObjects);
-  }
-
-  void TearDown() override {
-    FXJS_ReleaseRuntime(m_pIsolate, &m_pPersistentContext, &m_StaticObjects);
-    m_pPersistentContext.Reset();
-    FXJS_Release();
-    EmbedderTest::TearDown();
-  }
-
-  v8::Isolate* isolate() { return m_pIsolate; }
-  v8::Local<v8::Context> GetV8Context() {
-    return m_pPersistentContext.Get(m_pIsolate);
-  }
-
- private:
-  nonstd::unique_ptr<FXJS_ArrayBufferAllocator> m_pArrayBufferAllocator;
-  v8::Isolate* m_pIsolate;
-  v8::Global<v8::Context> m_pPersistentContext;
-  std::vector<v8::Global<v8::Object>*> m_StaticObjects;
-};
-
-TEST_F(FXJSV8Embeddertest, Getters) {
+TEST_F(FXJSV8EmbedderTest, Getters) {
   v8::Isolate::Scope isolate_scope(isolate());
 #ifdef PDF_ENABLE_XFA
   v8::Locker locker(isolate());
