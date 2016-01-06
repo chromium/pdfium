dcac57bc8b64fdc870d79d11a498ae7021cf8ae7
Lei Zhang <thestig@chromium.org>
2016-01-06 14:42:49 -0800

    Invalidate IPWL_FocusHandler and IPWL_Provider on destruction.
    BUG=572871
    R=tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/1564773003 .

--- fpdfsdk/src/formfiller/FFL_TextField.cpp
+++ fpdfsdk/src/formfiller/FFL_TextField.cpp
@@ -16,6 +16,8 @@ CFFL_TextField::CFFL_TextField(CPDFDoc_Environment* pApp, CPDFSDK_Annot* pAnnot)
 }
 
 CFFL_TextField::~CFFL_TextField() {
+  for (const auto& it : m_Maps)
+    it.second->InvalidateFocusHandler(this);
   delete m_pFontMap;
 }
 
