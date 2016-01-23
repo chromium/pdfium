chromium/pdfium

commit 90853cb1dfd1bf3803ec21cfae3e93948137be61

Fix infinite loops caused by calling circular indirect objects
There are multiple functions in CPDF_Object class which can cause infinite loop due to recursively calling circular indirect objects. Fix them by deference indirect object first.

BUG=pdfium:355
R=jun_fang@foxitsoftware.com, thestig@chromium.org

Review URL: https://codereview.chromium.org/1585533002 .

core/src/fpdfapi/fpdf_parser/fpdf_parser_objects.cpp
@@ -18,9 +18,6 @@ const FX_DWORD kBlockSize = 1024;
 
 }  // namespace
 
-// static
-int CPDF_Object::s_nCurRefDepth = 0;
-
 void CPDF_Object::Release() {
   if (m_ObjNum) {
     return;
@@ -48,65 +45,55 @@ void CPDF_Object::Destroy() {
       delete this;
   }
 }
-CFX_ByteString CPDF_Object::GetString() const {
-  switch (m_Type) {
-    case PDFOBJ_BOOLEAN:
-      return AsBoolean()->m_bValue ? "true" : "false";
-    case PDFOBJ_NUMBER:
-      return AsNumber()->GetString();
-    case PDFOBJ_STRING:
-      return AsString()->m_String;
-    case PDFOBJ_NAME:
-      return AsName()->m_Name;
-    case PDFOBJ_REFERENCE: {
-      const CPDF_Reference* pRef = AsReference();
-      if (!pRef->m_pObjList)
-        break;
 
-      CPDF_Object* pObj =
-          pRef->m_pObjList->GetIndirectObject(pRef->GetRefObjNum(), nullptr);
-      return pObj ? pObj->GetString() : CFX_ByteString();
+const CPDF_Object* const CPDF_Object::GetBasicObject() const {
+  const CPDF_Reference* pRef = AsReference();
+  if (!pRef) {
+    // This is not an indirect reference.
+    return this;
+  }
+  if (!pRef->m_pObjList)
+    return nullptr;
+  return pRef->m_pObjList->GetIndirectObject(pRef->GetRefObjNum(), nullptr);
+}
+
+CFX_ByteString CPDF_Object::GetString() const {
+  const CPDF_Object* obj = GetBasicObject();
+  if (obj) {
+    switch (obj->GetType()) {
+      case PDFOBJ_BOOLEAN:
+        return obj->AsBoolean()->GetString();
+      case PDFOBJ_NUMBER:
+        return obj->AsNumber()->GetString();
+      case PDFOBJ_STRING:
+        return obj->AsString()->GetString();
+      case PDFOBJ_NAME:
+        return obj->AsName()->GetString();
     }
   }
   return CFX_ByteString();
 }
-CFX_ByteStringC CPDF_Object::GetConstString() const {
-  switch (m_Type) {
-    case PDFOBJ_STRING: {
-      CFX_ByteString str = AsString()->m_String;
-      return CFX_ByteStringC((const uint8_t*)str, str.GetLength());
-    }
-    case PDFOBJ_NAME: {
-      CFX_ByteString name = AsName()->m_Name;
-      return CFX_ByteStringC((const uint8_t*)name, name.GetLength());
-    }
-    case PDFOBJ_REFERENCE: {
-      const CPDF_Reference* pRef = AsReference();
-      if (!pRef->m_pObjList)
-        break;
 
-      CPDF_Object* pObj =
-          pRef->m_pObjList->GetIndirectObject(pRef->GetRefObjNum(), nullptr);
-      return pObj ? pObj->GetConstString() : CFX_ByteStringC();
+CFX_ByteStringC CPDF_Object::GetConstString() const {
+  const CPDF_Object* obj = GetBasicObject();
+  if (obj) {
+    FX_DWORD type = obj->GetType();
+    if (type == PDFOBJ_STRING) {
+      CFX_ByteString str = obj->AsString()->GetString();
+      return CFX_ByteStringC(str);
+    }
+    if (type == PDFOBJ_NAME) {
+      CFX_ByteString name = obj->AsName()->GetString();
+      return CFX_ByteStringC(name);
     }
   }
   return CFX_ByteStringC();
 }
 
 FX_FLOAT CPDF_Object::GetNumber() const {
-  switch (m_Type) {
-    case PDFOBJ_NUMBER:
-      return AsNumber()->GetNumber();
-    case PDFOBJ_REFERENCE: {
-      const CPDF_Reference* pRef = AsReference();
-      if (!pRef->m_pObjList)
-        break;
-
-      CPDF_Object* pObj =
-          pRef->m_pObjList->GetIndirectObject(pRef->GetRefObjNum(), nullptr);
-      return pObj ? pObj->GetNumber() : 0;
-    }
-  }
+  const CPDF_Object* obj = GetBasicObject();
+  if (obj && obj->GetType() == PDFOBJ_NUMBER)
+    return AsNumber()->GetNumber();
   return 0;
 }
 
@@ -115,52 +102,30 @@ FX_FLOAT CPDF_Object::GetNumber16() const {
 }
 
 int CPDF_Object::GetInteger() const {
-  CFX_AutoRestorer<int> restorer(&s_nCurRefDepth);
-  if (++s_nCurRefDepth > kObjectRefMaxDepth)
-    return 0;
-
-  switch (m_Type) {
-    case PDFOBJ_BOOLEAN:
-      return AsBoolean()->m_bValue;
-    case PDFOBJ_NUMBER:
-      return AsNumber()->GetInteger();
-    case PDFOBJ_REFERENCE: {
-      const CPDF_Reference* pRef = AsReference();
-      PARSE_CONTEXT context;
-      FXSYS_memset(&context, 0, sizeof(PARSE_CONTEXT));
-      if (!pRef->m_pObjList)
-        return 0;
-
-      CPDF_Object* pObj =
-          pRef->m_pObjList->GetIndirectObject(pRef->GetRefObjNum(), &context);
-      return pObj ? pObj->GetInteger() : 0;
-    }
+  const CPDF_Object* obj = GetBasicObject();
+  if (obj) {
+    FX_DWORD type = obj->GetType();
+    if (type == PDFOBJ_BOOLEAN)
+      return obj->AsBoolean()->GetValue();
+    if (type == PDFOBJ_NUMBER)
+      return obj->AsNumber()->GetInteger();
   }
   return 0;
 }
 
 CPDF_Dictionary* CPDF_Object::GetDict() const {
-  switch (m_Type) {
-    case PDFOBJ_DICTIONARY:
+  const CPDF_Object* obj = GetBasicObject();
+  if (obj) {
+    FX_DWORD type = obj->GetType();
+    if (type == PDFOBJ_DICTIONARY) {
       // The method should be made non-const if we want to not be const.
       // See bug #234.
       return const_cast<CPDF_Dictionary*>(AsDictionary());
-    case PDFOBJ_STREAM:
-      return AsStream()->GetDict();
-    case PDFOBJ_REFERENCE: {
-      const CPDF_Reference* pRef = AsReference();
-      CPDF_IndirectObjectHolder* pIndirect = pRef->GetObjList();
-      if (!pIndirect)
-        return nullptr;
-      CPDF_Object* pObj =
-          pIndirect->GetIndirectObject(pRef->GetRefObjNum(), nullptr);
-      if (!pObj || (pObj == this))
-        return nullptr;
-      return pObj->GetDict();
     }
-    default:
-      return nullptr;
+    if (type == PDFOBJ_STREAM)
+      return AsStream()->GetDict();
   }
+  return nullptr;
 }
 
 CPDF_Array* CPDF_Object::GetArray() const {

core/include/fpdfapi/fpdf_objects.h
@@ -45,43 +45,30 @@ struct PARSE_CONTEXT;
 class CPDF_Object {
  public:
   int GetType() const { return m_Type; }
-
   FX_DWORD GetObjNum() const { return m_ObjNum; }
-
   FX_DWORD GetGenNum() const { return m_GenNum; }
 
   FX_BOOL IsIdentical(CPDF_Object* pObj) const;
-
   CPDF_Object* Clone(FX_BOOL bDirect = FALSE) const;
-
   CPDF_Object* CloneRef(CPDF_IndirectObjectHolder* pObjs) const;
 
   CPDF_Object* GetDirect() const;
+  int GetDirectType() const;
+  FX_BOOL IsModified() const { return FALSE; }
 
   void Release();
 
   CFX_ByteString GetString() const;
-
   CFX_ByteStringC GetConstString() const;
-
   CFX_WideString GetUnicodeText(CFX_CharMap* pCharMap = NULL) const;
   FX_FLOAT GetNumber() const;
-
   FX_FLOAT GetNumber16() const;
-
   int GetInteger() const;
-
   CPDF_Dictionary* GetDict() const;
-
   CPDF_Array* GetArray() const;
 
   void SetString(const CFX_ByteString& str);
-
   void SetUnicodeText(const FX_WCHAR* pUnicodes, int len = -1);
-
-  int GetDirectType() const;
-
-  FX_BOOL IsModified() const { return FALSE; }
 
   bool IsArray() const { return m_Type == PDFOBJ_ARRAY; }
   bool IsBoolean() const { return m_Type == PDFOBJ_BOOLEAN; }
@@ -94,25 +81,18 @@ class CPDF_Object {
 
   CPDF_Array* AsArray();
   const CPDF_Array* AsArray() const;
-
   CPDF_Boolean* AsBoolean();
   const CPDF_Boolean* AsBoolean() const;
-
   CPDF_Dictionary* AsDictionary();
   const CPDF_Dictionary* AsDictionary() const;
-
   CPDF_Name* AsName();
   const CPDF_Name* AsName() const;
-
   CPDF_Number* AsNumber();
   const CPDF_Number* AsNumber() const;
-
   CPDF_Reference* AsReference();
   const CPDF_Reference* AsReference() const;
-
   CPDF_Stream* AsStream();
   const CPDF_Stream* AsStream() const;
-
   CPDF_String* AsString();
   const CPDF_String* AsString() const;
 
@@ -122,8 +102,8 @@ class CPDF_Object {
   ~CPDF_Object() {}
   void Destroy();
 
-  static const int kObjectRefMaxDepth = 128;
-  static int s_nCurRefDepth;
+  const CPDF_Object* const GetBasicObject() const;
+
   FX_DWORD m_Type;
   FX_DWORD m_ObjNum;
   FX_DWORD m_GenNum;
@@ -146,6 +126,9 @@ class CPDF_Boolean : public CPDF_Object {
   FX_BOOL Identical(CPDF_Boolean* pOther) const {
     return m_bValue == pOther->m_bValue;
   }
+
+  CFX_ByteString GetString() const { return m_bValue ? "true" : "false"; }
+  FX_BOOL GetValue() const { return m_bValue; }
 
  protected:
   FX_BOOL m_bValue;
@@ -216,7 +199,7 @@ class CPDF_String : public CPDF_Object {
 
   explicit CPDF_String(const CFX_WideString& str);
 
-  CFX_ByteString& GetString() { return m_String; }
+  CFX_ByteString GetString() const { return m_String; }
 
   FX_BOOL Identical(CPDF_String* pOther) const {
     return m_String == pOther->m_String;
@@ -246,7 +229,7 @@ class CPDF_Name : public CPDF_Object {
   explicit CPDF_Name(const FX_CHAR* str)
       : CPDF_Object(PDFOBJ_NAME), m_Name(str) {}
 
-  CFX_ByteString& GetString() { return m_Name; }
+  CFX_ByteString GetString() const { return m_Name; }
 
   FX_BOOL Identical(CPDF_Name* pOther) const {
     return m_Name == pOther->m_Name;

fpdfsdk/src/fpdfview_embeddertest.cpp
@@ -235,6 +235,11 @@ TEST_F(FPDFViewEmbeddertest, Hang_344) {
   EXPECT_FALSE(OpenDocument("bug_344.pdf"));
 }
 
+// The test should pass when there is no infinite recursion in
+// CPDF_SyntaxParser::GetString().
+TEST_F(FPDFViewEmbeddertest, Hang_355) {
+  EXPECT_FALSE(OpenDocument("bug_355.pdf"));
+}
 // The test should pass even when the file has circular references to pages.
 TEST_F(FPDFViewEmbeddertest, Hang_360) {
   EXPECT_FALSE(OpenDocument("bug_360.pdf"));

testing/resources/bug_355.pdf
@@ -1,0 +1,15 @@
+%P%PDF-1%PDF-1.4
+%«è¢
+6 0 obj%<</Lengt 6 0 R/FiltDF-1.2
+%‚„œ”
+7 0 obj <<
+! /Type /Fojt
+trailer
+<<////OC3
+endobj
+2 0 obj <<
+  /ResouÁces <<
+  /FT 6 1 R
+>>
+%PDF-%PDF-2.6%‚„œ”
+1 /(0 er '/obFl

commit 7671d304ca4d3aec658c0ac8d5eea4c64e23b144

Fixed object references in CPDF_Object
This is a regression from commit 90853cb.

BUG=pdfium:365
R=tsepez@chromium.org

Review URL: https://codereview.chromium.org/1617043004 .

BUILD.gn
@@ -722,6 +722,7 @@ test("pdfium_unittests") {
     "core/src/fpdfapi/fpdf_font/fpdf_font_unittest.cpp",
     "core/src/fpdfapi/fpdf_page/fpdf_page_parser_old_unittest.cpp",
     "core/src/fpdfapi/fpdf_parser/fpdf_parser_decode_unittest.cpp",
+    "core/src/fpdfapi/fpdf_parser/fpdf_parser_objects_unittest.cpp",
     "core/src/fpdfapi/fpdf_parser/fpdf_parser_parser_unittest.cpp",
     "core/src/fpdftext/fpdf_text_int_unittest.cpp",
     "core/src/fxcodec/codec/fx_codec_jpx_unittest.cpp",

core/src/fpdfapi/fpdf_parser/fpdf_parser_objects.cpp
@@ -93,7 +93,7 @@ CFX_ByteStringC CPDF_Object::GetConstString() const {
 FX_FLOAT CPDF_Object::GetNumber() const {
   const CPDF_Object* obj = GetBasicObject();
   if (obj && obj->GetType() == PDFOBJ_NUMBER)
-    return AsNumber()->GetNumber();
+    return obj->AsNumber()->GetNumber();
   return 0;
 }
 
@@ -120,10 +120,10 @@ CPDF_Dictionary* CPDF_Object::GetDict() const {
     if (type == PDFOBJ_DICTIONARY) {
       // The method should be made non-const if we want to not be const.
       // See bug #234.
-      return const_cast<CPDF_Dictionary*>(AsDictionary());
+      return const_cast<CPDF_Dictionary*>(obj->AsDictionary());
     }
     if (type == PDFOBJ_STREAM)
-      return AsStream()->GetDict();
+      return obj->AsStream()->GetDict();
   }
   return nullptr;
 }

pdfium.gyp
@@ -713,6 +713,7 @@
         'core/src/fpdfapi/fpdf_font/fpdf_font_unittest.cpp',
         'core/src/fpdfapi/fpdf_page/fpdf_page_parser_old_unittest.cpp',
         'core/src/fpdfapi/fpdf_parser/fpdf_parser_decode_unittest.cpp',
+        'core/src/fpdfapi/fpdf_parser/fpdf_parser_objects_unittest.cpp',
         'core/src/fpdfapi/fpdf_parser/fpdf_parser_parser_unittest.cpp',
         'core/src/fpdftext/fpdf_text_int_unittest.cpp',
         'core/src/fxcodec/codec/fx_codec_jpx_unittest.cpp',

core/src/fpdfapi/fpdf_parser/fpdf_parser_objects_unittest.cpp
@@ -1,0 +1,160 @@
+// Copyright 2016 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/include/fpdfapi/fpdf_objects.h"
+
+#include <memory>
+#include <vector>
+
+#include "core/include/fxcrt/fx_basic.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class PDFObjectsTest : public testing::Test {
+ public:
+  void SetUp() override {
+    // Initialize different kinds of objects.
+    // Boolean objects.
+    CPDF_Boolean* boolean_false_obj = new CPDF_Boolean(false);
+    CPDF_Boolean* boolean_true_obj = new CPDF_Boolean(true);
+    // Number objects.
+    CPDF_Number* number_int_obj = new CPDF_Number(1245);
+    CPDF_Number* number_float_obj = new CPDF_Number(9.00345f);
+    // String objects.
+    CPDF_String* str_reg_obj = new CPDF_String(L"A simple test");
+    CPDF_String* str_spec_obj = new CPDF_String(L"\t\n");
+    // Name object.
+    CPDF_Name* name_obj = new CPDF_Name("space");
+    // Array object.
+    CPDF_Array* array_obj = new CPDF_Array;
+    array_obj->InsertAt(0, new CPDF_Number(8902));
+    array_obj->InsertAt(1, new CPDF_Name("address"));
+    // Dictionary object.
+    m_DictObj = new CPDF_Dictionary;
+    m_DictObj->SetAt("bool", new CPDF_Boolean(false));
+    m_DictObj->SetAt("num", new CPDF_Number(0.23f));
+    // Stream object.
+    const char content[] = "abcdefghijklmnopqrstuvwxyz";
+    size_t buf_len = FX_ArraySize(content);
+    uint8_t* buf = reinterpret_cast<uint8_t*>(malloc(buf_len));
+    memcpy(buf, content, buf_len);
+    m_StreamDictObj = new CPDF_Dictionary;
+    m_StreamDictObj->SetAt("key1", new CPDF_String(L" test dict"));
+    m_StreamDictObj->SetAt("key2", new CPDF_Number(-1));
+    CPDF_Stream* stream_obj = new CPDF_Stream(buf, buf_len, m_StreamDictObj);
+    // Null Object.
+    CPDF_Null* null_obj = new CPDF_Null;
+    // All direct objects.
+    CPDF_Object* objs[] = {boolean_false_obj, boolean_true_obj, number_int_obj,
+                           number_float_obj,  str_reg_obj,      str_spec_obj,
+                           name_obj,          array_obj,        m_DictObj,
+                           stream_obj,        null_obj};
+    for (int i = 0; i < FX_ArraySize(objs); ++i)
+      m_DirectObjs.emplace_back(objs[i]);
+
+    // Indirect references to indirect objects.
+    m_ObjHolder.reset(new CPDF_IndirectObjectHolder(nullptr));
+    CPDF_Object* referred_objs[] = {
+        boolean_true_obj, number_int_obj, str_spec_obj, name_obj,
+        array_obj,        m_DictObj,      stream_obj};
+    for (int i = 0; i < FX_ArraySize(referred_objs); ++i) {
+      m_ObjHolder->AddIndirectObject(referred_objs[i]);
+      m_RefObjs.emplace_back(
+          new CPDF_Reference(m_ObjHolder.get(), referred_objs[i]->GetObjNum()));
+    }
+  }
+
+ protected:
+  using ScopedObj = std::unique_ptr<CPDF_Object, ReleaseDeleter<CPDF_Object>>;
+
+  // m_ObjHolder needs to be declared first and destructed last since it also
+  // refers to some objects in m_DirectObjs.
+  std::unique_ptr<CPDF_IndirectObjectHolder> m_ObjHolder;
+  std::vector<ScopedObj> m_DirectObjs;
+  std::vector<ScopedObj> m_RefObjs;
+  CPDF_Dictionary* m_DictObj;
+  CPDF_Dictionary* m_StreamDictObj;
+};
+
+TEST_F(PDFObjectsTest, GetString) {
+  const char* direct_obj_results[] = {
+      "false", "true", "1245", "9.00345", "A simple test", "\t\n", "space",
+      "",      "",     "",     ""};
+  // Check for direct objects.
+  for (int i = 0; i < m_DirectObjs.size(); ++i)
+    EXPECT_STREQ(m_DirectObjs[i]->GetString().c_str(), direct_obj_results[i]);
+
+  // Check indirect references.
+  const char* indirect_obj_results[] = {"true", "1245", "\t\n", "space",
+                                        "",     "",     ""};
+  for (int i = 0; i < m_RefObjs.size(); ++i) {
+    EXPECT_STREQ(m_RefObjs[i]->GetString().c_str(), indirect_obj_results[i]);
+  }
+}
+
+TEST_F(PDFObjectsTest, GetConstString) {
+  const char* direct_obj_results[] = {
+      nullptr, nullptr, nullptr, nullptr, "A simple test", "\t\n",
+      "space", nullptr, nullptr, nullptr, nullptr};
+  // Check for direct objects.
+  for (int i = 0; i < m_DirectObjs.size(); ++i) {
+    if (!direct_obj_results[i]) {
+      EXPECT_EQ(m_DirectObjs[i]->GetConstString().GetCStr(),
+                direct_obj_results[i]);
+    } else {
+      EXPECT_STREQ(m_DirectObjs[i]->GetConstString().GetCStr(),
+                   direct_obj_results[i]);
+    }
+  }
+  // Check indirect references.
+  const char* indirect_obj_results[] = {nullptr, nullptr, "\t\n", "space",
+                                        nullptr, nullptr, nullptr};
+  for (int i = 0; i < m_RefObjs.size(); ++i) {
+    if (!indirect_obj_results[i])
+      EXPECT_EQ(m_RefObjs[i]->GetConstString().GetCStr(), nullptr);
+    else {
+      EXPECT_STREQ(m_RefObjs[i]->GetConstString().GetCStr(),
+                   indirect_obj_results[i]);
+    }
+  }
+}
+
+TEST_F(PDFObjectsTest, GetNumber) {
+  const FX_FLOAT direct_obj_results[] = {0, 0, 1245, 9.00345f, 0, 0,
+                                         0, 0, 0,    0,        0};
+  // Check for direct objects.
+  for (int i = 0; i < m_DirectObjs.size(); ++i)
+    EXPECT_EQ(m_DirectObjs[i]->GetNumber(), direct_obj_results[i]);
+
+  // Check indirect references.
+  const FX_FLOAT indirect_obj_results[] = {0, 1245, 0, 0, 0, 0, 0};
+  for (int i = 0; i < m_RefObjs.size(); ++i)
+    EXPECT_EQ(m_RefObjs[i]->GetNumber(), indirect_obj_results[i]);
+}
+
+TEST_F(PDFObjectsTest, GetInteger) {
+  const int direct_obj_results[] = {0, 1, 1245, 9, 0, 0, 0, 0, 0, 0, 0};
+  // Check for direct objects.
+  for (int i = 0; i < m_DirectObjs.size(); ++i)
+    EXPECT_EQ(m_DirectObjs[i]->GetInteger(), direct_obj_results[i]);
+
+  // Check indirect references.
+  const int indirect_obj_results[] = {1, 1245, 0, 0, 0, 0, 0};
+  for (int i = 0; i < m_RefObjs.size(); ++i)
+    EXPECT_EQ(m_RefObjs[i]->GetInteger(), indirect_obj_results[i]);
+}
+
+TEST_F(PDFObjectsTest, GetDict) {
+  const CPDF_Dictionary* direct_obj_results[] = {
+      nullptr, nullptr, nullptr,   nullptr,         nullptr, nullptr,
+      nullptr, nullptr, m_DictObj, m_StreamDictObj, nullptr};
+  // Check for direct objects.
+  for (int i = 0; i < m_DirectObjs.size(); ++i)
+    EXPECT_EQ(m_DirectObjs[i]->GetDict(), direct_obj_results[i]);
+
+  // Check indirect references.
+  const CPDF_Dictionary* indirect_obj_results[] = {
+      nullptr, nullptr, nullptr, nullptr, nullptr, m_DictObj, m_StreamDictObj};
+  for (int i = 0; i < m_RefObjs.size(); ++i)
+    EXPECT_EQ(m_RefObjs[i]->GetDict(), indirect_obj_results[i]);
+}