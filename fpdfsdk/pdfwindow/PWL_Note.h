0b5adb6ead171069aa6f8c49c529f1c6d381e155
dsinclair <dsinclair@chromium.org>
2016-04-06 14:18:42 -0700

    Remove IPWL_NoteNotify and CPWL_Note.
    The IPWL_NoteNotify interface was never implemented. In removing, found that
    CPWL_Note was never created. Removed both classes. There were a bunch of methods
    that were never called that interacted with this code that have also been
    removed.
    
    BUG=pdfium:468
    
    Review URL: https://codereview.chromium.org/1867623002

--- fpdfsdk/pdfwindow/PWL_Note.h
+++ fpdfsdk/pdfwindow/PWL_Note.h
@@ -25,26 +25,7 @@ class CPWL_Note_Options;
 class CPWL_Note_RBBox;
 class IPWL_NoteHandler;
 class IPWL_NoteItem;
-class IPWL_NoteNotify;
 class IPopup_Note;
-
-class IPWL_NoteNotify {
- public:
-  virtual ~IPWL_NoteNotify() {}
-  virtual void OnNoteMove(const FX_RECT& rtWin) = 0;
-  virtual void OnNoteShow(FX_BOOL bShow) = 0;
-  virtual void OnNoteActivate(FX_BOOL bActive) = 0;
-  virtual void OnNoteClose() = 0;
-  virtual void OnItemCreate(IPWL_NoteItem* pItem) = 0;
-  virtual void OnItemDelete(IPWL_NoteItem* pItem) = 0;
-  virtual void OnSetAuthorName(IPWL_NoteItem* pItem) = 0;
-  virtual void OnSetBkColor(IPWL_NoteItem* pItem) = 0;
-  virtual void OnSetContents(IPWL_NoteItem* pItem) = 0;
-  virtual void OnSetDateTime(IPWL_NoteItem* pItem) = 0;
-  virtual void OnSetSubjectName(IPWL_NoteItem* pItem) = 0;
-  virtual void OnPopupMenu(int32_t x, int32_t y) = 0;
-  virtual void OnPopupMenu(IPWL_NoteItem* pItem, int32_t x, int32_t y) = 0;
-};
 
 class IPWL_NoteHandler {
  public:
@@ -58,14 +39,11 @@ class IPWL_NoteItem {
   virtual void SetPrivateData(void* pData) = 0;
   virtual void SetBkColor(const CPWL_Color& color) = 0;
   virtual void SetSubjectName(const CFX_WideString& sName) = 0;
-  virtual void SetAuthorName(const CFX_WideString& sName) = 0;
   virtual void SetDateTime(FX_SYSTEMTIME time) = 0;
   virtual void SetContents(const CFX_WideString& sContents) = 0;
 
-  virtual IPWL_NoteItem* CreateSubItem() = 0;
   virtual int32_t CountSubItems() const = 0;
   virtual IPWL_NoteItem* GetSubItems(int32_t index) const = 0;
-  virtual void DeleteSubItem(IPWL_NoteItem* pNoteItem) = 0;
   virtual void SetFocus() = 0;
 
   virtual IPWL_NoteItem* GetParentItem() const = 0;
@@ -191,8 +169,6 @@ class CPWL_Note_Contents : public CPWL_ListCtrl {
   void SetText(const CFX_WideString& sText);
   CFX_WideString GetText() const;
 
-  CPWL_NoteItem* CreateSubItem();
-  void DeleteSubItem(IPWL_NoteItem* pNoteItem);
   int32_t CountSubItems() const;
   IPWL_NoteItem* GetSubItems(int32_t index) const;
 
@@ -223,24 +199,17 @@ class CPWL_NoteItem : public CPWL_Wnd, public IPWL_NoteItem {
 
   virtual FX_BOOL IsTopItem() const { return FALSE; }
 
-  virtual void ResetSubjectName(int32_t nItemIndex);
   void EnableRead(FX_BOOL bEnabled);
   void EnableModify(FX_BOOL bEnabled);
-
-  void OnContentsValidate();
-  void OnCreateNoteItem();
 
   // IPWL_NoteItem
   void SetPrivateData(void* pData) override;
   void SetBkColor(const CPWL_Color& color) override;
   void SetSubjectName(const CFX_WideString& sName) override;
-  void SetAuthorName(const CFX_WideString& sName) override;
   void SetDateTime(FX_SYSTEMTIME time) override;
   void SetContents(const CFX_WideString& sContents) override;
-  IPWL_NoteItem* CreateSubItem() override;
   int32_t CountSubItems() const override;
   IPWL_NoteItem* GetSubItems(int32_t index) const override;
-  void DeleteSubItem(IPWL_NoteItem* pNoteItem) override;
   void SetFocus() override { SetNoteFocus(FALSE); }
   IPWL_NoteItem* GetParentItem() const override;
   void* GetPrivateData() const override;
@@ -266,14 +235,10 @@ class CPWL_NoteItem : public CPWL_Wnd, public IPWL_NoteItem {
   FX_FLOAT GetItemLeftMargin() override;
   FX_FLOAT GetItemRightMargin() override;
 
-  CPWL_NoteItem* CreateNoteItem();
   CPWL_NoteItem* GetParentNoteItem() const;
 
   void SetNoteFocus(FX_BOOL bLast);
   void PopupNoteItemMenu(const CFX_FloatPoint& point);
-
-  virtual const CPWL_Note* GetNote() const;
-  virtual IPWL_NoteNotify* GetNoteNotify() const;
 
  protected:
   CPWL_Label* m_pSubject;
@@ -288,64 +253,6 @@ class CPWL_NoteItem : public CPWL_Wnd, public IPWL_NoteItem {
   FX_FLOAT m_fOldItemHeight;
   FX_BOOL m_bSizeChanged;
   FX_BOOL m_bAllowModify;
-};
-
-class CPWL_Note : public CPWL_NoteItem {
- public:
-  CPWL_Note(IPopup_Note* pPopupNote,
-            IPWL_NoteNotify* pNoteNotify,
-            IPWL_NoteHandler* pNoteHandler);
-  ~CPWL_Note() override;
-
-  IPWL_NoteItem* Reply();
-  void EnableNotify(FX_BOOL bEnabled);
-  void SetIconType(int32_t nType);
-  void SetOptionsText(const CFX_WideString& sText);
-  void EnableRead(FX_BOOL bEnabled);
-  void EnableModify(FX_BOOL bEnabled);
-
-  CFX_WideString GetReplyString() const;
-  void SetReplyString(const CFX_WideString& str);
-
-  // CPWL_NoteItem
-  void SetSubjectName(const CFX_WideString& sName) override;
-  void SetAuthorName(const CFX_WideString& sName) override;
-  CFX_WideString GetAuthorName() const override;
-  void SetBkColor(const CPWL_Color& color) override;
-  void ResetSubjectName(int32_t nItemIndex) override {}
-  FX_BOOL IsTopItem() const override { return TRUE; }
-  const CPWL_Note* GetNote() const override;
-  IPWL_NoteNotify* GetNoteNotify() const override;
-  FX_BOOL OnLButtonDown(const CFX_FloatPoint& point, uint32_t nFlag) override;
-  FX_BOOL OnRButtonUp(const CFX_FloatPoint& point, uint32_t nFlag) override;
-  FX_BOOL OnMouseWheel(short zDelta,
-                       const CFX_FloatPoint& point,
-                       uint32_t nFlag) override;
-  void RePosChildWnd() override;
-  void CreateChildWnd(const PWL_CREATEPARAM& cp) override;
-  void OnNotify(CPWL_Wnd* pWnd,
-                uint32_t msg,
-                intptr_t wParam = 0,
-                intptr_t lParam = 0) override;
-
- protected:
-  FX_BOOL ResetScrollBar();
-  void RePosNoteChildren();
-  FX_BOOL ScrollBarShouldVisible();
-
- private:
-  CPWL_Label* m_pAuthor;
-  CPWL_Note_Icon* m_pIcon;
-  CPWL_Note_CloseBox* m_pCloseBox;
-  CPWL_Note_LBBox* m_pLBBox;
-  CPWL_Note_RBBox* m_pRBBox;
-  CPWL_ScrollBar* m_pContentsBar;
-  CPWL_Note_Options* m_pOptions;
-  IPWL_NoteNotify* m_pNoteNotify;
-  FX_BOOL m_bResizing;
-  PWL_SCROLL_INFO m_OldScrollInfo;
-  FX_BOOL m_bEnableNotify;
-  CFX_WideString m_sReplyString;
 };
 
 #endif  // FPDFSDK_PDFWINDOW_PWL_NOTE_H_
