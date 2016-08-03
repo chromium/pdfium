0d8c2d120efe62d79d083f25b43aa814d0f24646
jaepark <jaepark@google.com>
2016-08-02 17:32:23 -0700

    Generate default AP stream for strike out annotation.
    This patch generates a default AP stream for strike out annotation so that
    strike out annotations without AP stream can be displayed.
    
    Also, roll DEPS for testing/corpus to ddc1938 to test strike out annotations.
    
    BUG=62625
    
    Review-Url: https://codereview.chromium.org/2206083002

--- core/fpdfdoc/cpvt_generateap.cpp
+++ core/fpdfdoc/cpvt_generateap.cpp
@@ -619,6 +619,35 @@ bool CPVT_GenerateAP::GenerateUnderlineAP(CPDF_Document* pDoc,
   return true;
 }
 
+bool CPVT_GenerateAP::GenerateStrikeOutAP(CPDF_Document* pDoc,
+                                          CPDF_Dictionary* pAnnotDict) {
+  // If AP dictionary exists, we use the appearance defined in the
+  // existing AP dictionary.
+  if (pAnnotDict->KeyExist("AP"))
+    return false;
+
+  CFX_ByteTextBuf sAppStream;
+  CFX_ByteString sExtGSDictName = "GS";
+  sAppStream << "/" << sExtGSDictName << " gs ";
+
+  sAppStream << GetColorStringWithDefault(pAnnotDict,
+                                          CPVT_Color(CPVT_Color::kRGB, 0, 0, 0),
+                                          PaintOperation::STROKE);
+
+  CFX_FloatRect rect = pAnnotDict->GetRectBy("Rect");
+  rect.Normalize();
+
+  FX_FLOAT fLineWidth = 1.0;
+  FX_FLOAT fY = (rect.top + rect.bottom) / 2;
+  sAppStream << fLineWidth << " w " << rect.left << " " << fY << " m "
+             << rect.right << " " << fY << " l S\n";
+
+  CPDF_Dictionary* pExtGStateDict =
+      GenerateExtGStateDict(*pAnnotDict, sExtGSDictName, "Normal");
+  GenerateAndSetAPDict(pDoc, pAnnotDict, sAppStream, pExtGStateDict);
+  return true;
+}
+
 // Static.
 CFX_ByteString CPVT_GenerateAP::GenerateEditAP(
     IPVT_FontMap* pFontMap,
