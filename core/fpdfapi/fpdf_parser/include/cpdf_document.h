0ca160fbf31a7408df282823f7a3d4cbb5f386a1
npm <npm@chromium.org>
2016-09-08 14:21:51 -0700

    Delete unused methods in CPDF_Document
    Review-Url: https://codereview.chromium.org/2323933002

--- core/fpdfapi/fpdf_parser/include/cpdf_document.h
+++ core/fpdfapi/fpdf_parser/include/cpdf_document.h
@@ -47,18 +47,11 @@ class CPDF_Document : public CPDF_IndirectObjectHolder {
   CPDF_Dictionary* GetRoot() const { return m_pRootDict; }
   CPDF_Dictionary* GetInfo() const { return m_pInfoDict; }
 
-  void GetID(CFX_ByteString& id1, CFX_ByteString& id2) const {
-    id1 = m_ID1;
-    id2 = m_ID2;
-  }
-
   int GetPageCount() const;
   CPDF_Dictionary* GetPage(int iPage);
   int GetPageIndex(uint32_t objnum);
   uint32_t GetUserPermissions() const;
   CPDF_DocPageData* GetPageData() const { return m_pDocPage; }
-  void ClearPageData();
-  void RemoveColorSpaceFromPageData(CPDF_Object* pObject);
 
   std::unique_ptr<JBig2_DocumentContext>* CodecContext() {
     return &m_pCodecContext;
@@ -66,8 +59,6 @@ class CPDF_Document : public CPDF_IndirectObjectHolder {
   std::unique_ptr<CPDF_LinkList>* LinksContext() { return &m_pLinksContext; }
 
   CPDF_DocRenderData* GetRenderData() const { return m_pDocRender.get(); }
-  void ClearRenderData();
-  void ClearRenderFont();
 
   FX_BOOL IsFormStream(uint32_t objnum, FX_BOOL& bForm) const;
 
@@ -102,11 +93,6 @@ class CPDF_Document : public CPDF_IndirectObjectHolder {
                             FX_BOOL bVert,
                             FX_BOOL bTranslateName = FALSE);
 #endif
-#if _FXM_PLATFORM_ == _FXM_PLATFORM_APPLE_
-  CPDF_Font* AddMacFont(CTFontRef pFont,
-                        FX_BOOL bVert,
-                        FX_BOOL bTranslateName = FALSE);
-#endif
 
  protected:
   friend class CPDF_Creator;
@@ -125,14 +111,11 @@ class CPDF_Document : public CPDF_IndirectObjectHolder {
                     uint32_t objnum,
                     int& index,
                     int level = 0);
-  FX_BOOL CheckOCGVisible(CPDF_Dictionary* pOCG, FX_BOOL bPrinting);
   CPDF_Object* ParseIndirectObject(uint32_t objnum) override;
 
   std::unique_ptr<CPDF_Parser> m_pParser;
   CPDF_Dictionary* m_pRootDict;
   CPDF_Dictionary* m_pInfoDict;
-  CFX_ByteString m_ID1;
-  CFX_ByteString m_ID2;
   bool m_bLinearized;
   int m_iFirstPageNo;
   uint32_t m_dwFirstPageObjNum;
