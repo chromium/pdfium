8b67b19d7e6dfb8984cc9c92ef59a81cb4edaa77
stackexploit <stackexploit@gmail.com>
2016-10-26 22:40:34 -0700

    libtiff: Prevent a buffer overflow in function PixarLogDecode.
    Fix potential buffer write overrun in PixarLogDecode() on corrupted/unexpected
    images. The issue has been fixed in upstream (libtiff revision 1.44,
    author: erouault, commitid: 2SqWSFG5a8Ewffcz, date: 2016-06-28 23:12:19 +0800).
    
    This CL applies the official patch to tif_pixarlog.c.
    
    BUG=chromium:654172
    R=dsinclair@chromium.org, thestig@chromium.org
    
    Review-Url: https://codereview.chromium.org/2453253003

--- third_party/libtiff/tif_pixarlog.c
+++ third_party/libtiff/tif_pixarlog.c
@@ -457,6 +457,7 @@ horizontalAccumulate8abgr(uint16 *wp, int n, int stride, unsigned char *op,
 typedef	struct {
 	TIFFPredictorState	predict;
 	z_stream		stream;
+	tmsize_t		tbuf_size; /* only set/used on reading for now */
 	uint16			*tbuf; 
 	uint16			stride;
 	int			state;
@@ -692,6 +693,7 @@ PixarLogSetupDecode(TIFF* tif)
 	sp->tbuf = (uint16 *) _TIFFmalloc(tbuf_size);
 	if (sp->tbuf == NULL)
 		return (0);
+	sp->tbuf_size = tbuf_size;
 	if (sp->user_datafmt == PIXARLOGDATAFMT_UNKNOWN)
 		sp->user_datafmt = PixarLogGuessDataFmt(td);
 	if (sp->user_datafmt == PIXARLOGDATAFMT_UNKNOWN) {
@@ -779,6 +781,12 @@ PixarLogDecode(TIFF* tif, uint8* op, tmsize_t occ, uint16 s)
 	if (sp->stream.avail_out != nsamples * sizeof(uint16))
 	{
 		TIFFErrorExt(tif->tif_clientdata, module, "ZLib cannot deal with buffers this size");
+		return (0);
+	}
+	/* Check that we will not fill more than what was allocated */
+	if (sp->stream.avail_out > sp->tbuf_size)
+	{
+		TIFFErrorExt(tif->tif_clientdata, module, "sp->stream.avail_out > sp->tbuf_size");
 		return (0);
 	}
 	do {
