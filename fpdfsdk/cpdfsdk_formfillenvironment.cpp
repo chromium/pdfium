chromium/pdfium

commit 6c659ab22988716c0f578460a2048663ab93805a

Cleanup nits from merge CL
This CL cleans up the nits from https://codereview.chromium.org/2410893002/.

Review-Url: https://codereview.chromium.org/2417633002

fpdfsdk/cpdfsdk_formfillenvironment.cpp
@@ -40,16 +40,13 @@ CPDFSDK_FormFillEnvironment::CPDFSDK_FormFillEnvironment(
     : m_pInfo(pFFinfo),
       m_pUnderlyingDoc(pDoc),
       m_pSysHandler(new CFX_SystemHandler(this)),
-      m_bChangeMask(FALSE),
-      m_bBeingDestroyed(FALSE) {}
+      m_bChangeMask(false),
+      m_bBeingDestroyed(false) {}
 
 CPDFSDK_FormFillEnvironment::~CPDFSDK_FormFillEnvironment() {
-  m_bBeingDestroyed = TRUE;
+  m_bBeingDestroyed = true;
 
   ClearAllFocusedAnnots();
-  for (auto& it : m_pageMap)
-    delete it.second;
-  m_pageMap.clear();
 
   // |m_pAnnotHandlerMgr| will try to access |m_pFormFiller|
   // when it cleans up. So, we must make sure it is cleaned up before
@@ -584,16 +581,16 @@ void CPDFSDK_FormFillEnvironment::ClearAllFocusedAnnots() {
 
 CPDFSDK_PageView* CPDFSDK_FormFillEnvironment::GetPageView(
     UnderlyingPageType* pUnderlyingPage,
-    bool ReNew) {
+    bool renew) {
   auto it = m_pageMap.find(pUnderlyingPage);
   if (it != m_pageMap.end())
-    return it->second;
+    return it->second.get();
 
-  if (!ReNew)
+  if (!renew)
     return nullptr;
 
   CPDFSDK_PageView* pPageView = new CPDFSDK_PageView(this, pUnderlyingPage);
-  m_pageMap[pUnderlyingPage] = pPageView;
+  m_pageMap[pUnderlyingPage].reset(pPageView);
   // Delay to load all the annotations, to avoid endless loop.
   pPageView->LoadFXAnnots();
   return pPageView;
@@ -612,7 +609,7 @@ CPDFSDK_PageView* CPDFSDK_FormFillEnvironment::GetPageView(int nIndex) {
     return nullptr;
 
   auto it = m_pageMap.find(pTempPage);
-  return it != m_pageMap.end() ? it->second : nullptr;
+  return it != m_pageMap.end() ? it->second.get() : nullptr;
 }
 
 void CPDFSDK_FormFillEnvironment::ProcJavascriptFun() {
@@ -664,7 +661,7 @@ void CPDFSDK_FormFillEnvironment::RemovePageView(
   if (it == m_pageMap.end())
     return;
 
-  CPDFSDK_PageView* pPageView = it->second;
+  CPDFSDK_PageView* pPageView = it->second.get();
   if (pPageView->IsLocked() || pPageView->IsBeingDestroyed())
     return;
 
@@ -683,8 +680,6 @@ void CPDFSDK_FormFillEnvironment::RemovePageView(
   // Remove the page from the map to make sure we don't accidentally attempt
   // to use the |pPageView| while we're cleaning it up.
   m_pageMap.erase(it);
-
-  delete pPageView;
 }
 
 UnderlyingPageType* CPDFSDK_FormFillEnvironment::GetPage(int nIndex) {
@@ -700,7 +695,7 @@ CPDFSDK_InterForm* CPDFSDK_FormFillEnvironment::GetInterForm() {
 void CPDFSDK_FormFillEnvironment::UpdateAllViews(CPDFSDK_PageView* pSender,
                                                  CPDFSDK_Annot* pAnnot) {
   for (const auto& it : m_pageMap) {
-    CPDFSDK_PageView* pPageView = it.second;
+    CPDFSDK_PageView* pPageView = it.second.get();
     if (pPageView != pSender)
       pPageView->UpdateView(pAnnot);
   }

fpdfsdk/cpdfsdk_formfillenvironment.h
@@ -34,37 +34,7 @@ class CPDFSDK_FormFillEnvironment
                               FPDF_FORMFILLINFO* pFFinfo);
   ~CPDFSDK_FormFillEnvironment();
 
-  CPDFSDK_InterForm* GetInterForm();
-
-  // Gets the document object for the next layer down; for master this is
-  // a CPDF_Document, but for XFA it is a CPDFXFA_Document.
-  UnderlyingDocumentType* GetUnderlyingDocument() const {
-#ifdef PDF_ENABLE_XFA
-    return GetXFADocument();
-#else   // PDF_ENABLE_XFA
-    return GetPDFDocument();
-#endif  // PDF_ENABLE_XFA
-  }
-
-  // Gets the CPDF_Document, either directly in master, or from the
-  // CPDFXFA_Document for XFA.
-  CPDF_Document* GetPDFDocument() const {
-#ifdef PDF_ENABLE_XFA
-    return m_pUnderlyingDoc ? m_pUnderlyingDoc->GetPDFDoc() : nullptr;
-#else   // PDF_ENABLE_XFA
-    return m_pUnderlyingDoc;
-#endif  // PDF_ENABLE_XFA
-  }
-
-#ifdef PDF_ENABLE_XFA
-  // Gets the XFA document directly (XFA-only).
-  CPDFXFA_Document* GetXFADocument() const { return m_pUnderlyingDoc; }
-  void ResetXFADocument() { m_pUnderlyingDoc = nullptr; }
-
-  int GetPageViewCount() const { return m_pageMap.size(); }
-#endif  // PDF_ENABLE_XFA
-
-  CPDFSDK_PageView* GetPageView(UnderlyingPageType* pPage, bool ReNew);
+  CPDFSDK_PageView* GetPageView(UnderlyingPageType* pPage, bool renew);
   CPDFSDK_PageView* GetPageView(int nIndex);
   CPDFSDK_PageView* GetCurrentView();
   void RemovePageView(UnderlyingPageType* pPage);
@@ -87,9 +57,9 @@ class CPDFSDK_FormFillEnvironment
   int GetPageCount() { return m_pUnderlyingDoc->GetPageCount(); }
   FX_BOOL GetPermissions(int nFlag);
 
-  FX_BOOL GetChangeMark() { return m_bChangeMask; }
-  void SetChangeMark() { m_bChangeMask = TRUE; }
-  void ClearChangeMark() { m_bChangeMask = FALSE; }
+  bool GetChangeMark() const { return m_bChangeMask; }
+  void SetChangeMark() { m_bChangeMask = true; }
+  void ClearChangeMark() { m_bChangeMask = false; }
 
   UnderlyingPageType* GetPage(int nIndex);
 
@@ -130,7 +100,20 @@ class CPDFSDK_FormFillEnvironment
                     float* fPosArray,
                     int sizeOfArray);
 
+  UnderlyingDocumentType* GetUnderlyingDocument() const {
+    return m_pUnderlyingDoc;
+  }
+
 #ifdef PDF_ENABLE_XFA
+  CPDF_Document* GetPDFDocument() const {
+    return m_pUnderlyingDoc ? m_pUnderlyingDoc->GetPDFDoc() : nullptr;
+  }
+
+  CPDFXFA_Document* GetXFADocument() const { return m_pUnderlyingDoc; }
+  void ResetXFADocument() { m_pUnderlyingDoc = nullptr; }
+
+  int GetPageViewCount() const { return m_pageMap.size(); }
+
   void DisplayCaret(FPDF_PAGE page,
                     FPDF_BOOL bVisible,
                     double left,
@@ -176,6 +159,8 @@ class CPDFSDK_FormFillEnvironment
   CFX_WideString GetLanguage();
 
   void PageEvent(int iPageCount, uint32_t dwEventType) const;
+#else   // PDF_ENABLE_XFA
+  CPDF_Document* GetPDFDocument() const { return m_pUnderlyingDoc; }
 #endif  // PDF_ENABLE_XFA
 
   int JS_appAlert(const FX_WCHAR* Msg,
@@ -221,20 +206,21 @@ class CPDFSDK_FormFillEnvironment
   CPDFSDK_AnnotHandlerMgr* GetAnnotHandlerMgr();  // Creates if not present.
   IJS_Runtime* GetJSRuntime();                    // Creates if not present.
   CPDFSDK_ActionHandler* GetActionHander();       // Creates if not present.
+  CPDFSDK_InterForm* GetInterForm();              // Creates if not present.
 
  private:
   std::unique_ptr<CPDFSDK_AnnotHandlerMgr> m_pAnnotHandlerMgr;
   std::unique_ptr<CPDFSDK_ActionHandler> m_pActionHandler;
   std::unique_ptr<IJS_Runtime> m_pJSRuntime;
   FPDF_FORMFILLINFO* const m_pInfo;
-  std::map<UnderlyingPageType*, CPDFSDK_PageView*> m_pageMap;
+  std::map<UnderlyingPageType*, std::unique_ptr<CPDFSDK_PageView>> m_pageMap;
   std::unique_ptr<CPDFSDK_InterForm> m_pInterForm;
   CPDFSDK_Annot::ObservedPtr m_pFocusAnnot;
   UnderlyingDocumentType* m_pUnderlyingDoc;
   std::unique_ptr<CFFL_InteractiveFormFiller> m_pFormFiller;
   std::unique_ptr<CFX_SystemHandler> m_pSysHandler;
-  FX_BOOL m_bChangeMask;
-  FX_BOOL m_bBeingDestroyed;
+  bool m_bChangeMask;
+  bool m_bBeingDestroyed;
 };
 
 #endif  // FPDFSDK_CPDFSDK_FORMFILLENVIRONMENT_H_

commit 2599ff7ae76a2e7514c1794281ccb0802d6df2bc

Fix CPDFSDK_FormFillEnvironment destruction order
This was broken in 6c659ab2.  Also fix m_PageMap naming while
we're at it to match conventions.

TEST=covered by forthcoming bug 662698 CL.

Review-Url: https://codereview.chromium.org/2489663002

fpdfsdk/cpdfsdk_formfillenvironment.cpp
@@ -41,12 +41,14 @@ CPDFSDK_FormFillEnvironment::CPDFSDK_FormFillEnvironment(
 
 CPDFSDK_FormFillEnvironment::~CPDFSDK_FormFillEnvironment() {
   m_bBeingDestroyed = true;
-
   ClearAllFocusedAnnots();
 
-  // |m_pAnnotHandlerMgr| will try to access |m_pFormFiller|
-  // when it cleans up. So, we must make sure it is cleaned up before
-  // |m_pFormFiller|.
+  // |m_PageMap| will try to access |m_pInterForm| when it cleans itself up.
+  // Make sure it is deleted before |m_pInterForm|.
+  m_PageMap.clear();
+
+  // |m_pAnnotHandlerMgr| will try to access |m_pFormFiller| when it cleans
+  // itself up. Make sure it is deleted before |m_pFormFiller|.
   m_pAnnotHandlerMgr.reset();
 
   // Must destroy the |m_pFormFiller| before the environment (|this|)
@@ -563,7 +565,7 @@ void CPDFSDK_FormFillEnvironment::PageEvent(int iPageCount,
 #endif  // PDF_ENABLE_XFA
 
 void CPDFSDK_FormFillEnvironment::ClearAllFocusedAnnots() {
-  for (auto& it : m_pageMap) {
+  for (auto& it : m_PageMap) {
     if (it.second->IsValidSDKAnnot(GetFocusAnnot()))
       KillFocusAnnot(0);
   }
@@ -572,15 +574,15 @@ void CPDFSDK_FormFillEnvironment::ClearAllFocusedAnnots() {
 CPDFSDK_PageView* CPDFSDK_FormFillEnvironment::GetPageView(
     UnderlyingPageType* pUnderlyingPage,
     bool renew) {
-  auto it = m_pageMap.find(pUnderlyingPage);
-  if (it != m_pageMap.end())
+  auto it = m_PageMap.find(pUnderlyingPage);
+  if (it != m_PageMap.end())
     return it->second.get();
 
   if (!renew)
     return nullptr;
 
   CPDFSDK_PageView* pPageView = new CPDFSDK_PageView(this, pUnderlyingPage);
-  m_pageMap[pUnderlyingPage].reset(pPageView);
+  m_PageMap[pUnderlyingPage].reset(pPageView);
   // Delay to load all the annotations, to avoid endless loop.
   pPageView->LoadFXAnnots();
   return pPageView;
@@ -598,8 +600,8 @@ CPDFSDK_PageView* CPDFSDK_FormFillEnvironment::GetPageView(int nIndex) {
   if (!pTempPage)
     return nullptr;
 
-  auto it = m_pageMap.find(pTempPage);
-  return it != m_pageMap.end() ? it->second.get() : nullptr;
+  auto it = m_PageMap.find(pTempPage);
+  return it != m_PageMap.end() ? it->second.get() : nullptr;
 }
 
 void CPDFSDK_FormFillEnvironment::ProcJavascriptFun() {
@@ -647,8 +649,8 @@ bool CPDFSDK_FormFillEnvironment::ProcOpenAction() {
 
 void CPDFSDK_FormFillEnvironment::RemovePageView(
     UnderlyingPageType* pUnderlyingPage) {
-  auto it = m_pageMap.find(pUnderlyingPage);
-  if (it == m_pageMap.end())
+  auto it = m_PageMap.find(pUnderlyingPage);
+  if (it == m_PageMap.end())
     return;
 
   CPDFSDK_PageView* pPageView = it->second.get();
@@ -669,7 +671,7 @@ void CPDFSDK_FormFillEnvironment::RemovePageView(
 
   // Remove the page from the map to make sure we don't accidentally attempt
   // to use the |pPageView| while we're cleaning it up.
-  m_pageMap.erase(it);
+  m_PageMap.erase(it);
 }
 
 UnderlyingPageType* CPDFSDK_FormFillEnvironment::GetPage(int nIndex) {
@@ -684,7 +686,7 @@ CPDFSDK_InterForm* CPDFSDK_FormFillEnvironment::GetInterForm() {
 
 void CPDFSDK_FormFillEnvironment::UpdateAllViews(CPDFSDK_PageView* pSender,
                                                  CPDFSDK_Annot* pAnnot) {
-  for (const auto& it : m_pageMap) {
+  for (const auto& it : m_PageMap) {
     CPDFSDK_PageView* pPageView = it.second.get();
     if (pPageView != pSender)
       pPageView->UpdateView(pAnnot);

fpdfsdk/cpdfsdk_formfillenvironment.h
@@ -112,7 +112,7 @@ class CPDFSDK_FormFillEnvironment
   CPDFXFA_Context* GetXFAContext() const { return m_pUnderlyingDoc; }
   void ResetXFADocument() { m_pUnderlyingDoc = nullptr; }
 
-  int GetPageViewCount() const { return m_pageMap.size(); }
+  int GetPageViewCount() const { return m_PageMap.size(); }
 
   void DisplayCaret(FPDF_PAGE page,
                     FPDF_BOOL bVisible,
@@ -213,7 +213,7 @@ class CPDFSDK_FormFillEnvironment
   std::unique_ptr<CPDFSDK_ActionHandler> m_pActionHandler;
   std::unique_ptr<IJS_Runtime> m_pJSRuntime;
   FPDF_FORMFILLINFO* const m_pInfo;
-  std::map<UnderlyingPageType*, std::unique_ptr<CPDFSDK_PageView>> m_pageMap;
+  std::map<UnderlyingPageType*, std::unique_ptr<CPDFSDK_PageView>> m_PageMap;
   std::unique_ptr<CPDFSDK_InterForm> m_pInterForm;
   CPDFSDK_Annot::ObservedPtr m_pFocusAnnot;
   UnderlyingDocumentType* m_pUnderlyingDoc;