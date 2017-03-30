e2e1794d55a0890341eb4fe3e1d0ed80379f905a
Dominik Röttsches <drott@chromium.org>
2017-03-30 20:36:24 +0300

    Remove unnecessary FreeType include.
    BUG=706824
    
    Change-Id: I7f2138689d8e0d93fcb100c0215c8cf6bde427de
    Reviewed-on: https://pdfium-review.googlesource.com/3392
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>

--- xfa/fgas/font/cfgas_fontmgr.h
+++ xfa/fgas/font/cfgas_fontmgr.h
@@ -18,7 +18,6 @@
 #include "core/fxge/cfx_fontmapper.h"
 #include "core/fxge/fx_freetype.h"
 #include "core/fxge/ifx_systemfontinfo.h"
-#include "freetype/fttypes.h"
 #include "xfa/fgas/crt/ifgas_stream.h"
 
 #define FX_FONTSTYLE_Normal 0x00
