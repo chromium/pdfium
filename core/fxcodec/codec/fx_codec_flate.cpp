36c5b170e722362d4b5f7cb6e4e0099ed227a307
Miklos Vajna <vmiklos@collabora.co.uk>
2017-04-13 10:09:06 +0200

    core: allow building against system zlib
    In a way similar to the existing USE_SYSTEM_LIBJPEG. The default is of
    course still the DEPS-based zlib checkout.
    
    Change-Id: I649e75beb1a19c4cf7abda42fa10ebe5babfe946
    Reviewed-on: https://pdfium-review.googlesource.com/4070
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

--- core/fxcodec/codec/fx_codec_flate.cpp
+++ core/fxcodec/codec/fx_codec_flate.cpp
@@ -15,7 +15,12 @@
 #include "core/fxcrt/fx_ext.h"
 #include "third_party/base/numerics/safe_conversions.h"
 #include "third_party/base/ptr_util.h"
+
+#if defined(USE_SYSTEM_ZLIB)
+#include <zlib.h>
+#else
 #include "third_party/zlib/zlib.h"
+#endif
 
 extern "C" {
 
