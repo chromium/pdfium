852fb12d554abbbda65bbbf3720117a0aad5a9c9
Dan Sinclair <dsinclair@chromium.org>
2017-05-08 14:22:48 -0400

    [lcms] Verify enough data to service request before allocating
    If the count of items is large enough, there maybe not enough data in
    the file to read. This Cl verifies we'll have enough data before
    attempting to allocate the memory to store the results.
    
    Bug: chromium:718504
    Change-Id: I82e7df3511e529c4bd72a772e9d6e607a0615927
    Reviewed-on: https://pdfium-review.googlesource.com/5110
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: dsinclair <dsinclair@chromium.org>

--- third_party/lcms2-2.6/src/cmstypes.c
+++ third_party/lcms2-2.6/src/cmstypes.c
@@ -173,6 +173,12 @@ cmsBool ReadPositionTable(struct _cms_typehandler_struct* self,
 {
     cmsUInt32Number i;
     cmsUInt32Number *ElementOffsets = NULL, *ElementSizes = NULL;
+    cmsUInt32Number currentPosition;
+
+    currentPosition = io->Tell(io);
+    // Verify there is enough space left to read two cmsUInt32Number items for Count items.
+    if (((io->ReportedSize - currentPosition) / (2 * sizeof(cmsUInt32Number))) < Count)
+        return FALSE;
 
     // Let's take the offsets to each element
     ElementOffsets = (cmsUInt32Number *) _cmsCalloc(io ->ContextID, Count, sizeof(cmsUInt32Number));
