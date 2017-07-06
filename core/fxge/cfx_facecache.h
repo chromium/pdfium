75e1ab05e731d99b9acdc86de47640720e848415
Cary Clark <caryclark@google.com>
2017-07-06 13:45:58 +0000

    Revert "typeface double delete"
    This reverts commit ddf2418ba8e5d925909d7955ac22b33f37ccce44.
    
    Reason for revert: not right
    
    Original change's description:
    > typeface double delete
    > 
    > SkTypeface was doubly deleted at pdfium teardown
    > SkTypeface has two pointers but no owners.
    > Making the font cache an owner fixes the bug but
    > violates checkdeps rules. Let me know what to
    > do about that.
    > 
    > R=​dsinclair@chromium.org,npm@chromium.org
    > Bug: 736133
    > Change-Id: I756a41258a5ac86e70139d7a587c5da9bb7a707b
    > Reviewed-on: https://pdfium-review.googlesource.com/7270
    > Reviewed-by: Nicolás Peña <npm@chromium.org>
    > Commit-Queue: Cary Clark <caryclark@google.com>
    
    TBR=dsinclair@chromium.org,caryclark@google.com,npm@chromium.org,caryclark@skia.org
    
    Change-Id: I255f50acf2cbaecc85b8e5ac3893c1fbc77b6492
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 736133
    Reviewed-on: https://pdfium-review.googlesource.com/7310
    Reviewed-by: Cary Clark <caryclark@google.com>
    Commit-Queue: Cary Clark <caryclark@google.com>

--- core/fxge/cfx_facecache.h
+++ core/fxge/cfx_facecache.h
@@ -14,10 +14,6 @@
 #include "core/fxge/fx_font.h"
 #include "core/fxge/fx_freetype.h"
 
-#if defined _SKIA_SUPPORT_ || _SKIA_SUPPORT_PATHS_
-#include "third_party/skia/include/core/SkTypeface.h"
-#endif
-
 class CFX_FaceCache {
  public:
   explicit CFX_FaceCache(FXFT_Face face);
@@ -64,7 +60,7 @@ class CFX_FaceCache {
   std::map<CFX_ByteString, std::unique_ptr<CFX_SizeGlyphCache>> m_SizeMap;
   std::map<uint32_t, std::unique_ptr<CFX_PathData>> m_PathMap;
 #if defined _SKIA_SUPPORT_ || _SKIA_SUPPORT_PATHS_
-  sk_sp<SkTypeface> m_pTypeface;
+  CFX_UnownedPtr<CFX_TypeFace> m_pTypeface;
 #endif
 };
 
