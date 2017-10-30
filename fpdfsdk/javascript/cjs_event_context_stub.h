5ce2d2e0c7899dc2d3b3ac6a25293894fea04183
Dan Sinclair <dsinclair@chromium.org>
2017-10-30 16:50:51 +0000

    Rename JS_Runtime_Stub to cjs_runtimestub
    This CL renames JS_Runtime_Stub.cpp to cjs_runtimestub.cpp to match the
    contents. The event context stub is moved to to
    cjs_event_context_stub.cpp.
    
    Change-Id: I1b231b10aaedd1fe484e55736b0fb562d0a0df88
    Reviewed-on: https://pdfium-review.googlesource.com/17037
    Commit-Queue: dsinclair <dsinclair@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>

--- fpdfsdk/javascript/cjs_event_context_stub.h
+++ fpdfsdk/javascript/cjs_event_context_stub.h
@@ -1,15 +1,13 @@
-// Copyright 2015 PDFium Authors. All rights reserved.
+// Copyright 2017 PDFium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 // Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
 
-#include <memory>
+#ifndef FPDFSDK_JAVASCRIPT_CJS_EVENT_CONTEXT_STUB_H_
+#define FPDFSDK_JAVASCRIPT_CJS_EVENT_CONTEXT_STUB_H_
 
-#include "core/fxcrt/unowned_ptr.h"
 #include "fpdfsdk/javascript/ijs_event_context.h"
-#include "fpdfsdk/javascript/ijs_runtime.h"
-#include "third_party/base/ptr_util.h"
 
 class CJS_EventContextStub final : public IJS_EventContext {
  public:
@@ -17,9 +15,7 @@ class CJS_EventContextStub final : public IJS_EventContext {
   ~CJS_EventContextStub() override {}
 
   // IJS_EventContext:
-  bool RunScript(const WideString& script, WideString* info) override {
-    return false;
-  }
+  bool RunScript(const WideString& script, WideString* info) override;
 
   void OnApp_Init() override {}
   void OnDoc_Open(CPDFSDK_FormFillEnvironment* pFormFillEnv,
@@ -119,50 +115,4 @@ class CJS_EventContextStub final : public IJS_EventContext {
   void OnExternal_Exec() override {}
 };
 
-class CJS_RuntimeStub final : public IJS_Runtime {
- public:
-  explicit CJS_RuntimeStub(CPDFSDK_FormFillEnvironment* pFormFillEnv)
-      : m_pFormFillEnv(pFormFillEnv) {}
-  ~CJS_RuntimeStub() override {}
-
-  IJS_EventContext* NewEventContext() override {
-    if (!m_pContext)
-      m_pContext = pdfium::MakeUnique<CJS_EventContextStub>();
-    return m_pContext.get();
-  }
-
-  void ReleaseEventContext(IJS_EventContext* pContext) override {}
-
-  CPDFSDK_FormFillEnvironment* GetFormFillEnv() const override {
-    return m_pFormFillEnv.Get();
-  }
-
-#ifdef PDF_ENABLE_XFA
-  bool GetValueByName(const ByteStringView&, CFXJSE_Value*) override {
-    return false;
-  }
-
-  bool SetValueByName(const ByteStringView&, CFXJSE_Value*) override {
-    return false;
-  }
-#endif  // PDF_ENABLE_XFA
-
-  int ExecuteScript(const WideString& script, WideString* info) override {
-    return 0;
-  }
-
- protected:
-  UnownedPtr<CPDFSDK_FormFillEnvironment> const m_pFormFillEnv;
-  std::unique_ptr<CJS_EventContextStub> m_pContext;
-};
-
-// static
-void IJS_Runtime::Initialize(unsigned int slot, void* isolate) {}
-
-// static
-void IJS_Runtime::Destroy() {}
-
-// static
-IJS_Runtime* IJS_Runtime::Create(CPDFSDK_FormFillEnvironment* pFormFillEnv) {
-  return new CJS_RuntimeStub(pFormFillEnv);
-}
+#endif  // FPDFSDK_JAVASCRIPT_CJS_EVENT_CONTEXT_STUB_H_
