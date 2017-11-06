05ac5961fec712fe9d0f364012982620d814aade
Lei Zhang <thestig@chromium.org>
2017-11-06 15:27:20 +0000

    Pack struct FXCMAP_CMap more densely to save 1 KB.
    Change-Id: Ida9738f4ad7ef266c2edb794f12dcf49b64fc590
    Reviewed-on: https://pdfium-review.googlesource.com/17750
    Commit-Queue: dsinclair <dsinclair@chromium.org>
    Reviewed-by: dsinclair <dsinclair@chromium.org>

--- core/fpdfapi/cmaps/cmap_int.h
+++ core/fpdfapi/cmaps/cmap_int.h
@@ -11,15 +11,15 @@
 #include "core/fxcrt/fx_system.h"
 
 struct FXCMAP_CMap {
-  enum MapType { None, Single, Range };
+  enum MapType : uint8_t { None, Single, Range };
 
   const char* m_Name;
-  MapType m_WordMapType;
   const uint16_t* m_pWordMap;
-  uint16_t m_WordCount;
-  MapType m_DWordMapType;
   const uint16_t* m_pDWordMap;
+  uint16_t m_WordCount;
   uint16_t m_DWordCount;
+  MapType m_WordMapType;
+  MapType m_DWordMapType;
   int8_t m_UseOffset;
 };
 
