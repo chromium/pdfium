82f2afe603ccb1e879a7a292259b0882b3d27bb6
Lei Zhang <thestig@chromium.org>
2017-12-05 20:26:52 +0000

    Fix CXFA_SimpleParser member destruction order.
    To avoid confusing UnownedPtr.
    
    BUG=chromium:791616
    
    Change-Id: Ieb637f726049eee7c640b6de3b6ecd848ab40384
    Reviewed-on: https://pdfium-review.googlesource.com/20391
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

--- xfa/fxfa/parser/cxfa_simple_parser.h
+++ xfa/fxfa/parser/cxfa_simple_parser.h
@@ -73,8 +73,8 @@ class CXFA_SimpleParser {
                         CFX_XMLInstruction* pXMLInstruction,
                         XFA_PacketType ePacketID);
 
-  UnownedPtr<CFX_XMLParser> m_pXMLParser;  // Owned by |m_pXMLDoc|
   std::unique_ptr<CFX_XMLDoc> m_pXMLDoc;
+  UnownedPtr<CFX_XMLParser> m_pXMLParser;  // Owned by |m_pXMLDoc|
   RetainPtr<CFX_SeekableStreamProxy> m_pStream;
   RetainPtr<IFX_SeekableStream> m_pFileRead;
   UnownedPtr<CXFA_Document> m_pFactory;
