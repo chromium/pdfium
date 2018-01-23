4d4731c620ea1fa95c07f8855f84d28c78090119
Tom Sepez <tsepez@chromium.org>
2018-01-23 00:43:09 +0000

    Tidy fxjs/cfxjse_isolatetracker.h
    Move constructors to .cpp file.
    Remove unused member and its accessor method.
    Remove unused system includes.
    IsolateHandleRootContext is-a IsolateHandle (not has-a).
    
    Change-Id: Ifcba44f3ea7ad963fee731c24b2e79c5a8124539
    Reviewed-on: https://pdfium-review.googlesource.com/23171
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>

--- fxjs/cfxjse_isolatetracker.h
+++ fxjs/cfxjse_isolatetracker.h
@@ -7,19 +7,13 @@
 #ifndef FXJS_CFXJSE_ISOLATETRACKER_H_
 #define FXJS_CFXJSE_ISOLATETRACKER_H_
 
-#include <map>
-#include <memory>
-#include <vector>
-
-#include "v8/include/v8.h"
-
 #include "fxjs/cfxjse_runtimedata.h"
+#include "v8/include/v8.h"
 
 class CFXJSE_ScopeUtil_IsolateHandle {
  public:
-  explicit CFXJSE_ScopeUtil_IsolateHandle(v8::Isolate* pIsolate)
-      : m_isolate(pIsolate), m_iscope(pIsolate), m_hscope(pIsolate) {}
-  v8::Isolate* GetIsolate() { return m_isolate; }
+  explicit CFXJSE_ScopeUtil_IsolateHandle(v8::Isolate* pIsolate);
+  ~CFXJSE_ScopeUtil_IsolateHandle();
 
  private:
   CFXJSE_ScopeUtil_IsolateHandle(const CFXJSE_ScopeUtil_IsolateHandle&) =
@@ -28,18 +22,15 @@ class CFXJSE_ScopeUtil_IsolateHandle {
   void* operator new(size_t size) = delete;
   void operator delete(void*, size_t) = delete;
 
-  v8::Isolate* m_isolate;
   v8::Isolate::Scope m_iscope;
   v8::HandleScope m_hscope;
 };
 
-class CFXJSE_ScopeUtil_IsolateHandleRootContext {
+class CFXJSE_ScopeUtil_IsolateHandleRootContext
+    : public CFXJSE_ScopeUtil_IsolateHandle {
  public:
-  explicit CFXJSE_ScopeUtil_IsolateHandleRootContext(v8::Isolate* pIsolate)
-      : m_parent(pIsolate),
-        m_cscope(v8::Local<v8::Context>::New(
-            pIsolate,
-            CFXJSE_RuntimeData::Get(pIsolate)->m_hRootContext)) {}
+  explicit CFXJSE_ScopeUtil_IsolateHandleRootContext(v8::Isolate* pIsolate);
+  ~CFXJSE_ScopeUtil_IsolateHandleRootContext();
 
  private:
   CFXJSE_ScopeUtil_IsolateHandleRootContext(
@@ -48,7 +39,6 @@ class CFXJSE_ScopeUtil_IsolateHandleRootContext {
   void* operator new(size_t size) = delete;
   void operator delete(void*, size_t) = delete;
 
-  CFXJSE_ScopeUtil_IsolateHandle m_parent;
   v8::Context::Scope m_cscope;
 };
 
