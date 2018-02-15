5b097c73dec7e7979166b3396efcf9e001b42b9e
Nicolas Pena <npm@chromium.org>
2018-02-15 18:33:15 +0000

    Fix roll-freetype script
    In c3532457d457e868afb8506e047ab353f2651bc0, a cmp was added to the roller
    script, but it should be cp instead since we want both files to be the same.
    
    Change-Id: I12dc32ab1633196267537e4db3a3522058e44a9d
    Reviewed-on: https://pdfium-review.googlesource.com/26830
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>

--- third_party/freetype/roll-freetype.sh
+++ third_party/freetype/roll-freetype.sh
@@ -14,7 +14,8 @@ FTVERSION=`git -C third_party/freetype/src/ describe --long`
 FTCOMMIT=`git -C third_party/freetype/src/ rev-parse HEAD`
 
 # Make sure our copy of pstables.h matches the one in freetype/src.
-cmp third_party/freetype/src/src/psnames/pstables.h \
+# May need to --bypass-hooks to prevent formatting of this file.
+cp third_party/freetype/src/src/psnames/pstables.h \
   third_party/freetype/include/pstables.h
 
 sed -i "s/^Version: .*\$/Version: ${FTVERSION%-*}/" \
@@ -23,4 +24,5 @@ sed -i "s/^Revision: .*\$/Revision: ${FTCOMMIT}/" \
   third_party/freetype/README.pdfium
 
 git add third_party/freetype/README.pdfium
+git add third_party/freetype/include/pstables.h
 git commit --quiet --amend --no-edit
