7c7f78a432a41b7074d225a91a590185e02aacf7
Tom Sepez <tsepez@chromium.org>
2018-04-27 22:46:28 +0000

    Tidy cfx_gif.h.
    Use C++ style struct syntax (file already has other C++ features).
    
    Assert that things have packed as intended since they map to
    known layouts. Order these asserts in the same order as .h file.
    
    Change-Id: I0a006c4b5789fb544783f488d5b4e609e32c7ec1
    Reviewed-on: https://pdfium-review.googlesource.com/31654
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- core/fxcodec/gif/cfx_gif.h
+++ core/fxcodec/gif/cfx_gif.h
@@ -36,74 +36,78 @@ extern const char kGifSignature89[];
 #define GIF_D_STATUS_IMG_DATA 0x0A
 
 #pragma pack(1)
-typedef struct {
+struct CFX_GifGlobalFlags {
   uint8_t pal_bits : 3;
   uint8_t sort_flag : 1;
   uint8_t color_resolution : 3;
   uint8_t global_pal : 1;
-} CFX_GifGlobalFlags;
+};
 
-typedef struct {
+struct CFX_GifLocalFlags {
   uint8_t pal_bits : 3;
   uint8_t reserved : 2;
   uint8_t sort_flag : 1;
   uint8_t interlace : 1;
   uint8_t local_pal : 1;
-} CFX_GifLocalFlags;
+};
 
-typedef struct { char signature[6]; } CFX_GifHeader;
+struct CFX_GifHeader {
+  char signature[6];
+};
 
-typedef struct {
+struct CFX_GifLocalScreenDescriptor {
   uint16_t width;
   uint16_t height;
   CFX_GifGlobalFlags global_flags;
   uint8_t bc_index;
   uint8_t pixel_aspect;
-} CFX_GifLocalScreenDescriptor;
+};
 
-typedef struct {
+struct CFX_CFX_GifImageInfo {
   uint16_t left;
   uint16_t top;
   uint16_t width;
   uint16_t height;
   CFX_GifLocalFlags local_flags;
-} CFX_CFX_GifImageInfo;
+};
 
-typedef struct {
+struct CFX_GifControlExtensionFlags {
   uint8_t transparency : 1;
   uint8_t user_input : 1;
   uint8_t disposal_method : 3;
   uint8_t reserved : 3;
-} CFX_GifControlExtensionFlags;
+};
 
-typedef struct {
+struct CFX_GifGraphicControlExtension {
   uint8_t block_size;
   CFX_GifControlExtensionFlags gce_flags;
   uint16_t delay_time;
   uint8_t trans_index;
-} CFX_GifGraphicControlExtension;
+};
 
-typedef struct {
+struct CFX_GifPlainTextExtension {
   uint8_t block_size;
   uint16_t grid_left;
   uint16_t grid_top;
   uint16_t grid_width;
   uint16_t grid_height;
-
   uint8_t char_width;
   uint8_t char_height;
-
   uint8_t fc_index;
   uint8_t bc_index;
-} CFX_GifPlainTextExtension;
+};
 
-typedef struct {
+struct GifApplicationExtension {
   uint8_t block_size;
   uint8_t app_identify[8];
   uint8_t app_authentication[3];
-} GifApplicationExtension;
+};
 
-typedef struct { uint8_t r, g, b; } CFX_GifPalette;
+struct CFX_GifPalette {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+};
 #pragma pack()
 
 enum class CFX_GifDecodeStatus {
