d29141627b461eaf3507eaf833184802394a5a69
Henrique Nakashima <hnakashima@chromium.org>
2018-05-11 20:47:48 +0000

    Reland "Filter out "Type" and "DecodeParms" from trailer keys."
    This reverts commit 466bd4f4e0cb2bc2a7b3626948609268c52c4690.
    
    Reason for revert: On closer inspection, it seems the test is not
    flaky, and the root cause of the failure is an SEH exception,
    which is the true source of flakiness. Other tests are also affected.
    
    Original change's description:
    > Revert "Filter out "Type" and "DecodeParms" from trailer keys."
    >
    > This reverts commit 5f7c5be8b7072d46e8d8088a1ec14370ecfad44c.
    >
    > Reason for revert: New test is flaky, ID is not stable.
    >
    > Original change's description:
    > > Filter out "Type" and "DecodeParms" from trailer keys.
    > >
    > > Bug: pdfium:873
    > > Change-Id: I12ae5b8776f5a73c4be81bed53ada05c94d46882
    > > Reviewed-on: https://pdfium-review.googlesource.com/32190
    > > Reviewed-by: dsinclair <dsinclair@chromium.org>
    > > Commit-Queue: dsinclair <dsinclair@chromium.org>
    >
    > TBR=dsinclair@chromium.org,hnakashima@chromium.org
    >
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Bug: pdfium:873
    > Change-Id: I642771e12c61181c7b81681dae7b2f5549c14b18
    > Reviewed-on: https://pdfium-review.googlesource.com/32430
    > Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    > Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    
    TBR=dsinclair@chromium.org,hnakashima@chromium.org
    
    Change-Id: I2552729610c9f8adf02c70a2a43e2383ceda19b5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: pdfium:873
    Reviewed-on: https://pdfium-review.googlesource.com/32397
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>

--- core/fpdfapi/edit/cpdf_creator.cpp
+++ core/fpdfapi/edit/cpdf_creator.cpp
@@ -627,7 +627,8 @@ int32_t CPDF_Creator::WriteDoc_Stage4() {
       CPDF_Object* pValue = it.second.get();
       if (key == "Encrypt" || key == "Size" || key == "Filter" ||
           key == "Index" || key == "Length" || key == "Prev" || key == "W" ||
-          key == "XRefStm" || key == "ID") {
+          key == "XRefStm" || key == "ID" || key == "DecodeParms" ||
+          key == "Type") {
         continue;
       }
       if (!m_Archive->WriteString(("/")) ||
