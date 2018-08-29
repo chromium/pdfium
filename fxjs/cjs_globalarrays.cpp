b2b1f55214688d5e3bfd5015e0a4829d2f314c27
Lei Zhang <thestig@chromium.org>
2018-08-29 19:24:39 +0000

    Stop using deprecated V8 APIs in CJS_GlobalArrays.
    Change-Id: I7ed3037d8d8862787ad7364eb5f55cd22eb879ad
    Reviewed-on: https://pdfium-review.googlesource.com/41357
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>

--- fxjs/cjs_globalarrays.cpp
+++ fxjs/cjs_globalarrays.cpp
@@ -8,10 +8,11 @@
 
 #define GLOBAL_ARRAY(rt, name, ...)                                          \
   {                                                                          \
-    const wchar_t* values[] = {__VA_ARGS__};                                 \
+    static const wchar_t* const values[] = {__VA_ARGS__};                    \
     v8::Local<v8::Array> array = (rt)->NewArray();                           \
+    v8::Local<v8::Context> ctx = (rt)->GetIsolate()->GetCurrentContext();    \
     for (size_t i = 0; i < FX_ArraySize(values); ++i)                        \
-      array->Set(i, (rt)->NewString(values[i]));                             \
+      array->Set(ctx, i, (rt)->NewString(values[i])).FromJust();             \
     (rt)->SetConstArray((name), array);                                      \
     (rt)->DefineGlobalConst(                                                 \
         (name), [](const v8::FunctionCallbackInfo<v8::Value>& info) {        \
