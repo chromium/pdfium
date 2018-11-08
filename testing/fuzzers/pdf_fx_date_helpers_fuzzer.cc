e5e0aea5a4043debe4786fcb63666fc1f5143612
Tom Sepez <tsepez@chromium.org>
2018-11-08 00:28:16 +0000

    Add pdf_fx_date_helpers_fuzzer target.
    Change-Id: Ic7191267628ce765b26a39debeb87d175d0a69fc
    Reviewed-on: https://pdfium-review.googlesource.com/c/45131
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

---
+++ testing/fuzzers/pdf_fx_date_helpers_fuzzer.cc
@@ -0,0 +1,23 @@
+// Copyright 2018 The PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "core/fxcrt/widestring.h"
+#include "fxjs/fx_date_helpers.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  auto* short_data = reinterpret_cast<const unsigned short*>(data);
+  size_t short_size = size / sizeof(unsigned short);
+  if (short_size > 2) {
+    double ignore;
+    size_t short_len1 = short_size / 2;
+    size_t short_len2 = short_size - short_len1;
+    WideString input1 = WideString::FromUTF16LE(short_data, short_len1);
+    WideString input2 =
+        WideString::FromUTF16LE(short_data + short_len1, short_len2);
+    FX_ParseDateUsingFormat(input1, input2, &ignore);
+  }
+  return 0;
+}
