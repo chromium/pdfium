4be774133f9448693d1a7c285c3258c5dadd3a07
Tom Sepez <tsepez@chromium.org>
2018-11-30 22:22:01 +0000

    Remove wide literals from xfa_basic_data_element_script.cpp
    These aren't present in the binary anyways since they are excluded
    by the use of the ATTR() macro, but we might as well use the
    narrow versions to avoid having to ponder converting them later.
    
    Change-Id: Ibd6dc728c3629240057f060a45753ca0fee6f8af
    Reviewed-on: https://pdfium-review.googlesource.com/c/46172
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- fxjs/xfa/cjx_object.h
+++ fxjs/xfa/cjx_object.h
@@ -290,7 +290,7 @@ typedef void (CJX_Object::*XFA_ATTRIBUTE_CALLBACK)(CFXJSE_Value* pValue,
                                                    XFA_Attribute eAttribute);
 
 struct XFA_SCRIPTATTRIBUTEINFO {
-  uint32_t uHash;
+  uint32_t uHash;  // Hashed as wide string.
   XFA_Attribute attribute;
   XFA_ScriptType eValueType;
   XFA_ATTRIBUTE_CALLBACK callback;
