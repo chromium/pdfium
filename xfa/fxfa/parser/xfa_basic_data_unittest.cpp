407a081da693b7ed877e85be24c3eac9fe831315
Tom Sepez <tsepez@chromium.org>
2018-12-12 00:56:32 +0000

    Remove fragile script index mechanism.
    Replace a index lookup and linear scan with a binary search
    over a compound key. Should be able to add/delete elements
    easily after this last step.
    
    Move element-attribute information to .inc file. Move the corresponding
    element name out of a comment and into the macro itself so that we may
    use it as part of that key.
    
    
    Change-Id: Ib97a7f54c167c3f3323c35a51b336d00f347a689
    Reviewed-on: https://pdfium-review.googlesource.com/c/47052
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>

--- xfa/fxfa/parser/xfa_basic_data_unittest.cpp
+++ xfa/fxfa/parser/xfa_basic_data_unittest.cpp
@@ -4,7 +4,22 @@
 
 #include "xfa/fxfa/parser/xfa_basic_data.h"
 
+#include <utility>
+
 #include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+void DoElementAttrTestCase(XFA_Element elem,
+                           XFA_Attribute attr,
+                           std::pair<XFA_Element, XFA_Attribute>* so_far) {
+  auto curr = std::make_pair(elem, attr);
+  EXPECT_LE(*so_far, curr) << " for " << static_cast<int>(elem) << ", "
+                           << static_cast<int>(attr);
+  *so_far = curr;
+}
+
+}  // namespace
 
 TEST(XFABasicDataTest, GetAttributeByName) {
   Optional<XFA_ATTRIBUTEINFO> result = XFA_GetAttributeByName(L"");
@@ -44,4 +59,14 @@ TEST(XFABasicDataTest, GetAttributeValueByName) {
   result = XFA_GetAttributeValueByName(L"lowered");
   ASSERT_TRUE(result.has_value());
   EXPECT_EQ(XFA_AttributeValue::Lowered, result.value());
+}
+
+// TODO(tsepez): convert EXPECT_LE to EXPECT_LT after removing duplicate rows.
+TEST(XFABasicDataText, ElementAttributeOrder) {
+  std::pair<XFA_Element, XFA_Attribute> so_far = {};
+#undef ELEM_ATTR____
+#define ELEM_ATTR____(a, b, c) \
+  DoElementAttrTestCase(XFA_Element::a, XFA_Attribute::b, &so_far);
+#include "xfa/fxfa/parser/element_attributes.inc"
+#undef ELEM_ATTR____
 }
