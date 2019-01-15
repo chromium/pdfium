06a67e4489a685553dafe069e032cfa438812e51
Lei Zhang <thestig@chromium.org>
2019-01-15 20:17:02 +0000

    Simplify CPVT_FloatRect constructors.
    Change-Id: I2b1aaefbb0c49f9468bf01a07cea8e9679b82492
    Reviewed-on: https://pdfium-review.googlesource.com/c/48290
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

--- core/fpdfdoc/cpvt_floatrect.h
+++ core/fpdfdoc/cpvt_floatrect.h
@@ -11,24 +11,16 @@
 
 class CPVT_FloatRect final : public CFX_FloatRect {
  public:
-  CPVT_FloatRect() { left = top = right = bottom = 0.0f; }
+  CPVT_FloatRect() = default;
 
   CPVT_FloatRect(float other_left,
                  float other_top,
                  float other_right,
-                 float other_bottom) {
-    left = other_left;
-    top = other_top;
-    right = other_right;
-    bottom = other_bottom;
-  }
+                 float other_bottom)
+      : CFX_FloatRect(other_left, other_bottom, other_right, other_top) {}
 
-  explicit CPVT_FloatRect(const CFX_FloatRect& rect) {
-    left = rect.left;
-    top = rect.top;
-    right = rect.right;
-    bottom = rect.bottom;
-  }
+  explicit CPVT_FloatRect(const CFX_FloatRect& rect)
+      : CFX_FloatRect(rect.left, rect.bottom, rect.right, rect.top) {}
 
   float Height() const {
     if (top > bottom)
