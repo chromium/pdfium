b440777fde3300145493ca117c556409db7261c0
Lei Zhang <thestig@chromium.org>
2019-01-16 19:30:47 +0000

    Limit pdf_cmap_fuzzer input to 256 KB.
    Larger inputs are unlikely to do anything interesting with
    CPDF_CMap::LoadEmbedded().
    
    BUG=chromium:921373
    
    Change-Id: I51adc25a316d5d8828418e3325bd37be3ec71395
    Reviewed-on: https://pdfium-review.googlesource.com/c/48350
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

--- testing/fuzzers/pdf_cmap_fuzzer.cc
+++ testing/fuzzers/pdf_cmap_fuzzer.cc
@@ -8,6 +8,9 @@
 #include "third_party/base/span.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  if (size > 256 * 1024)
+    return 0;
+
   pdfium::MakeRetain<CPDF_CMap>()->LoadEmbedded(pdfium::make_span(data, size));
   return 0;
 }
