f421410912993be4dffe3149653a5ffa0a7eff9f
Lei Zhang <thestig@chromium.org>
2019-03-21 17:10:58 +0000

    Combine code in CFXJSE_FormCalcContext::GenerateSomExpression().
    There exists an if-else branch where the two branches are very similar.
    
    Change-Id: Ic6c6ed1120dca2b9bdd9b17ea371b8f5f8ac5243
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/52050
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>

--- fxjs/xfa/cfxjse_formcalc_context.cpp
+++ fxjs/xfa/cfxjse_formcalc_context.cpp
@@ -5412,18 +5412,16 @@ ByteString CFXJSE_FormCalcContext::GenerateSomExpression(ByteStringView bsName,
     return ByteString(bsName, "[") + ByteString::FormatInteger(iIndexValue) +
            "]";
   }
-  ByteString bsSomExp;
-  if (iIndexFlags == 2) {
-    bsSomExp = (iIndexValue < 0) ? (bsName + "[-") : (bsName + "[+");
-    iIndexValue = (iIndexValue < 0) ? (0 - iIndexValue) : iIndexValue;
-    bsSomExp += ByteString::FormatInteger(iIndexValue);
-    bsSomExp += "]";
-  } else {
-    bsSomExp = (iIndexValue < 0) ? (bsName + "[") : (bsName + "[-");
-    iIndexValue = (iIndexValue < 0) ? (0 - iIndexValue) : iIndexValue;
-    bsSomExp += ByteString::FormatInteger(iIndexValue);
-    bsSomExp += "]";
-  }
+
+  const bool bNegative = iIndexValue < 0;
+  ByteString bsSomExp(bsName);
+  if (iIndexFlags == 2)
+    bsSomExp += bNegative ? "[-" : "[+";
+  else
+    bsSomExp += bNegative ? "[" : "[-";
+  iIndexValue = bNegative ? 0 - iIndexValue : iIndexValue;
+  bsSomExp += ByteString::FormatInteger(iIndexValue);
+  bsSomExp += "]";
   return bsSomExp;
 }
 
