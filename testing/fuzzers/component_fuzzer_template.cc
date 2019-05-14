0db20e0a220b9ce2ba7d6bee980ec1bcd7a667e7
Tom Anderson <thomasanderson@chromium.org>
2019-05-14 22:38:47 +0000

    Fix ODR violations in public fuzzers
    In 93101066, I assumed a public_fuzzer didn't depend on private PDFium
    code, however this is not true.  Some public_fuzzer's have direct
    dependencies on PDFium internals.  So in component builds, they will
    both statically link in parts of PDFium, and dynamically link in (the
    same code from) libpdfium.so. The fix is to export the public fuzzers
    from libpdfium.so too.
    
    BUG=960288
    R=thestig
    
    Change-Id: I10d03c46c72b454760e5664c4939ab173557cb3b
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/54250
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>

--- testing/fuzzers/component_fuzzer_template.cc
+++ testing/fuzzers/component_fuzzer_template.cc
@@ -5,17 +5,14 @@
 #include <cstddef>
 #include <cstdint>
 
+#include "public/fpdfview.h"
+
 // This template is used in component builds to forward to the real fuzzers
-// which are exported from the PDFium shared library.  FUZZER_IMPL is a macro
-// defined at build time that contains the name of the real fuzzer.
+// which are exported from the PDFium shared library.
 
-#if defined(WIN32)
-#define IMPORT __declspec(dllimport)
-#else
-#define IMPORT
-#endif
-
-extern "C" IMPORT int FUZZER_IMPL(const uint8_t* data, size_t size);
+// FUZZER_IMPL is a macro defined at build time that contains the name of the
+// real fuzzer.
+extern "C" FPDF_EXPORT int FUZZER_IMPL(const uint8_t* data, size_t size);
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   return FUZZER_IMPL(data, size);
