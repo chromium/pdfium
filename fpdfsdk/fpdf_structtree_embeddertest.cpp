0a79453af68f705c02c02040f2ea8ffe3d5ec7ee
Hui Yingst <nigi@chromium.org>
2019-05-15 23:18:31 +0000

    Modified embeddertests for FPDF_StructElement.
    - Added title "TitleText" to test file tagged_alt_text.in and built
      matching binary file tagged_alt_text.pdf.
    - Added embeddertest for FPDF_StructElement_GetTitle().
    - Added checks for nullptr parameters for the test of
      FPDF_StructElement_GetType().
    
    BUG=pdfium:1242
    
    Change-Id: I993bf40c3dff3773bafb546887d24661624fac0a
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/54492
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

--- fpdfsdk/fpdf_structtree_embeddertest.cpp
+++ fpdfsdk/fpdf_structtree_embeddertest.cpp
@@ -107,7 +107,12 @@ TEST_F(FPDFStructTreeEmbedderTest, GetType) {
         FPDF_StructTree_GetChildAtIndex(struct_tree.get(), 0);
     ASSERT_NE(nullptr, element);
 
+    // test nullptr inputs
     unsigned short buffer[12];
+    ASSERT_EQ(0U, FPDF_StructElement_GetType(nullptr, buffer, sizeof(buffer)));
+    ASSERT_EQ(0U, FPDF_StructElement_GetType(nullptr, nullptr, 0));
+    ASSERT_EQ(18U, FPDF_StructElement_GetType(element, nullptr, 0));
+
     memset(buffer, 0, sizeof(buffer));
     // Deliberately pass in a small buffer size to make sure |buffer| remains
     // untouched.
@@ -117,6 +122,44 @@ TEST_F(FPDFStructTreeEmbedderTest, GetType) {
 
     ASSERT_EQ(18U, FPDF_StructElement_GetType(element, buffer, sizeof(buffer)));
     const wchar_t kExpected[] = L"Document";
+    EXPECT_EQ(WideString(kExpected),
+              WideString::FromUTF16LE(buffer, FXSYS_len(kExpected)));
+  }
+
+  UnloadPage(page);
+}
+
+TEST_F(FPDFStructTreeEmbedderTest, GetTitle) {
+  ASSERT_TRUE(OpenDocument("tagged_alt_text.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  {
+    ScopedFPDFStructTree struct_tree(FPDF_StructTree_GetForPage(page));
+    ASSERT_TRUE(struct_tree);
+    ASSERT_EQ(1, FPDF_StructTree_CountChildren(struct_tree.get()));
+
+    FPDF_STRUCTELEMENT element =
+        FPDF_StructTree_GetChildAtIndex(struct_tree.get(), 0);
+    ASSERT_NE(nullptr, element);
+
+    // test nullptr inputs
+    unsigned short buffer[12];
+    ASSERT_EQ(0U, FPDF_StructElement_GetTitle(nullptr, buffer, sizeof(buffer)));
+    ASSERT_EQ(0U, FPDF_StructElement_GetTitle(nullptr, nullptr, 0));
+    ASSERT_EQ(20U, FPDF_StructElement_GetTitle(element, nullptr, 0));
+
+    memset(buffer, 0, sizeof(buffer));
+    // Deliberately pass in a small buffer size to make sure |buffer| remains
+    // untouched.
+    ASSERT_EQ(20U, FPDF_StructElement_GetTitle(element, buffer, 1));
+    for (size_t i = 0; i < FX_ArraySize(buffer); ++i)
+      EXPECT_EQ(0U, buffer[i]);
+
+    ASSERT_EQ(20U,
+              FPDF_StructElement_GetTitle(element, buffer, sizeof(buffer)));
+
+    const wchar_t kExpected[] = L"TitleText";
     EXPECT_EQ(WideString(kExpected),
               WideString::FromUTF16LE(buffer, FXSYS_len(kExpected)));
   }
