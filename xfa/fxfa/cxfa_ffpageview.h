032ebd62b2ac4085763736c4a3a533380697eced
asweintraub <asweintraub@google.com>
2019-06-06 23:00:37 +0000

    Fix copyright notices.
    PiperOrigin-RevId: 251636410
    Change-Id: Id306cbef2a94af56f3550351c1b97c95d485888e
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/55730
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

--- xfa/fxfa/cxfa_ffpageview.h
+++ xfa/fxfa/cxfa_ffpageview.h
@@ -1,4 +1,4 @@
-// Copyrig 2014 PDFium Authors. All rights reserved.
+// Copyright 2014 PDFium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
