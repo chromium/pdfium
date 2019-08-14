af235d500348bd5257f82a96cb50bb430bd1af3f
Tom Sepez <tsepez@chromium.org>
2019-08-14 19:47:15 +0000

    Move FXFM_ENC_TAG() macro into sole .cpp file using it.
    Avoids a PDF_ENABLE_XFA since it now is entirely within an
    XFA-only file.
    
    Change-Id: I5cff7e6145a3a7947ff34e63b2421476d419296b
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/59290
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- core/fxge/cfx_unicodeencoding.h
+++ core/fxge/cfx_unicodeencoding.h
@@ -11,25 +11,6 @@
 
 #include "core/fxcrt/unowned_ptr.h"
 
-#ifdef PDF_ENABLE_XFA
-#define FXFM_ENC_TAG(a, b, c, d)                                          \
-  (((uint32_t)(a) << 24) | ((uint32_t)(b) << 16) | ((uint32_t)(c) << 8) | \
-   (uint32_t)(d))
-#define FXFM_ENCODING_MS_SYMBOL FXFM_ENC_TAG('s', 'y', 'm', 'b')
-#define FXFM_ENCODING_UNICODE FXFM_ENC_TAG('u', 'n', 'i', 'c')
-#define FXFM_ENCODING_MS_SJIS FXFM_ENC_TAG('s', 'j', 'i', 's')
-#define FXFM_ENCODING_MS_GB2312 FXFM_ENC_TAG('g', 'b', ' ', ' ')
-#define FXFM_ENCODING_MS_BIG5 FXFM_ENC_TAG('b', 'i', 'g', '5')
-#define FXFM_ENCODING_MS_WANSUNG FXFM_ENC_TAG('w', 'a', 'n', 's')
-#define FXFM_ENCODING_MS_JOHAB FXFM_ENC_TAG('j', 'o', 'h', 'a')
-#define FXFM_ENCODING_ADOBE_STANDARD FXFM_ENC_TAG('A', 'D', 'O', 'B')
-#define FXFM_ENCODING_ADOBE_EXPERT FXFM_ENC_TAG('A', 'D', 'B', 'E')
-#define FXFM_ENCODING_ADOBE_CUSTOM FXFM_ENC_TAG('A', 'D', 'B', 'C')
-#define FXFM_ENCODING_ADOBE_LATIN_1 FXFM_ENC_TAG('l', 'a', 't', '1')
-#define FXFM_ENCODING_OLD_LATIN_2 FXFM_ENC_TAG('l', 'a', 't', '2')
-#define FXFM_ENCODING_APPLE_ROMAN FXFM_ENC_TAG('a', 'r', 'm', 'n')
-#endif  // PDF_ENABLE_XFA
-
 class CFX_Font;
 
 class CFX_UnicodeEncoding {
