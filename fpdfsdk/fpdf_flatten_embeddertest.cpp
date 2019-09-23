310ddbffb0674d34a393de1c795d5f9b72e15a81
Lei Zhang <thestig@chromium.org>
2019-09-23 12:50:04 +0000

    Add a test for a checkbox that does not print.
    When flattened, the button annotation in bug_861842.pdf, which looks
    like a checkbox, disappears. Add an embedder test to illustrate this
    problem.
    
    Bug: chromium:861842
    Change-Id: Ief691438f4c1cc150586e5f51999d83b1cbebe51
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/59931
    Commit-Queue: dsinclair <dsinclair@chromium.org>
    Reviewed-by: dsinclair <dsinclair@chromium.org>

--- fpdfsdk/fpdf_flatten_embeddertest.cpp
+++ fpdfsdk/fpdf_flatten_embeddertest.cpp
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
 #include "public/fpdf_flatten.h"
 #include "public/fpdfview.h"
 #include "testing/embedder_test.h"
@@ -35,6 +36,38 @@ TEST_F(FPDFFlattenEmbedderTest, FlatPrint) {
   EXPECT_TRUE(page);
   EXPECT_EQ(FLATTEN_SUCCESS, FPDFPage_Flatten(page, FLAT_PRINT));
   UnloadPage(page);
+}
+
+// TODO(crbug.com/pdfium/11): Fix this test and enable.
+#if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
+#define MAYBE_BUG_861842 DISABLED_BUG_861842
+#else
+#define MAYBE_BUG_861842 BUG_861842
+#endif
+TEST_F(FPDFFlattenEmbedderTest, MAYBE_BUG_861842) {
+#if defined(OS_WIN)
+  static const char kCheckboxHash[] = "95fba3cb7bce7e0d3c94279f60984e17";
+#elif defined(OS_MACOSX)
+  static const char kCheckboxHash[] = "0a1d1d63d4452bc26a1c5c547d309655";
+#else
+  static const char kCheckboxHash[] = "594265790b81df2d93120d33b72a6ada";
+#endif
+
+  EXPECT_TRUE(OpenDocument("bug_861842.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  ScopedFPDFBitmap bitmap = RenderLoadedPageWithFlags(page, FPDF_ANNOT);
+  CompareBitmap(bitmap.get(), 100, 120, kCheckboxHash);
+
+  EXPECT_EQ(FLATTEN_SUCCESS, FPDFPage_Flatten(page, FLAT_PRINT));
+  EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
+
+  UnloadPage(page);
+
+  // TODO(crbug.com/861842): This should not render blank.
+  static const char kBlankPageHash[] = "48400809c3862dae64b0cd00d51057a4";
+  VerifySavedDocument(100, 120, kBlankPageHash);
 }
 
 // TODO(crbug.com/pdfium/11): Fix this test and enable.
