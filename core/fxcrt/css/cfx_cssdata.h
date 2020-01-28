94294b48c239efaeb08c825da07525c520cdc5e0
Tom Sepez <tsepez@chromium.org>
2020-01-28 21:36:36 +0000

    Avoid including string_view_template.h directly.
    Prefer fx_string.h, since the use of the template is essentially
    an implementation detail that can be hidden behind it.
    
    Change-Id: I124146605d48243e2379a94f611386c346be6566
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/65670
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- core/fxcrt/css/cfx_cssdata.h
+++ core/fxcrt/css/cfx_cssdata.h
@@ -10,7 +10,7 @@
 #include "core/fxcrt/css/cfx_css.h"
 #include "core/fxcrt/css/cfx_cssnumbervalue.h"
 #include "core/fxcrt/css/cfx_cssvalue.h"
-#include "core/fxcrt/string_view_template.h"
+#include "core/fxcrt/fx_string.h"
 #include "core/fxge/fx_dib.h"
 
 class CFX_CSSData {
