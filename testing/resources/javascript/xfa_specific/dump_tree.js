5c267d021cb328aed00c23ad0b496f91267125b5
Tom Sepez <tsepez@chromium.org>
2020-02-04 21:25:45 +0000

    Add dumpTree() testing helper JS function.
    Attributes aren't directly exposed as JS properties on the object,
    so specific methods are required to navigate the tree.
    
    Change-Id: I1748efec6c77069b10c83b91ec2472c1d5bfe507
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/65950
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

---
+++ testing/resources/javascript/xfa_specific/dump_tree.js
@@ -0,0 +1,20 @@
+// Copyright 2020 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Note: Be sure that this file is only included inside a CDATA block,
+// otherwise the less-than comparision below will break the XML parse.
+
+function dumpTree(node, level) {
+  level = level || 0;
+  var indentation = "| ".repeat(level);
+  try {
+    app.alert(indentation + node.className);
+    var children = node.nodes;
+    for (var i = 0; i < children.length; ++i) {
+      dumpTree(children.item(i), level + 1);
+    }
+  } catch (e) {
+    app.alert(indentation + "Error: " + e);
+  }
+}
