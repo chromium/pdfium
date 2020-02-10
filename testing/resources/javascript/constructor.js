72f2915aa3b45634c3a287d2cece5633c98a2920
Tom Sepez <tsepez@chromium.org>
2020-02-10 23:43:18 +0000

    Add JS test for constructing FXJSOBJTYPE_DYNAMIC objects.
    These won't be full-fledged objects since they lack C++ bindings
    (as tested in icons.in), but they shouldn't allow their constructor
    to be invoked as a plain function.
    
    Change-Id: I9be1eddfc6dc4eb2d836670c0eb3fd095c7bde8f
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/66171
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- testing/resources/javascript/constructor.js
+++ testing/resources/javascript/constructor.js
@@ -2,9 +2,32 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+function testLegalConstructor(name, allowed) {
+  const constructorString = name + ".constructor";
+  var constructor;
+  try {
+    constructor = eval(constructorString);
+  } catch (e) {
+    app.alert("FAIL: No such " + constructorString);
+    return;
+  }
+  try {
+    constructor();
+    app.alert("FAIL: " + constructorString + "(): returned");
+  } catch (e) {
+    app.alert("PASS: " + constructorString + "(): " + e);
+  }
+  try {
+    var thing = new constructor;
+    app.alert("PASS: new " + constructorString + ": " + thing);
+  } catch (e) {
+    app.alert("FAIL: new " + constructorString + ": " + e);
+  }
+}
+
 function testIllegalConstructor(name, allowed) {
   const constructorString = name + ".constructor";
-  let constructor;
+  var constructor;
   try {
     constructor = eval(constructorString);
   } catch (e) {
