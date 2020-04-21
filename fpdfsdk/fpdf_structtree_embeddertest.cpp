2c941a1c143ef7d4dfe96663de86f833e35b1258
Lei Zhang <thestig@chromium.org>
2020-04-21 19:20:15 +0000

    Add a FPDFStructTreeEmbedderTest for MCR and OBJR element types.
    Provide test coverage and illustrate issues with the existing StructTree
    code, where they cannot retrieve inline objects.
    
    The test PDF is derived from a HTML file with the following content:
    
    <body>
    <a name="top">
    <p>hello world</p>
    <p><a href="#top">link to top</a>
    </body>
    
    The HTML is converted to a tagged PDF using Chromium's headless mode
    with the following flags, with the assumption that Chromium's tagged PDF
    generator code works properly:
    
    --print-to-pdf --export-tagged-pdf --print-to-pdf-no-header
    
    The output PDF from Chromium is then minimized to produce the
    tagged_mcr_objr.in and tagged_mcr_objr.pdf.
    
    Bug: pdfium:672,pdfium:1297
    Change-Id: I0ba6e9a9a1bba57acf1ed052d4bec631e67d7677
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/68990
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>

--- fpdfsdk/fpdf_structtree_embeddertest.cpp
+++ fpdfsdk/fpdf_structtree_embeddertest.cpp
@@ -21,33 +21,33 @@ TEST_F(FPDFStructTreeEmbedderTest, GetAltText) {
 
     FPDF_STRUCTELEMENT element =
         FPDF_StructTree_GetChildAtIndex(struct_tree.get(), -1);
-    EXPECT_EQ(nullptr, element);
+    EXPECT_FALSE(element);
     element = FPDF_StructTree_GetChildAtIndex(struct_tree.get(), 1);
-    EXPECT_EQ(nullptr, element);
+    EXPECT_FALSE(element);
     element = FPDF_StructTree_GetChildAtIndex(struct_tree.get(), 0);
-    ASSERT_NE(nullptr, element);
+    ASSERT_TRUE(element);
     EXPECT_EQ(-1, FPDF_StructElement_GetMarkedContentID(element));
     EXPECT_EQ(0U, FPDF_StructElement_GetAltText(element, nullptr, 0));
 
     ASSERT_EQ(1, FPDF_StructElement_CountChildren(element));
     FPDF_STRUCTELEMENT child_element =
         FPDF_StructElement_GetChildAtIndex(element, -1);
-    EXPECT_EQ(nullptr, child_element);
+    EXPECT_FALSE(child_element);
     child_element = FPDF_StructElement_GetChildAtIndex(element, 1);
-    EXPECT_EQ(nullptr, child_element);
+    EXPECT_FALSE(child_element);
     child_element = FPDF_StructElement_GetChildAtIndex(element, 0);
-    ASSERT_NE(nullptr, child_element);
+    ASSERT_TRUE(child_element);
     EXPECT_EQ(-1, FPDF_StructElement_GetMarkedContentID(child_element));
     EXPECT_EQ(0U, FPDF_StructElement_GetAltText(child_element, nullptr, 0));
 
     ASSERT_EQ(1, FPDF_StructElement_CountChildren(child_element));
     FPDF_STRUCTELEMENT gchild_element =
         FPDF_StructElement_GetChildAtIndex(child_element, -1);
-    EXPECT_EQ(nullptr, gchild_element);
+    EXPECT_FALSE(gchild_element);
     gchild_element = FPDF_StructElement_GetChildAtIndex(child_element, 1);
-    EXPECT_EQ(nullptr, gchild_element);
+    EXPECT_FALSE(gchild_element);
     gchild_element = FPDF_StructElement_GetChildAtIndex(child_element, 0);
-    ASSERT_NE(nullptr, gchild_element);
+    ASSERT_TRUE(gchild_element);
     EXPECT_EQ(-1, FPDF_StructElement_GetMarkedContentID(gchild_element));
     ASSERT_EQ(24U, FPDF_StructElement_GetAltText(gchild_element, nullptr, 0));
 
@@ -69,7 +69,7 @@ TEST_F(FPDFStructTreeEmbedderTest, GetAltText) {
     ASSERT_EQ(1, FPDF_StructElement_CountChildren(gchild_element));
     FPDF_STRUCTELEMENT ggchild_element =
         FPDF_StructElement_GetChildAtIndex(gchild_element, 0);
-    EXPECT_EQ(nullptr, ggchild_element);
+    EXPECT_FALSE(ggchild_element);
   }
 
   UnloadPage(page);
@@ -105,7 +105,7 @@ TEST_F(FPDFStructTreeEmbedderTest, GetType) {
 
     FPDF_STRUCTELEMENT element =
         FPDF_StructTree_GetChildAtIndex(struct_tree.get(), 0);
-    ASSERT_NE(nullptr, element);
+    ASSERT_TRUE(element);
 
     // test nullptr inputs
     unsigned short buffer[12];
@@ -141,7 +141,7 @@ TEST_F(FPDFStructTreeEmbedderTest, GetTitle) {
 
     FPDF_STRUCTELEMENT element =
         FPDF_StructTree_GetChildAtIndex(struct_tree.get(), 0);
-    ASSERT_NE(nullptr, element);
+    ASSERT_TRUE(element);
 
     // test nullptr inputs
     unsigned short buffer[13];
@@ -166,7 +166,7 @@ TEST_F(FPDFStructTreeEmbedderTest, GetTitle) {
     ASSERT_EQ(1, FPDF_StructElement_CountChildren(element));
     FPDF_STRUCTELEMENT child_element =
         FPDF_StructElement_GetChildAtIndex(element, 0);
-    ASSERT_NE(nullptr, element);
+    ASSERT_TRUE(element);
 
     ASSERT_EQ(26U, FPDF_StructElement_GetTitle(child_element, buffer,
                                                sizeof(buffer)));
@@ -188,5 +188,111 @@ TEST_F(FPDFStructTreeEmbedderTest, GetStructTreeForNestedTaggedPDF) {
     ScopedFPDFStructTree struct_tree(FPDF_StructTree_GetForPage(page));
     ASSERT_TRUE(struct_tree);
   }
+  UnloadPage(page);
+}
+
+TEST_F(FPDFStructTreeEmbedderTest, MarkedContentReferenceAndObjectReference) {
+  ASSERT_TRUE(OpenDocument("tagged_mcr_objr.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  {
+    ScopedFPDFStructTree struct_tree(FPDF_StructTree_GetForPage(page));
+    ASSERT_TRUE(struct_tree);
+    ASSERT_EQ(1, FPDF_StructTree_CountChildren(struct_tree.get()));
+
+    FPDF_STRUCTELEMENT object8 =
+        FPDF_StructTree_GetChildAtIndex(struct_tree.get(), 0);
+    ASSERT_TRUE(object8);
+    unsigned short buffer[12];
+    ASSERT_EQ(18U, FPDF_StructElement_GetType(object8, buffer, sizeof(buffer)));
+    const wchar_t kExpectedObject8Type[] = L"Document";
+    EXPECT_EQ(WideString(kExpectedObject8Type),
+              WideString::FromUTF16LE(buffer, FXSYS_len(kExpectedObject8Type)));
+    EXPECT_EQ(-1, FPDF_StructElement_GetMarkedContentID(object8));
+    ASSERT_EQ(2, FPDF_StructElement_CountChildren(object8));
+
+    // First branch. 10 -> 12 -> 13 -> Inline dict.
+    FPDF_STRUCTELEMENT object10 =
+        FPDF_StructElement_GetChildAtIndex(object8, 0);
+    ASSERT_TRUE(object10);
+    ASSERT_EQ(20U,
+              FPDF_StructElement_GetType(object10, buffer, sizeof(buffer)));
+    const wchar_t kExpectedObject10Type[] = L"NonStruct";
+    EXPECT_EQ(
+        WideString(kExpectedObject10Type),
+        WideString::FromUTF16LE(buffer, FXSYS_len(kExpectedObject10Type)));
+    EXPECT_EQ(-1, FPDF_StructElement_GetMarkedContentID(object10));
+    ASSERT_EQ(1, FPDF_StructElement_CountChildren(object10));
+
+    FPDF_STRUCTELEMENT object12 =
+        FPDF_StructElement_GetChildAtIndex(object10, 0);
+    ASSERT_TRUE(object12);
+    ASSERT_EQ(4U, FPDF_StructElement_GetType(object12, buffer, sizeof(buffer)));
+    const wchar_t kExpectedObject12Type[] = L"P";
+    EXPECT_EQ(
+        WideString(kExpectedObject12Type),
+        WideString::FromUTF16LE(buffer, FXSYS_len(kExpectedObject12Type)));
+    EXPECT_EQ(-1, FPDF_StructElement_GetMarkedContentID(object12));
+    ASSERT_EQ(1, FPDF_StructElement_CountChildren(object12));
+
+    FPDF_STRUCTELEMENT object13 =
+        FPDF_StructElement_GetChildAtIndex(object12, 0);
+    ASSERT_TRUE(object13);
+    ASSERT_EQ(20U,
+              FPDF_StructElement_GetType(object13, buffer, sizeof(buffer)));
+    const wchar_t kExpectedObject13Type[] = L"NonStruct";
+    EXPECT_EQ(
+        WideString(kExpectedObject13Type),
+        WideString::FromUTF16LE(buffer, FXSYS_len(kExpectedObject13Type)));
+    EXPECT_EQ(-1, FPDF_StructElement_GetMarkedContentID(object13));
+    ASSERT_EQ(1, FPDF_StructElement_CountChildren(object13));
+
+    // TODO(crbug.com/pdfium/672): Fetch this child element.
+    EXPECT_FALSE(FPDF_StructElement_GetChildAtIndex(object13, 0));
+
+    // Second branch. 11 -> 14 -> Inline dict.
+    //                         -> 15 -> Inline dict.
+    FPDF_STRUCTELEMENT object11 =
+        FPDF_StructElement_GetChildAtIndex(object8, 1);
+    ASSERT_TRUE(object11);
+    ASSERT_EQ(4U, FPDF_StructElement_GetType(object11, buffer, sizeof(buffer)));
+    const wchar_t kExpectedObject11Type[] = L"P";
+    EXPECT_EQ(
+        WideString(kExpectedObject11Type),
+        WideString::FromUTF16LE(buffer, FXSYS_len(kExpectedObject11Type)));
+    EXPECT_EQ(-1, FPDF_StructElement_GetMarkedContentID(object11));
+    ASSERT_EQ(1, FPDF_StructElement_CountChildren(object11));
+
+    FPDF_STRUCTELEMENT object14 =
+        FPDF_StructElement_GetChildAtIndex(object11, 0);
+    ASSERT_TRUE(object14);
+    ASSERT_EQ(20U,
+              FPDF_StructElement_GetType(object14, buffer, sizeof(buffer)));
+    const wchar_t kExpectedObject14Type[] = L"NonStruct";
+    EXPECT_EQ(
+        WideString(kExpectedObject14Type),
+        WideString::FromUTF16LE(buffer, FXSYS_len(kExpectedObject14Type)));
+    EXPECT_EQ(-1, FPDF_StructElement_GetMarkedContentID(object14));
+    ASSERT_EQ(2, FPDF_StructElement_CountChildren(object14));
+
+    // TODO(crbug.com/pdfium/672): Object 15 should be at index 1.
+    EXPECT_FALSE(FPDF_StructElement_GetChildAtIndex(object14, 1));
+    FPDF_STRUCTELEMENT object15 =
+        FPDF_StructElement_GetChildAtIndex(object14, 0);
+    ASSERT_TRUE(object15);
+    ASSERT_EQ(20U,
+              FPDF_StructElement_GetType(object15, buffer, sizeof(buffer)));
+    const wchar_t kExpectedObject15Type[] = L"NonStruct";
+    EXPECT_EQ(
+        WideString(kExpectedObject15Type),
+        WideString::FromUTF16LE(buffer, FXSYS_len(kExpectedObject15Type)));
+    EXPECT_EQ(-1, FPDF_StructElement_GetMarkedContentID(object15));
+    ASSERT_EQ(1, FPDF_StructElement_CountChildren(object15));
+
+    // TODO(crbug.com/pdfium/672): Fetch this child element.
+    EXPECT_FALSE(FPDF_StructElement_GetChildAtIndex(object15, 0));
+  }
+
   UnloadPage(page);
 }
