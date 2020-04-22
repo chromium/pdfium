7f446cc3c79882af7860771d4a54c536cc75b74b
Hui Yingst <nigi@chromium.org>
2020-04-22 23:51:37 +0000

    Fix embedder test FPDFEditPageEmbedderTest.Rotation for Skia.
    With Skia/SkiaPaths enabled, Skia's own way of hairline handling
    caused the discrepancy comparing to AGG's expected results.
    Since this discrepancy is minor and expected to happen when
    rectangles are relatively small, this CL adds the expected results
    for Skia to make the embedder test pass.
    
    Bug: pdfium:1500
    Change-Id: I26fc8a866db9c0db767c7dea7815548cba1ec8ad
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/69111
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>

--- fpdfsdk/fpdf_editpage_embeddertest.cpp
+++ fpdfsdk/fpdf_editpage_embeddertest.cpp
@@ -8,15 +8,14 @@
 
 class FPDFEditPageEmbedderTest : public EmbedderTest {};
 
-// TODO(crbug.com/pdfium/11): Fix this test and enable.
+TEST_F(FPDFEditPageEmbedderTest, Rotation) {
 #if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
-#define MAYBE_Rotation DISABLED_Rotation
+  const char kOriginalMD5[] = "b4e411a6b5ffa59a50efede2efece597";
+  const char kRotatedMD5[] = "eded83f75f3d0332c584c416c571c0df";
 #else
-#define MAYBE_Rotation Rotation
-#endif
-TEST_F(FPDFEditPageEmbedderTest, MAYBE_Rotation) {
   const char kOriginalMD5[] = "0a90de37f52127619c3dfb642b5fa2fe";
   const char kRotatedMD5[] = "d599429574ff0dcad3bc898ea8b874ca";
+#endif
 
   {
     ASSERT_TRUE(OpenDocument("rectangles.pdf"));
