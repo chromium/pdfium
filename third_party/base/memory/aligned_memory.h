8fe986ac721dbc286e999276f95946dd1a0a0dd5
Lei Zhang <thestig@chromium.org>
2020-06-10 18:51:48 +0000

    Add third_party/base/memory/aligned_memory.h.
    Make AlignedAlloc() available for use in an upcoming CL to fix some
    memory alignment issues.
    
    Borrow Chromium's base/aligned_memory.h at crrev.com/773784, but without
    crrev.com/756778, which drags in some unneeded bits that introduce more
    Chromium dependencies. The other modifications are:
    
    - Wrap in pdfium namespace.
    - Update headers and header guards.
    - Change logging/checks to work with PDFium's version of base/logging.h.
    
    Bug: chromium:1086796
    Change-Id: I1cfe69b2f3ea4621fd7d02ede3aa24d7a8431954
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/70472
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

---
+++ third_party/base/memory/aligned_memory.h
@@ -0,0 +1,62 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BASE_MEMORY_ALIGNED_MEMORY_H_
+#define THIRD_PARTY_BASE_MEMORY_ALIGNED_MEMORY_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <type_traits>
+
+#include "build/build_config.h"
+#include "third_party/base/base_export.h"
+#include "third_party/base/compiler_specific.h"
+
+#if defined(COMPILER_MSVC)
+#include <malloc.h>
+#else
+#include <stdlib.h>
+#endif
+
+// A runtime sized aligned allocation can be created:
+//
+//   float* my_array = static_cast<float*>(AlignedAlloc(size, alignment));
+//
+//   // ... later, to release the memory:
+//   AlignedFree(my_array);
+//
+// Or using unique_ptr:
+//
+//   std::unique_ptr<float, AlignedFreeDeleter> my_array(
+//       static_cast<float*>(AlignedAlloc(size, alignment)));
+
+namespace pdfium {
+namespace base {
+
+// This can be replaced with std::aligned_alloc when we have C++17.
+// Caveat: std::aligned_alloc requires the size parameter be an integral
+// multiple of alignment.
+BASE_EXPORT void* AlignedAlloc(size_t size, size_t alignment);
+
+inline void AlignedFree(void* ptr) {
+#if defined(COMPILER_MSVC)
+  _aligned_free(ptr);
+#else
+  free(ptr);
+#endif
+}
+
+// Deleter for use with unique_ptr. E.g., use as
+//   std::unique_ptr<Foo, base::AlignedFreeDeleter> foo;
+struct AlignedFreeDeleter {
+  inline void operator()(void* ptr) const {
+    AlignedFree(ptr);
+  }
+};
+
+}  // namespace base
+}  // namespace pdfium
+
+#endif  // THIRD_PARTY_BASE_MEMORY_ALIGNED_MEMORY_H_
