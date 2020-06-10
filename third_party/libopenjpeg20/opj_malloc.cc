cff6cfc61523389959346693a0d3d46b05c7b8ed
Lei Zhang <thestig@chromium.org>
2020-06-10 18:55:18 +0000

    Reimplement third_party/libopenjpeg20/opj_malloc.h.
    Reimplement opj_malloc() and related memory (de)allocation functions
    used by OpenJPEG. Instead of being based on a bunch of if-defs based on
    platforms and compilers, standarize on using PartitionAlloc for normal
    allocations, and use third_party/base/memory/aligned_memory.h for all
    aligned allocations.
    
    This fixes a mismatch where the previous implementation assumed many
    64-bit POSIX platforms allocate 16-byte aligned memory, when OpenJPEG
    actually requires 32-byte alignment in some cases.
    
    Bug: chromium:1086796
    Change-Id: I45a3e56b32185285f0792325704c372e65ee200f
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/70473
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

---
+++ third_party/libopenjpeg20/opj_malloc.cc
@@ -0,0 +1,42 @@
+// Copyright 2020 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Deliberately not including opj_malloc.h, which has poisoned malloc and
+// friends.
+
+#include "core/fxcrt/fx_memory.h"
+#include "third_party/base/memory/aligned_memory.h"
+
+extern "C" {
+
+void* opj_malloc(size_t size) {
+  return FXMEM_DefaultAlloc(size);
+}
+
+void* opj_calloc(size_t numOfElements, size_t sizeOfElements) {
+  return FXMEM_DefaultCalloc(numOfElements, sizeOfElements);
+}
+
+void* opj_aligned_malloc(size_t size) {
+  return size ? pdfium::base::AlignedAlloc(size, 16) : nullptr;
+}
+
+void opj_aligned_free(void* ptr) {
+  pdfium::base::AlignedFree(ptr);
+}
+
+void* opj_aligned_32_malloc(size_t size) {
+  return size ? pdfium::base::AlignedAlloc(size, 32) : nullptr;
+}
+
+void* opj_realloc(void* m, size_t s) {
+  return FXMEM_DefaultRealloc(m, s);
+}
+
+void opj_free(void* m) {
+  if (m)
+    FXMEM_DefaultFree(m);
+}
+
+}  // extern "C"
