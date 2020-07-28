e4e6b60e55084a6e79bc7c0035d93ac5e77ed451
Hui Yingst <nigi@chromium.org>
2020-07-28 22:34:41 +0000

    Enable FPDFParserDecodeEmbedderTest.Bug_455199 for Skia/SkiaPaths.
    This CL enables FPDFParserDecodeEmbedderTest.Bug_455199 since it now
    passes for Skia/SkiaPaths.
    
    Bug: pdfium:1500
    Change-Id: Ie5a38787dfd2298fef7d13eb5d337698e46a1961
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/72051
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>

--- core/fpdfapi/parser/fpdf_parser_decode_embeddertest.cpp
+++ core/fpdfapi/parser/fpdf_parser_decode_embeddertest.cpp
@@ -39,13 +39,7 @@ TEST_F(FPDFParserDecodeEmbedderTest, Bug_555784) {
   UnloadPage(page);
 }
 
-// TODO(crbug.com/pdfium/11): Fix this test and enable.
-#if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
-#define MAYBE_Bug_455199 DISABLED_Bug_455199
-#else
-#define MAYBE_Bug_455199 Bug_455199
-#endif
-TEST_F(FPDFParserDecodeEmbedderTest, MAYBE_Bug_455199) {
+TEST_F(FPDFParserDecodeEmbedderTest, Bug_455199) {
   // Tests object numbers with a value > 01000000.
   // Should open successfully.
   ASSERT_TRUE(OpenDocument("bug_455199.pdf"));
