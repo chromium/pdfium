9d8e5ec1ac15edb77142be91c3d8fc7d36079b97
Hui Yingst <nigi@chromium.org>
2020-07-30 22:33:22 +0000

    Enable FPDFEditEmbedderTest.SetTextRenderMode for Skia/SkiaPaths.
    When Skia/SkiaPaths is enabled, rendering results for
    FPDFEditEmbedderTest.SetTextRenderMode have small discrepancies compared
    to AGG's expected results due to Skia using different anti-aliasing and
    font face rendering methods.
    
    This CL updates the Skia/SkiaPaths expected checksums for this test so
    that it can pass.
    
    Bug: pdfium:1500
    Change-Id: I39b818b2efe378ad214bff1b7bf434c368d4178c
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/72055
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>

--- fpdfsdk/fpdf_edit_embeddertest.cpp
+++ fpdfsdk/fpdf_edit_embeddertest.cpp
@@ -2136,22 +2136,20 @@ TEST_F(FPDFEditEmbedderTest, GetTextRenderMode) {
   UnloadPage(page);
 }
 
-// TODO(crbug.com/pdfium/11): Fix this test and enable.
+TEST_F(FPDFEditEmbedderTest, SetTextRenderMode) {
 #if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
-#define MAYBE_SetTextRenderMode DISABLED_SetTextRenderMode
+  const char kOriginalChecksum[] = "4960c720a6cdb7b2b16be809cce58aff";
+  const char kStrokeChecksum[] = "4c099cf9abc1565806bd7cd1cca5ac1a";
 #else
-#define MAYBE_SetTextRenderMode SetTextRenderMode
-#endif
-TEST_F(FPDFEditEmbedderTest, MAYBE_SetTextRenderMode) {
-// Platform specific original bitmaps
-#if defined(OS_APPLE)
-  const char md5[] = "2453a9a524ee3f5f525dd21345ec4d81";
-#elif defined(OS_WIN)
-  const char md5[] = "de6e86bad3e9fda753a8471a45cfbb58";
+#if defined(OS_WIN)
+  const char kOriginalChecksum[] = "de6e86bad3e9fda753a8471a45cfbb58";
+#elif defined(OS_APPLE)
+  const char kOriginalChecksum[] = "2453a9a524ee3f5f525dd21345ec4d81";
 #else
-  const char md5[] = "5a012d2920ac075c39ffa9437ea42faa";
+  const char kOriginalChecksum[] = "5a012d2920ac075c39ffa9437ea42faa";
 #endif
-  const char md5_stroke[] = "412e52e621b46bd77baf2162e1fb1a1d";
+  const char kStrokeChecksum[] = "412e52e621b46bd77baf2162e1fb1a1d";
+#endif  // defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
 
   {
     ASSERT_TRUE(OpenDocument("text_render_mode.pdf"));
@@ -2162,7 +2160,7 @@ TEST_F(FPDFEditEmbedderTest, MAYBE_SetTextRenderMode) {
     // Check the bitmap
     {
       ScopedFPDFBitmap page_bitmap = RenderPage(page);
-      CompareBitmap(page_bitmap.get(), 612, 446, md5);
+      CompareBitmap(page_bitmap.get(), 612, 446, kOriginalChecksum);
     }
 
     // Cannot set on a null object.
@@ -2188,7 +2186,7 @@ TEST_F(FPDFEditEmbedderTest, MAYBE_SetTextRenderMode) {
     // Check that bitmap displays changed content
     {
       ScopedFPDFBitmap page_bitmap = RenderPage(page);
-      CompareBitmap(page_bitmap.get(), 612, 446, md5_stroke);
+      CompareBitmap(page_bitmap.get(), 612, 446, kStrokeChecksum);
     }
 
     // Save a copy.
@@ -2211,7 +2209,7 @@ TEST_F(FPDFEditEmbedderTest, MAYBE_SetTextRenderMode) {
               FPDFTextObj_GetTextRenderMode(page_object));
 
     ScopedFPDFBitmap bitmap = RenderSavedPage(saved_page);
-    CompareBitmap(bitmap.get(), 612, 446, md5_stroke);
+    CompareBitmap(bitmap.get(), 612, 446, kStrokeChecksum);
 
     CloseSavedPage(saved_page);
     CloseSavedDocument();
