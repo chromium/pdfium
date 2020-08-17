5988d0f33501ab96e661377f204f11b36e58276a
Tom Sepez <tsepez@chromium.org>
2020-08-17 22:49:05 +0000

    No need to clear local variable in XFA_LoadImageFromBuffer().
    It's not aliased, and as a RetainPtr<>, will take care of everything
    in same way when it goes out of scope.
    
    Change-Id: Id0dc3729297548ef001cee70e31250988e752702
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/72850
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- xfa/fxfa/cxfa_ffwidget.cpp
+++ xfa/fxfa/cxfa_ffwidget.cpp
@@ -191,24 +191,18 @@ RetainPtr<CFX_DIBitmap> XFA_LoadImageFromBuffer(
   size_t nFrames;
   FXCODEC_STATUS status;
   std::tie(status, nFrames) = pProgressiveDecoder->GetFrames();
-  if (status != FXCODEC_STATUS_DECODE_READY || nFrames == 0) {
-    pBitmap = nullptr;
-    return pBitmap;
-  }
+  if (status != FXCODEC_STATUS_DECODE_READY || nFrames == 0)
+    return nullptr;
 
   status = pProgressiveDecoder->StartDecode(pBitmap, 0, 0, pBitmap->GetWidth(),
                                             pBitmap->GetHeight());
-  if (IsFXCodecErrorStatus(status)) {
-    pBitmap = nullptr;
-    return pBitmap;
-  }
+  if (IsFXCodecErrorStatus(status))
+    return nullptr;
 
   while (status == FXCODEC_STATUS_DECODE_TOBECONTINUE) {
     status = pProgressiveDecoder->ContinueDecode();
-    if (IsFXCodecErrorStatus(status)) {
-      pBitmap = nullptr;
-      return pBitmap;
-    }
+    if (IsFXCodecErrorStatus(status))
+      return nullptr;
   }
 
   return pBitmap;
