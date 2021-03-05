2b25a153590a1b1efb9d08314f6e896a3b8912df
Tom Sepez <tsepez@chromium.org>
2021-03-05 16:43:04 +0000

    Avoid signed overflow in PixarLogDecode().
    Casting an unsigned (uInt) to a signed (tmsize_t) can be perilous
    on 32-bit platforms.
    
    Bug: chromium:1184636
    Change-Id: Ie5171278573a26c6fd223319896e702dcd9510eb
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/78390
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- third_party/libtiff/tif_pixarlog.c
+++ third_party/libtiff/tif_pixarlog.c
@@ -799,7 +799,7 @@ PixarLogDecode(TIFF* tif, uint8* op, tmsize_t occ, uint16 s)
 		return (0);
 	}
 	/* Check that we will not fill more than what was allocated */
-	if ((tmsize_t)sp->stream.avail_out > sp->tbuf_size)
+        if (sp->tbuf_size < 0 || sp->stream.avail_out > (uInt) sp->tbuf_size)
 	{
 		TIFFErrorExt(tif->tif_clientdata, module, "sp->stream.avail_out > sp->tbuf_size");
 		return (0);
