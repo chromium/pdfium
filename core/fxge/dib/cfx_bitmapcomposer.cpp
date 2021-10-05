839d18189fe31cf4c9416d1a745ef06bd2257982
Tom Sepez <tsepez@chromium.org>
2021-10-05 05:14:42 +0000

    M95: Use more safe arithmetic in CFX_DIBBase
    Most of the calculations are "safe" because we know that the DIB
    has validated sizes before allocating a buffer, and that calculations
    in terms of bytes won't overflow and will be within the buffer. But
    calculations in terms of bits might create overflow in temporaries,
    so use safe arithmetic there instead.
    
    Re-arranging the order of operations thus converting to bytes first
    might be one option, but we want to handle the 1 bpp case.
    
    Test would require large images that might not be possible on
    all platforms.
    
    Bug: chromium:1253399
    Change-Id: I3c6c5b8b1f1bf3f429c7d377a8a84c5ab53cafd9
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/85510
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    (cherry picked from commit a8b293732a0160d1bc1d5b0ad5744922f0f820d5)
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/85670
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>

--- core/fxge/dib/cfx_bitmapcomposer.cpp
+++ core/fxge/dib/cfx_bitmapcomposer.cpp
@@ -9,6 +9,7 @@
 #include <string.h>
 
 #include "core/fxcrt/fx_coordinates.h"
+#include "core/fxcrt/fx_safe_types.h"
 #include "core/fxge/cfx_cliprgn.h"
 #include "core/fxge/dib/cfx_dibitmap.h"
 
@@ -113,9 +114,16 @@ void CFX_BitmapComposer::ComposeScanline(int line,
                 (m_DestLeft - m_pClipRgn->GetBox().left);
   }
   uint8_t* dest_scan = m_pBitmap->GetWritableScanline(line + m_DestTop);
-  if (dest_scan)
-    dest_scan += m_DestLeft * m_pBitmap->GetBPP() / 8;
+  if (dest_scan) {
+    FX_SAFE_UINT32 offset = m_DestLeft;
+    offset *= m_pBitmap->GetBPP();
+    offset /= 8;
+    if (!offset.IsValid())
+      return;
 
+    // Help some compilers perform pointer arithmetic against safe numerics.
+    dest_scan += static_cast<uint32_t>(offset.ValueOrDie());
+  }
   uint8_t* dest_alpha_scan =
       m_pBitmap->GetWritableAlphaMaskScanline(line + m_DestTop);
   if (dest_alpha_scan)
