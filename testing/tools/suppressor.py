b2ba62475873fe4500db5578458875d4daeba28f
Hui Yingst <nigi@chromium.org>
2021-10-18 16:30:08 +0000

    Enable suppressing test results depending on rendering options.
    Adds an extra column in testing/SUPPRESSIONS with possible values of
    "*", "agg", "skia" or "skiapaths" to indicate whether to suppress
    the result of a test file depending on the rendering options.
    
    Also modifies the test suppressor to check the Skia/SkiaPaths build
    configs against this new column's value while running tests.
    
    Bug: pdfium:11
    Change-Id: I007bfc6be9e497340b8004a84a408977f1a34beb
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/85970
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>

--- testing/tools/suppressor.py
+++ testing/tools/suppressor.py
@@ -16,6 +16,8 @@ class Suppressor:
   def __init__(self, finder, features, js_disabled, xfa_disabled):
     self.has_v8 = not js_disabled and 'V8' in features
     self.has_xfa = not js_disabled and not xfa_disabled and 'XFA' in features
+    self.has_skiapaths = 'SKIAPATHS' in features
+    self.has_skia = 'SKIA' in features
     self.suppression_set = self._LoadSuppressedSet('SUPPRESSIONS', finder)
     self.image_suppression_set = self._LoadSuppressedSet(
         'SUPPRESSIONS_IMAGE_DIFF', finder)
@@ -23,9 +25,12 @@ class Suppressor:
   def _LoadSuppressedSet(self, suppressions_filename, finder):
     v8_option = "v8" if self.has_v8 else "nov8"
     xfa_option = "xfa" if self.has_xfa else "noxfa"
+    rendering_option = "skia" if self.has_skia else (
+        "skiapaths" if self.has_skiapaths else "agg")
     with open(os.path.join(finder.TestingDir(), suppressions_filename)) as f:
       return set(
-          self._FilterSuppressions(common.os_name(), v8_option, xfa_option,
+          self._FilterSuppressions(common.os_name(), v8_option,
+                                   xfa_option, rendering_option,
                                    self._ExtractSuppressions(f)))
 
   def _ExtractSuppressions(self, f):
@@ -34,20 +39,24 @@ class Suppressor:
                                for x in f.readlines()] if y
     ]
 
-  def _FilterSuppressions(self, os_name, js, xfa, unfiltered_list):
+  def _FilterSuppressions(self, os_name, js, xfa, rendering_option,
+                          unfiltered_list):
     return [
         x[0]
         for x in unfiltered_list
-        if self._MatchSuppression(x, os_name, js, xfa)
+        if self._MatchSuppression(x, os_name, js, xfa, rendering_option)
     ]
 
-  def _MatchSuppression(self, item, os_name, js, xfa):
+  def _MatchSuppression(self, item, os_name, js, xfa, rendering_option):
     os_column = item[1].split(",")
     js_column = item[2].split(",")
     xfa_column = item[3].split(",")
+    rendering_option_column = item[4].split(",")
     return (('*' in os_column or os_name in os_column) and
             ('*' in js_column or js in js_column) and
-            ('*' in xfa_column or xfa in xfa_column))
+            ('*' in xfa_column or xfa in xfa_column) and
+            ('*' in rendering_option_column or
+             rendering_option in rendering_option_column))
 
   def IsResultSuppressed(self, input_filename):
     if input_filename in self.suppression_set:
