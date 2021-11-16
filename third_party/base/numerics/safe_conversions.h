15c1e6ae5cf2cd2ca02b0a530fdb85e106ab4781
Tom Sepez <tsepez@chromium.org>
2021-11-16 21:02:27 +0000

    Remove stream operators from safe_conversions.h and clamped_math.h
    Avoid dragging <ostream> into all modules using safe math.
    
    -- fix resulting IWYU.
    
    Change-Id: Id81b198ad1a60f0d8e81b5a7069dee76d7da9d1e
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/87130
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- third_party/base/numerics/safe_conversions.h
+++ third_party/base/numerics/safe_conversions.h
@@ -19,10 +19,6 @@
 #define BASE_HAS_OPTIMIZED_SAFE_CONVERSIONS (0)
 #endif
 
-#if !BASE_NUMERICS_DISABLE_OSTREAM_OPERATORS
-#include <ostream>
-#endif
-
 namespace pdfium {
 namespace base {
 namespace internal {
@@ -311,15 +307,6 @@ constexpr StrictNumeric<typename UnderlyingType<T>::type> MakeStrictNum(
     const T value) {
   return value;
 }
-
-#if !BASE_NUMERICS_DISABLE_OSTREAM_OPERATORS
-// Overload the ostream output operator to make logging work nicely.
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const StrictNumeric<T>& value) {
-  os << static_cast<T>(value);
-  return os;
-}
-#endif
 
 #define BASE_NUMERIC_COMPARISON_OPERATORS(CLASS, NAME, OP)              \
   template <typename L, typename R,                                     \
