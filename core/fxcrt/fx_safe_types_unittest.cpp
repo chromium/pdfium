6dfe8826b6a6c8b7f71a839a0ef56ae0163a3683
Tom Sepez <tsepez@chromium.org>
2022-02-28 20:17:37 +0000

    Add fx_safe_types_unitttest.cpp
    Change-Id: I8db2cf733fc0347b69e06752be66df68b47434c7
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/91090
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

---
+++ core/fxcrt/fx_safe_types_unittest.cpp
@@ -0,0 +1,22 @@
+// Copyright 2022 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <limits>
+
+#include "core/fxcrt/fx_safe_types.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+// PDFium relies on safe types handling the --2147483648 boundary
+// condition without overflow.
+TEST(FXSafeTypes, UnaryMinus) {
+  FX_SAFE_INT32 safe_val = std::numeric_limits<int32_t>::min();
+  EXPECT_TRUE(safe_val.IsValid());
+  EXPECT_FALSE((-safe_val).IsValid());
+}
+
+TEST(FXSafeTypes, SubtractFromZero) {
+  FX_SAFE_INT32 safe_val = std::numeric_limits<int32_t>::min();
+  EXPECT_TRUE(safe_val.IsValid());
+  EXPECT_FALSE((0 - safe_val).IsValid());
+}
