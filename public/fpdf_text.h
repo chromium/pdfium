ddc66f03da6051712e3d2dbc0ce22666aff13bed
Tom Sepez <tsepez@chromium.org>
2022-03-15 18:31:49 +0000

    Update FPDFText_CountRects() comments.
    Document current public behavior before updating to wider types
    internally in a subsequent CL.
    
    Change-Id: I0bf567536e4e6eced1f38b3f5f8c5a811cca7240
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/91490
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- public/fpdf_text.h
+++ public/fpdf_text.h
@@ -348,20 +348,22 @@ FPDF_EXPORT int FPDF_CALLCONV FPDFText_GetText(FPDF_TEXTPAGE text_page,
                                                unsigned short* result);
 
 // Function: FPDFText_CountRects
-//          Count number of rectangular areas occupied by a segment of texts.
+//          Counts number of rectangular areas occupied by a segment of text,
+//          and caches the result for subsequent FPDFText_GetRect() calls.
 // Parameters:
 //          text_page   -   Handle to a text page information structure.
 //                          Returned by FPDFText_LoadPage function.
-//          start_index -   Index for the start characters.
-//          count       -   Number of characters.
+//          start_index -   Index for the start character.
+//          count       -   Number of characters, or -1 for all remaining.
 // Return value:
-//          Number of rectangles. Zero for error.
+//          Number of rectangles, 0 if text_page is null, or -1 on bad
+//          start_index.
 // Comments:
 //          This function, along with FPDFText_GetRect can be used by
 //          applications to detect the position on the page for a text segment,
-//          so proper areas can be highlighted. FPDFTEXT will automatically
-//          merge small character boxes into bigger one if those characters
-//          are on the same line and use same font settings.
+//          so proper areas can be highlighted. The FPDFText_* functions will
+//          automatically merge small character boxes into bigger one if those
+//          characters are on the same line and use same font settings.
 //
 FPDF_EXPORT int FPDF_CALLCONV FPDFText_CountRects(FPDF_TEXTPAGE text_page,
                                                   int start_index,
