7fa683297a72500400b7f1a62ca6a12c8c220d34
Tom Sepez <tsepez@chromium.org>
2022-08-29 17:56:04 +0000

    Use spans in place of pointers in fx_font.cpp
    Change-Id: Ib625418c4dc69fbb46e7887cc987c5aadd1493c7
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/96990
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>

--- core/fxge/fx_font.cpp
+++ core/fxge/fx_font.cpp
@@ -129,11 +129,11 @@ ByteString GetNameFromTT(pdfium::span<const uint8_t> name_table,
 }
 
 size_t GetTTCIndex(pdfium::span<const uint8_t> pFontData, size_t font_offset) {
-  const uint8_t* p = pFontData.data() + 8;
-  size_t nfont = FXSYS_UINT32_GET_MSBFIRST(p);
+  pdfium::span<const uint8_t> p = pFontData.subspan(8);
+  size_t nfont = FXSYS_UINT32_GET_MSBFIRST(p.data());
   for (size_t index = 0; index < nfont; index++) {
-    p = pFontData.data() + 12 + index * 4;
-    if (FXSYS_UINT32_GET_MSBFIRST(p) == font_offset)
+    p = pFontData.subspan(12 + index * 4);
+    if (FXSYS_UINT32_GET_MSBFIRST(p.data()) == font_offset)
       return index;
   }
   return 0;
