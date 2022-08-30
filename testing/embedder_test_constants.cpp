ef3036859ddbf398338143eb2c4f729cdeadd4df
Alan Screen <awscreen@chromium.org>
2022-08-30 00:43:35 +0000

    Refactor embeddertest constants for runtime renderer selection
    Update expectation values for both Skia and AGG renderers in a build,
    and choose the appropriate values for comparison based upon a runtime
    check of the active renderer being used.
    
    Bug: pdfium:1878
    Change-Id: Idb22eff408defc4c2dbd440d6e89e059d9739cb5
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/97013
    Reviewed-by: Nigi <nigi@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>

--- testing/embedder_test_constants.cpp
+++ testing/embedder_test_constants.cpp
@@ -5,101 +5,78 @@
 #include "testing/embedder_test_constants.h"
 
 #include "build/build_config.h"
+#include "core/fxge/cfx_defaultrenderdevice.h"
 
 namespace pdfium {
 
 const char* AnnotationStampWithApChecksum() {
-#if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
-  static constexpr char kAnnotationStampWithApChecksum[] =
-      "a31381406d0b95049e418720750b78dd";
-#elif BUILDFLAG(IS_APPLE)
-  static constexpr char kAnnotationStampWithApChecksum[] =
-      "d243b5d64752be0f45b86df7bd2e2708";
+  if (CFX_DefaultRenderDevice::SkiaIsDefaultRenderer() ||
+      CFX_DefaultRenderDevice::SkiaPathsIsDefaultRenderer()) {
+    return "a31381406d0b95049e418720750b78dd";
+  }
+#if BUILDFLAG(IS_APPLE)
+  return "d243b5d64752be0f45b86df7bd2e2708";
 #else
-  static constexpr char kAnnotationStampWithApChecksum[] =
-      "cdde6c161679ab10b07c38c1ef04b7e8";
+  return "cdde6c161679ab10b07c38c1ef04b7e8";
 #endif
-
-  return kAnnotationStampWithApChecksum;
 }
 
 const char kBlankPage612By792Checksum[] = "1940568c9ba33bac5d0b1ee9558c76b3";
 
 const char* Bug890322Checksum() {
-#if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
-  static constexpr char kBug890322Checksum[] =
-      "793689536cf64fe792c2f241888c0cf3";
-#else
-  static constexpr char kBug890322Checksum[] =
-      "6c674642154408e877d88c6c082d67e9";
-#endif
-
-  return kBug890322Checksum;
+  if (CFX_DefaultRenderDevice::SkiaIsDefaultRenderer() ||
+      CFX_DefaultRenderDevice::SkiaPathsIsDefaultRenderer()) {
+    return "793689536cf64fe792c2f241888c0cf3";
+  }
+  return "6c674642154408e877d88c6c082d67e9";
 }
 
 const char* HelloWorldChecksum() {
-#if BUILDFLAG(IS_APPLE) && !defined(_SKIA_SUPPORT_) && \
-    !defined(_SKIA_SUPPORT_PATHS_)
-  static constexpr char kHelloWorldChecksum[] =
-      "6eef7237f7591f07616e238422086737";
-#else
-  static constexpr char kHelloWorldChecksum[] =
-      "c1c548442e0e0f949c5550d89bf8ae3b";
+#if BUILDFLAG(IS_APPLE)
+  if (!CFX_DefaultRenderDevice::SkiaIsDefaultRenderer() &&
+      !CFX_DefaultRenderDevice::SkiaPathsIsDefaultRenderer()) {
+    return "6eef7237f7591f07616e238422086737";
+  }
 #endif
-
-  return kHelloWorldChecksum;
+  return "c1c548442e0e0f949c5550d89bf8ae3b";
 }
 
 const char* HelloWorldRemovedChecksum() {
-#if BUILDFLAG(IS_APPLE) && !defined(_SKIA_SUPPORT_) && \
-    !defined(_SKIA_SUPPORT_PATHS_)
-  static constexpr char kHelloWorldRemovedChecksum[] =
-      "6e1cae48a2e35c521dee4ca502f48af6";
-#else
-  static constexpr char kHelloWorldRemovedChecksum[] =
-      "4a9b80f675f7f3bf2da1b02f12449e4b";
+#if BUILDFLAG(IS_APPLE)
+  if (!CFX_DefaultRenderDevice::SkiaIsDefaultRenderer() &&
+      !CFX_DefaultRenderDevice::SkiaPathsIsDefaultRenderer()) {
+    return "6e1cae48a2e35c521dee4ca502f48af6";
+  }
 #endif
-
-  return kHelloWorldRemovedChecksum;
+  return "4a9b80f675f7f3bf2da1b02f12449e4b";
 }
 
 const char* ManyRectanglesChecksum() {
-#if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
-  static constexpr char kManyRectanglesChecksum[] =
-      "4e7e280c1597222afcb0ee3bb90ec119";
-#else
-  static constexpr char kManyRectanglesChecksum[] =
-      "b0170c575b65ecb93ebafada0ff0f038";
-#endif
-
-  return kManyRectanglesChecksum;
+  if (CFX_DefaultRenderDevice::SkiaIsDefaultRenderer() ||
+      CFX_DefaultRenderDevice::SkiaPathsIsDefaultRenderer()) {
+    return "4e7e280c1597222afcb0ee3bb90ec119";
+  }
+  return "b0170c575b65ecb93ebafada0ff0f038";
 }
 
 const char* RectanglesChecksum() {
-#if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
-  static constexpr char kRectanglesChecksum[] =
-      "b4e411a6b5ffa59a50efede2efece597";
-#else
-  static constexpr char kRectanglesChecksum[] =
-      "0a90de37f52127619c3dfb642b5fa2fe";
-#endif
-
-  return kRectanglesChecksum;
+  if (CFX_DefaultRenderDevice::SkiaIsDefaultRenderer() ||
+      CFX_DefaultRenderDevice::SkiaPathsIsDefaultRenderer()) {
+    return "b4e411a6b5ffa59a50efede2efece597";
+  }
+  return "0a90de37f52127619c3dfb642b5fa2fe";
 }
 
 const char* TextFormChecksum() {
-#if defined(_SKIA_SUPPORT_) || defined(_SKIA_SUPPORT_PATHS_)
-  static constexpr char kTextFormChecksum[] =
-      "e6d2eb75f18d773f0dad938b1bb22e23";
-#elif BUILDFLAG(IS_APPLE)
-  static constexpr char kTextFormChecksum[] =
-      "fa2bf756942a950101fc147fc4ef3f82";
+  if (CFX_DefaultRenderDevice::SkiaIsDefaultRenderer() ||
+      CFX_DefaultRenderDevice::SkiaPathsIsDefaultRenderer()) {
+    return "e6d2eb75f18d773f0dad938b1bb22e23";
+  }
+#if BUILDFLAG(IS_APPLE)
+  return "fa2bf756942a950101fc147fc4ef3f82";
 #else
-  static constexpr char kTextFormChecksum[] =
-      "6f86fe1dbed5965d91aec6e0b829e29f";
+  return "6f86fe1dbed5965d91aec6e0b829e29f";
 #endif
-
-  return kTextFormChecksum;
 }
 
 }  // namespace pdfium
