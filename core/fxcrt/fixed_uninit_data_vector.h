45d7210180b80fb20790bb08a2a65e2aaa96dc90
Lei Zhang <thestig@chromium.org>
2022-09-15 20:59:51 +0000

    Add a FixedUninitDataVector container.
    This is a simple container to hold data of a fixed size. It does not
    initialize the underlying memory, so it does not have the same
    performance issues as std::vector in debug builds. Its public methods
    are very limited and generally requires callers to get to the underlying
    data via spans.
    
    Change-Id: Ic995e37707084f940a87a502b25e317d8a71dd5d
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/98070
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

---
+++ core/fxcrt/fixed_uninit_data_vector.h
@@ -0,0 +1,63 @@
+// Copyright 2022 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CORE_FXCRT_FIXED_UNINIT_DATA_VECTOR_H_
+#define CORE_FXCRT_FIXED_UNINIT_DATA_VECTOR_H_
+
+#include <stddef.h>
+
+#include <memory>
+#include <utility>
+
+#include "core/fxcrt/fx_memory_wrappers.h"
+#include "third_party/base/span.h"
+
+namespace fxcrt {
+
+// A simple data container that has a fixed size.
+// Unlike std::vector, its data starts out uninitialized and it cannot be
+// implicitly copied.
+// Access its data using spans.
+template <typename T>
+class FixedUninitDataVector {
+ public:
+  FixedUninitDataVector() : FixedUninitDataVector(0) {}
+  explicit FixedUninitDataVector(size_t size)
+      : data_(size ? FX_AllocUninit(T, size) : nullptr), size_(size) {}
+  FixedUninitDataVector(const FixedUninitDataVector&) = delete;
+  FixedUninitDataVector& operator=(const FixedUninitDataVector&) = delete;
+  FixedUninitDataVector(FixedUninitDataVector&& that) noexcept {
+    data_ = std::move(that.data_);
+    size_ = that.size_;
+    that.size_ = 0;
+  }
+  FixedUninitDataVector& operator=(FixedUninitDataVector&& that) noexcept {
+    data_ = std::move(that.data_);
+    size_ = that.size_;
+    that.size_ = 0;
+    return *this;
+  }
+  ~FixedUninitDataVector() = default;
+
+  pdfium::span<T> writable_span() {
+    return pdfium::make_span(data_.get(), size_);
+  }
+
+  pdfium::span<const T> span() const {
+    return pdfium::make_span(data_.get(), size_);
+  }
+
+  size_t size() const { return size_; }
+  bool empty() const { return size_ == 0; }
+
+ private:
+  std::unique_ptr<T, FxFreeDeleter> data_;
+  size_t size_;
+};
+
+}  // namespace fxcrt
+
+using fxcrt::FixedUninitDataVector;
+
+#endif  // CORE_FXCRT_FIXED_UNINIT_DATA_VECTOR_H_
---
+++ core/fxcrt/fixed_uninit_data_vector_unittest.cpp
@@ -0,0 +1,61 @@
+// Copyright 2022 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/fxcrt/fixed_uninit_data_vector.h"
+
+#include "core/fxcrt/span_util.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/base/span.h"
+
+TEST(FixedUninitDataVector, NoData) {
+  FixedUninitDataVector<int> vec;
+  EXPECT_EQ(0u, vec.size());
+  EXPECT_TRUE(vec.empty());
+  EXPECT_TRUE(vec.span().empty());
+  EXPECT_TRUE(vec.writable_span().empty());
+}
+
+TEST(FixedUninitDataVector, WithData) {
+  FixedUninitDataVector<int> vec(4);
+  EXPECT_FALSE(vec.empty());
+  EXPECT_EQ(4u, vec.size());
+  EXPECT_EQ(4u, vec.span().size());
+  EXPECT_EQ(4u, vec.writable_span().size());
+
+  constexpr int kData[] = {1, 2, 3, 4};
+  fxcrt::spancpy(vec.writable_span(), pdfium::make_span(kData));
+  EXPECT_THAT(vec.span(), testing::ElementsAre(1, 2, 3, 4));
+}
+
+TEST(FixedUninitDataVector, Move) {
+  FixedUninitDataVector<int> vec(4);
+  constexpr int kData[] = {1, 2, 3, 4};
+  ASSERT_EQ(4u, vec.writable_span().size());
+  fxcrt::spancpy(vec.writable_span(), pdfium::make_span(kData));
+
+  FixedUninitDataVector<int> vec2(std::move(vec));
+  EXPECT_FALSE(vec2.empty());
+  EXPECT_EQ(4u, vec2.size());
+  EXPECT_EQ(4u, vec2.span().size());
+  EXPECT_EQ(4u, vec2.writable_span().size());
+  EXPECT_THAT(vec2.span(), testing::ElementsAre(1, 2, 3, 4));
+
+  EXPECT_EQ(0u, vec.size());
+  EXPECT_TRUE(vec.empty());
+  EXPECT_TRUE(vec.span().empty());
+  EXPECT_TRUE(vec.writable_span().empty());
+
+  vec = std::move(vec2);
+  EXPECT_FALSE(vec.empty());
+  EXPECT_EQ(4u, vec.size());
+  EXPECT_EQ(4u, vec.span().size());
+  EXPECT_EQ(4u, vec.writable_span().size());
+  EXPECT_THAT(vec.span(), testing::ElementsAre(1, 2, 3, 4));
+
+  EXPECT_EQ(0u, vec2.size());
+  EXPECT_TRUE(vec2.empty());
+  EXPECT_TRUE(vec2.span().empty());
+  EXPECT_TRUE(vec2.writable_span().empty());
+}
