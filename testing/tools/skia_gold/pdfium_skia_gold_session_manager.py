03571c10b7b90519a1ea3ed833615a72cf0e1c2e
Lei Zhang <thestig@chromium.org>
2022-10-28 21:18:00 +0000

    Delete shebangs from some skia_gold scripts.
    They are not needed since the scripts are only imported as modules and
    never executed directly.
    
    Change-Id: I378e13c6d517ea6353dc0c4bbf71cda85803c6ab
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/100271
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

--- testing/tools/skia_gold/pdfium_skia_gold_session_manager.py
+++ testing/tools/skia_gold/pdfium_skia_gold_session_manager.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Copyright 2021 The PDFium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
