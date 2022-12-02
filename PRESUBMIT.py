ef89e738e4cfdb894229568105e370aba51d51e5
Lei Zhang <thestig@chromium.org>
2022-12-02 19:15:00 +0000

    Update PRESUBMIT.py to use PY3 and presubmit format v2.
    See https://pdfium-review.googlesource.com/82132 and
    https://pdfium-review.googlesource.com/100370 for the same changes to
    PRESUBMIT.py on the main branch.
    
    Bug: pdfium:1674
    Change-Id: If2fbc24a22dcd8fe9020a9f6e11c08f2c53722a4
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/102033
    Reviewed-by: K. Moon <kmoon@chromium.org>

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -2,6 +2,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+PRESUBMIT_VERSION = '2.0.0'
+
+USE_PYTHON3 = True
+
 
 def CheckChangeOnUpload(input_api, output_api):
   return input_api.canned_checks.CheckChangedLUCIConfigs(input_api, output_api)
