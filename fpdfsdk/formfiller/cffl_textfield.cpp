a0d16d18d072ce77e639a09ed211340a2ad9034e
Tom Sepez <tsepez@chromium.org>
2023-03-14 21:18:45 +0000

    M110: Observe CPWL_* object destruction across CPDFSDK_Widget methods
    This is a simple fix to stop the symptoms while we investigate
    how to avoid mutations at these points in the first place.
    
    -- fix some nearby braces and annoying blank lines while at it.
    
    Bug: chromium:1419831
    Change-Id: I20c38806b91c7c0c9016bb1b567a04ce319243d8
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/104397
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    (cherry picked from commit 63e3719f1ec20ee6db804b2b2d4b00680db18d9c)
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/104833
    Auto-Submit: Tom Sepez <tsepez@chromium.org>

--- fpdfsdk/formfiller/cffl_textfield.cpp
+++ fpdfsdk/formfiller/cffl_textfield.cpp
@@ -142,26 +142,29 @@ bool CFFL_TextField::IsDataChanged(const CPDFSDK_PageView* pPageView) {
 }
 
 void CFFL_TextField::SaveData(const CPDFSDK_PageView* pPageView) {
-  CPWL_Edit* pWnd = GetPWLEdit(pPageView);
-  if (!pWnd)
+  ObservedPtr<CPWL_Edit> observed_edit(GetPWLEdit(pPageView));
+  if (!observed_edit) {
     return;
-
+  }
   WideString sOldValue = m_pWidget->GetValue();
-  WideString sNewValue = pWnd->GetText();
+  if (!observed_edit) {
+    return;
+  }
+  WideString sNewValue = observed_edit->GetText();
   ObservedPtr<CPDFSDK_Widget> observed_widget(m_pWidget);
   ObservedPtr<CFFL_TextField> observed_this(this);
   m_pWidget->SetValue(sNewValue);
-  if (!observed_widget)
+  if (!observed_widget) {
     return;
-
+  }
   m_pWidget->ResetFieldAppearance();
-  if (!observed_widget)
+  if (!observed_widget) {
     return;
-
+  }
   m_pWidget->UpdateField();
-  if (!observed_widget || !observed_this)
+  if (!observed_widget || !observed_this) {
     return;
-
+  }
   SetChangeMark();
 }
 
@@ -172,9 +175,7 @@ void CFFL_TextField::GetActionData(const CPDFSDK_PageView* pPageView,
     case CPDF_AAction::kKeyStroke:
       if (CPWL_Edit* pWnd = GetPWLEdit(pPageView)) {
         fa.bFieldFull = pWnd->IsTextFull();
-
         fa.sValue = pWnd->GetText();
-
         if (fa.bFieldFull) {
           fa.sChange.clear();
           fa.sChangeEx.clear();
