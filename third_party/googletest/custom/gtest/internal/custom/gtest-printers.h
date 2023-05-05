7ef2ee35613b365df6877c6b072f5b05c6a2daf0
Lei Zhang <thestig@chromium.org>
2023-05-05 23:02:13 +0000

    Teach googletest about C-string to ByteString comparisons
    Add custom gtest printer code to treat the C-string in a C-string to
    ByteString comparison as a string instead of a pointer.
    
    Change-Id: I9b496da300dd134c4e3a8ee626939c70815a6c8f
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/107135
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

---
+++ third_party/googletest/custom/gtest/internal/custom/gtest-printers.h
@@ -0,0 +1,33 @@
+// Copyright 2023 The PDFium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_GOOGLETEST_CUSTOM_GTEST_INTERNAL_CUSTOM_GTEST_PRINTERS_H_
+#define THIRD_PARTY_GOOGLETEST_CUSTOM_GTEST_INTERNAL_CUSTOM_GTEST_PRINTERS_H_
+
+#include <string>
+
+#include "core/fxcrt/bytestring.h"
+
+namespace testing {
+
+// If a C string is compared with a PDFium string object, then it is meant to
+// point to a NUL-terminated string, and thus print it as a string.
+
+#define GTEST_IMPL_FORMAT_C_STRING_AS_STRING_(CharType, OtherStringType) \
+  template <>                                                            \
+  class internal::FormatForComparison<CharType*, OtherStringType> {      \
+   public:                                                               \
+    static std::string Format(CharType* value) {                         \
+      return ::testing::PrintToString(value);                            \
+    }                                                                    \
+  }
+
+GTEST_IMPL_FORMAT_C_STRING_AS_STRING_(char, fxcrt::ByteString);
+GTEST_IMPL_FORMAT_C_STRING_AS_STRING_(const char, fxcrt::ByteString);
+
+#undef GTEST_IMPL_FORMAT_C_STRING_AS_STRING_
+
+}  // namespace testing
+
+#endif  // THIRD_PARTY_GOOGLETEST_CUSTOM_GTEST_INTERNAL_CUSTOM_GTEST_PRINTERS_H_
