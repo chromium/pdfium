57d1f79d444beeafdaad4bf7604a8ae40d17bf78
David Benjamin <davidben@google.com>
2023-11-10 22:02:39 +0000

    Avoid the NULL, 0 undefined behavior problem in CFX_GlyphCache
    A couple Chromium tests run into it here. Use the FXSYS wrapper
    functions, which avoid this.
    
    https://luci-milo.appspot.com/ui/inv/build-8764924042485453521/test-results?q=All%2FPDFiumPrintTest.Basic%2F0&sortby=&groupby=
    https://luci-milo.appspot.com/ui/inv/build-8764924042485453521/test-results?q=PrintRenderFrameHelperTest.PrintWithIframe&sortby=&groupby=
    https://luci-milo.appspot.com/ui/inv/build-8764924042485453521/test-results?q=PrintRenderFrameHelperTest.RoundingAndHeadersAndFooters&sortby=&groupby=
    
    Bug: chromium:1394755
    Change-Id: I439c3a1b9911e79f8cef01d7cc3f5d79a67910ab
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/113470
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: David Benjamin <davidben@google.com>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- core/fxge/cfx_glyphcache.cpp
+++ core/fxge/cfx_glyphcache.cpp
@@ -15,6 +15,7 @@
 
 #include "build/build_config.h"
 #include "core/fxcrt/fx_codepage.h"
+#include "core/fxcrt/fx_memcpy_wrappers.h"
 #include "core/fxge/cfx_defaultrenderdevice.h"
 #include "core/fxge/cfx_font.h"
 #include "core/fxge/cfx_fontmgr.h"
@@ -220,10 +221,12 @@ std::unique_ptr<CFX_GlyphBitmap> CFX_GlyphCache::RenderGlyph(
       }
     }
   } else {
-    memset(pDestBuf, 0, dest_pitch * bmheight);
+    FXSYS_memset(pDestBuf, 0, dest_pitch * bmheight);
     int rowbytes = std::min(abs(src_pitch), dest_pitch);
-    for (int row = 0; row < bmheight; row++)
-      memcpy(pDestBuf + row * dest_pitch, pSrcBuf + row * src_pitch, rowbytes);
+    for (int row = 0; row < bmheight; row++) {
+      FXSYS_memcpy(pDestBuf + row * dest_pitch, pSrcBuf + row * src_pitch,
+                   rowbytes);
+    }
   }
   return pGlyphBitmap;
 }
