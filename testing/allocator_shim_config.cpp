471679d54e8ca05deaa63df0224fe768204f6a4f
Bartek Nowierski <bartekn@chromium.org>
2023-12-21 05:05:19 +0000

    Use the dedicated ConfigurePartitionsForTesting()
    This will counterplay the interface changes of ConfigurePartitions() in
    the future
    
    Change-Id: I4739c33715ef3446cd57589f58c65986faa2c8b5
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/114890
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>

--- testing/allocator_shim_config.cpp
+++ testing/allocator_shim_config.cpp
@@ -6,7 +6,7 @@
 
 #include "partition_alloc/dangling_raw_ptr_checks.h"
 #include "partition_alloc/partition_alloc_buildflags.h"
-#include "partition_alloc/shim/allocator_shim.h"
+#include "partition_alloc/shim/allocator_shim_default_dispatch_to_partition_alloc.h"
 #include "third_party/base/check.h"
 
 namespace pdfium {
@@ -17,12 +17,7 @@ void ConfigurePartitionAllocShimPartitionForTest() {
 #if BUILDFLAG(ENABLE_DANGLING_RAW_PTR_CHECKS)
   partition_alloc::SetDanglingRawPtrDetectedFn([](uintptr_t) { CHECK(0); });
 #endif  // BUILDFLAG(ENABLE_DANGLING_RAW_PTR_CHECKS)
-  allocator_shim::ConfigurePartitions(
-      allocator_shim::EnableBrp(true),
-      allocator_shim::EnableMemoryTagging(false),
-      allocator_shim::SplitMainPartition(true),
-      allocator_shim::UseDedicatedAlignedPartition(true), 0,
-      allocator_shim::BucketDistribution::kNeutral);
+  allocator_shim::ConfigurePartitionsForTesting();
 #endif  // BUILDFLAG(ENABLE_BACKUP_REF_PTR_SUPPORT)
 #endif  // BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 }
