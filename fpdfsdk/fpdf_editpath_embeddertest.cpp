28483abbe97766cfa14cd7f259b98c6d8ab26f36
Lei Zhang <thestig@chromium.org>
2024-03-01 01:08:31 +0000

    Add path objects tests for a PDF saving bug
    Add FPDFEditPathEmbedderTest cases to demonstrate a PDF saving bug,
    where a no-op FPDFPageObj_SetMatrix() call causes the saved PDF to have
    incorrect transformation matrices. These test cases cover path objects
    and path objects in forms.
    
    Bug: pdfium:2132
    Change-Id: I81f13fa6a471c4f7184e43f105ff0cc151bfccff
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/117052
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Thomas Sepez <tsepez@google.com>

--- fpdfsdk/fpdf_editpath_embeddertest.cpp
+++ fpdfsdk/fpdf_editpath_embeddertest.cpp
@@ -4,9 +4,13 @@
 
 #include "core/fxcrt/check.h"
 #include "core/fxcrt/fx_system.h"
+#include "core/fxge/cfx_defaultrenderdevice.h"
 #include "public/fpdf_edit.h"
 #include "testing/embedder_test.h"
+#include "testing/embedder_test_constants.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+using pdfium::RectanglesChecksum;
 
 class FPDFEditPathEmbedderTest : public EmbedderTest {};
 
@@ -61,4 +65,133 @@ TEST_F(FPDFEditPathEmbedderTest, VerifyCorrectColoursReturned) {
 
   CloseSavedPage(page);
   CloseSavedDocument();
+}
+
+TEST_F(FPDFEditPathEmbedderTest, GetAndSetMatrixForPath) {
+  constexpr int kExpectedWidth = 200;
+  constexpr int kExpectedHeight = 300;
+
+  OpenDocument("rectangles_double_flipped.pdf");
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  {
+    ScopedFPDFBitmap bitmap = RenderLoadedPage(page);
+    CompareBitmap(bitmap.get(), kExpectedWidth, kExpectedHeight,
+                  RectanglesChecksum());
+  }
+
+  FPDF_PAGEOBJECT path = FPDFPage_GetObject(page, 0);
+  ASSERT_TRUE(path);
+  ASSERT_EQ(FPDF_PAGEOBJ_PATH, FPDFPageObj_GetType(path));
+
+  FS_MATRIX matrix;
+  ASSERT_TRUE(FPDFPageObj_GetMatrix(path, &matrix));
+  EXPECT_FLOAT_EQ(1.0f, matrix.a);
+  EXPECT_FLOAT_EQ(0.0f, matrix.b);
+  EXPECT_FLOAT_EQ(0.0f, matrix.c);
+  EXPECT_FLOAT_EQ(-1.0f, matrix.d);
+  EXPECT_FLOAT_EQ(0.0f, matrix.e);
+  EXPECT_FLOAT_EQ(300.0f, matrix.f);
+
+  ASSERT_TRUE(FPDFPageObj_SetMatrix(path, &matrix));
+  {
+    ScopedFPDFBitmap bitmap = RenderLoadedPage(page);
+    CompareBitmap(bitmap.get(), kExpectedWidth, kExpectedHeight,
+                  RectanglesChecksum());
+  }
+
+  ASSERT_TRUE(FPDFPage_GenerateContent(page));
+  ASSERT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
+
+  {
+    ScopedFPDFBitmap bitmap = RenderLoadedPage(page);
+    CompareBitmap(bitmap.get(), kExpectedWidth, kExpectedHeight,
+                  RectanglesChecksum());
+  }
+
+  UnloadPage(page);
+
+  // TODO(crbug.com/pdfium/2132): This should use RectanglesChecksum().
+  const char* const kWrongChecksum = []() {
+    if (CFX_DefaultRenderDevice::UseSkiaRenderer()) {
+      return "a8b00bc40677a73c15a08b9769d1b576";
+    }
+    return "8f3a555ef9c0d5031831ae3715273707";
+  }();
+  VerifySavedDocument(kExpectedWidth, kExpectedHeight, kWrongChecksum);
+}
+
+TEST_F(FPDFEditPathEmbedderTest, GetAndSetMatrixForFormWithPath) {
+  constexpr int kExpectedWidth = 200;
+  constexpr int kExpectedHeight = 300;
+
+  OpenDocument("form_object_with_path.pdf");
+  FPDF_PAGE page = LoadPage(0);
+  ASSERT_TRUE(page);
+
+  {
+    ScopedFPDFBitmap bitmap = RenderLoadedPage(page);
+    CompareBitmap(bitmap.get(), kExpectedWidth, kExpectedHeight,
+                  RectanglesChecksum());
+  }
+
+  FPDF_PAGEOBJECT form = FPDFPage_GetObject(page, 0);
+  ASSERT_TRUE(form);
+  ASSERT_EQ(FPDF_PAGEOBJ_FORM, FPDFPageObj_GetType(form));
+
+  FS_MATRIX matrix;
+  ASSERT_TRUE(FPDFPageObj_GetMatrix(form, &matrix));
+  EXPECT_FLOAT_EQ(2.0f, matrix.a);
+  EXPECT_FLOAT_EQ(0.0f, matrix.b);
+  EXPECT_FLOAT_EQ(0.0f, matrix.c);
+  EXPECT_FLOAT_EQ(-1.0f, matrix.d);
+  EXPECT_FLOAT_EQ(0.0f, matrix.e);
+  EXPECT_FLOAT_EQ(300.0f, matrix.f);
+
+  ASSERT_TRUE(FPDFPageObj_SetMatrix(form, &matrix));
+  {
+    ScopedFPDFBitmap bitmap = RenderLoadedPage(page);
+    CompareBitmap(bitmap.get(), kExpectedWidth, kExpectedHeight,
+                  RectanglesChecksum());
+  }
+
+  FPDF_PAGEOBJECT path = FPDFFormObj_GetObject(form, 0);
+  ASSERT_TRUE(path);
+  ASSERT_EQ(FPDF_PAGEOBJ_PATH, FPDFPageObj_GetType(path));
+
+  ASSERT_TRUE(FPDFPageObj_GetMatrix(path, &matrix));
+  EXPECT_FLOAT_EQ(0.5f, matrix.a);
+  EXPECT_FLOAT_EQ(0.0f, matrix.b);
+  EXPECT_FLOAT_EQ(0.0f, matrix.c);
+  EXPECT_FLOAT_EQ(-1.0f, matrix.d);
+  EXPECT_FLOAT_EQ(0.0f, matrix.e);
+  EXPECT_FLOAT_EQ(300.0f, matrix.f);
+
+  ASSERT_TRUE(FPDFPageObj_SetMatrix(path, &matrix));
+  {
+    ScopedFPDFBitmap bitmap = RenderLoadedPage(page);
+    CompareBitmap(bitmap.get(), kExpectedWidth, kExpectedHeight,
+                  RectanglesChecksum());
+  }
+
+  ASSERT_TRUE(FPDFPage_GenerateContent(page));
+  ASSERT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
+
+  {
+    ScopedFPDFBitmap bitmap = RenderLoadedPage(page);
+    CompareBitmap(bitmap.get(), kExpectedWidth, kExpectedHeight,
+                  RectanglesChecksum());
+  }
+
+  UnloadPage(page);
+
+  // TODO(crbug.com/pdfium/2132): This should use RectanglesChecksum().
+  const char* const kWrongChecksum = []() {
+    if (CFX_DefaultRenderDevice::UseSkiaRenderer()) {
+      return "f7c69f8d0bb92541d48dbbbba003396d";
+    }
+    return "37a215071985bd88079fedb56b001685";
+  }();
+  VerifySavedDocument(kExpectedWidth, kExpectedHeight, kWrongChecksum);
 }
