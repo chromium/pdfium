93eaf885a8dbf1da20491eab25d5acf99aa1bcbb
Tom Sepez <tsepez@chromium.org>
2024-05-31 01:30:16 +0000

    Remove file-wide unsafe-buffers suppression from cxfa_fmlexer.cpp,
    Apart from the debug build, the file was already compliant. Convert
    to std::array<> for that case.
    
    -- fix some constant naming while at it.
    
    Change-Id: If75c039684bd7e79f7a638cad049c5c776aa8312
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/119830
    Reviewed-by: Thomas Sepez <tsepez@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- xfa/fxfa/formcalc/cxfa_fmlexer.cpp
+++ xfa/fxfa/formcalc/cxfa_fmlexer.cpp
@@ -4,16 +4,12 @@
 
 // Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
 
-#if defined(UNSAFE_BUFFERS_BUILD)
-// TODO(crbug.com/pdfium/2154): resolve buffer safety issues.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "xfa/fxfa/formcalc/cxfa_fmlexer.h"
 
 #include <algorithm>
 
 #include "core/fxcrt/fx_extension.h"
+#include "core/fxcrt/stl_util.h"
 
 namespace {
 
@@ -45,7 +41,7 @@ struct XFA_FMKeyword {
   const char* m_keyword;  // Raw, POD struct.
 };
 
-const XFA_FMKeyword keyWords[] = {
+const XFA_FMKeyword kKeyWords[] = {
     {TOKdo, "do"},
     {TOKkseq, "eq"},
     {TOKksge, "ge"},
@@ -86,7 +82,7 @@ const XFA_FMKeyword keyWords[] = {
 };
 
 #ifndef NDEBUG
-const char* const tokenStrings[] = {
+constexpr auto kTokenStrings = fxcrt::ToArray<const char*>({
     "TOKand",        "TOKlparen",     "TOKrparen",   "TOKmul",
     "TOKplus",       "TOKcomma",      "TOKminus",    "TOKdot",
     "TOKdiv",        "TOKlt",         "TOKassign",   "TOKgt",
@@ -104,17 +100,18 @@ const char* const tokenStrings[] = {
     "TOKcontinue",   "TOKfunc",       "TOKendif",    "TOKstar",
     "TOKidentifier", "TOKunderscore", "TOKdollar",   "TOKexclamation",
     "TOKcall",       "TOKstring",     "TOKnumber",   "TOKreserver",
-};
+});
 #endif  // NDEBUG
 
 XFA_FM_TOKEN TokenizeIdentifier(WideStringView str) {
   const XFA_FMKeyword* result =
-      std::find_if(std::begin(keyWords), std::end(keyWords),
+      std::find_if(std::begin(kKeyWords), std::end(kKeyWords),
                    [str](const XFA_FMKeyword& iter) {
                      return str.EqualsASCII(iter.m_keyword);
                    });
-  if (result != std::end(keyWords) && str.EqualsASCII(result->m_keyword))
+  if (result != std::end(kKeyWords) && str.EqualsASCII(result->m_keyword)) {
     return result->m_type;
+  }
   return TOKidentifier;
 }
 
@@ -134,7 +131,7 @@ CXFA_FMLexer::Token::~Token() = default;
 #ifndef NDEBUG
 WideString CXFA_FMLexer::Token::ToDebugString() const {
   WideString str = WideString::FromASCII("type = ");
-  str += WideString::FromASCII(tokenStrings[m_type]);
+  str += WideString::FromASCII(kTokenStrings[m_type]);
   str += WideString::FromASCII(", string = ");
   str += m_string;
   return str;
