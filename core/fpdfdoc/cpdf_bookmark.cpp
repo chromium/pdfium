c907c78ce8981827513b8ed314d78d31fd310074
Tom Sepez <tsepez@chromium.org>
2024-06-12 02:41:44 +0000

    Avoid UNSAFE_TODO() in CPDF_Bookmark.
    Construct a string as a string rather than an initial vector.
    
    Bug: 42271176
    Change-Id: I9136cf25533a7c2d941de42e9f0885bcce5c0cb0
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/120311
    Reviewed-by: Thomas Sepez <tsepez@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>

--- core/fpdfdoc/cpdf_bookmark.cpp
+++ core/fpdfdoc/cpdf_bookmark.cpp
@@ -6,12 +6,12 @@
 
 #include "core/fpdfdoc/cpdf_bookmark.h"
 
+#include <algorithm>
 #include <utility>
 
 #include "core/fpdfapi/parser/cpdf_array.h"
 #include "core/fpdfapi/parser/cpdf_dictionary.h"
 #include "core/fpdfapi/parser/cpdf_string.h"
-#include "core/fxcrt/data_vector.h"
 #include "core/fxge/dib/fx_dib.h"
 
 CPDF_Bookmark::CPDF_Bookmark() = default;
@@ -24,25 +24,21 @@ CPDF_Bookmark::CPDF_Bookmark(RetainPtr<const CPDF_Dictionary> pDict)
 CPDF_Bookmark::~CPDF_Bookmark() = default;
 
 WideString CPDF_Bookmark::GetTitle() const {
-  if (!m_pDict)
+  if (!m_pDict) {
     return WideString();
-
+  }
   RetainPtr<const CPDF_String> pString =
       ToString(m_pDict->GetDirectObjectFor("Title"));
-  if (!pString)
+  if (!pString) {
     return WideString();
-
-  WideString title = pString->GetUnicodeText();
-  size_t len = title.GetLength();
-  if (!len)
-    return WideString();
-
-  DataVector<wchar_t> buf(len);
-  for (size_t i = 0; i < len; i++) {
-    wchar_t w = title[i];
-    buf[i] = w > 0x20 ? w : 0x20;
   }
-  return UNSAFE_TODO(WideString::Create(buf.data(), len));
+  WideString title = pString->GetUnicodeText();
+  WideString result;
+  result.Reserve(title.GetLength());
+  for (const wchar_t wc : title) {
+    result += std::max(wc, static_cast<wchar_t>(0x20));
+  }
+  return result;
 }
 
 CPDF_Dest CPDF_Bookmark::GetDest(CPDF_Document* pDocument) const {
