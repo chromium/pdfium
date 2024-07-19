aa3c865d86d90c6510bc5477e7b45dbe943825ee
Lei Zhang <thestig@chromium.org>
2024-07-19 19:42:20 +0000

    Use FPDF_StructElement_GetMarkedContentIdAtIndex() in pdfium_test
    Change the --show-structure code to show all the MCIDs for a struct
    element by using FPDF_StructElement_GetMarkedContentIdAtIndex() instead
    of FPDF_StructElement_GetMarkedContentID().
    
    Change-Id: I681939747e95666b56193a1e93c6dcba855d045c
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/122030
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@google.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>

--- testing/helpers/dump.cc
+++ testing/helpers/dump.cc
@@ -178,9 +178,12 @@ void DumpChildStructure(FPDF_STRUCTELEMENT child, int indent) {
            ConvertToWString(buf, len).c_str());
   }
 
-  int mcid = FPDF_StructElement_GetMarkedContentID(child);
-  if (mcid != -1) {
-    printf("%*s MCID: %d\n", indent * 2, "", mcid);
+  const int mcid_count = FPDF_StructElement_GetMarkedContentIdCount(child);
+  for (int i = 0; i < mcid_count; ++i) {
+    int mcid = FPDF_StructElement_GetMarkedContentIdAtIndex(child, i);
+    if (mcid != -1) {
+      printf("%*s MCID%d: %d\n", indent * 2, "", i, mcid);
+    }
   }
 
   FPDF_STRUCTELEMENT parent = FPDF_StructElement_GetParent(child);
