06c6fda8aea66809ef95e4efb98e1220caac71d2
Lei Zhang <thestig@chromium.org>
2024-08-02 00:27:05 +0000

    Consolidate values in core/fxge/render_defines.h
    Since various values have been deleted from render_defines.h, there are
    some gaps in between the remaining values. Compact the values.
    
    Change-Id: I999b0c7414b76faffa8a812ca6f53a07466f4f08
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/122932
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@google.com>

--- core/fxge/render_defines.h
+++ core/fxge/render_defines.h
@@ -7,25 +7,25 @@
 #ifndef CORE_FXGE_RENDER_DEFINES_H_
 #define CORE_FXGE_RENDER_DEFINES_H_
 
-#define FXDC_PIXEL_WIDTH 2
-#define FXDC_PIXEL_HEIGHT 3
-#define FXDC_BITS_PIXEL 4
-#define FXDC_HORZ_SIZE 5
-#define FXDC_VERT_SIZE 6
-#define FXDC_RENDER_CAPS 7
+#define FXDC_PIXEL_WIDTH 1
+#define FXDC_PIXEL_HEIGHT 2
+#define FXDC_BITS_PIXEL 3
+#define FXDC_HORZ_SIZE 4
+#define FXDC_VERT_SIZE 5
+#define FXDC_RENDER_CAPS 6
 
 #define FXRC_GET_BITS 0x01
-#define FXRC_ALPHA_PATH 0x10
-#define FXRC_ALPHA_IMAGE 0x20
-#define FXRC_ALPHA_OUTPUT 0x40
-#define FXRC_BLEND_MODE 0x80
-#define FXRC_SOFT_CLIP 0x100
-#define FXRC_BYTEMASK_OUTPUT 0x800
+#define FXRC_ALPHA_PATH 0x02
+#define FXRC_ALPHA_IMAGE 0x04
+#define FXRC_ALPHA_OUTPUT 0x08
+#define FXRC_BLEND_MODE 0x10
+#define FXRC_SOFT_CLIP 0x20
+#define FXRC_BYTEMASK_OUTPUT 0x40
 // Assuming these are Skia-only for now. If this assumption changes, update both
 // the #if logic here, as well as the callsites that check these capabilities.
 #if defined(PDF_USE_SKIA)
-#define FXRC_FILLSTROKE_PATH 0x2000
-#define FXRC_SHADING 0x4000
+#define FXRC_FILLSTROKE_PATH 0x80
+#define FXRC_SHADING 0x100
 #endif
 
 #endif  // CORE_FXGE_RENDER_DEFINES_H_
