f972b61d8c6a2b4515236f837c1fe4ece239743c
Andy Phan <andyphan@chromium.org>
2024-12-11 17:22:11 -0800

    [AGG] Support negative dash phases
    Support negative dash phases by following the ISO 32000-2:2020 section
    8.4.3.6 specification.
    
    Note that the resulting PDF for the pixel test dashed_lines.in is still
    incorrect, since it contains dashed lines for odd sized dash arrays,
    which still needs to be fixed.
    
    Bug: 40491355, 370546365
    Change-Id: I957f222f2ef09d49fc8b2728f3ba98eedbab77b8
    Reviewed-on: https://pdfium-review.googlesource.com/c/pdfium/+/127230
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>

--- third_party/agg23/agg_vcgen_dash.cpp
+++ third_party/agg23/agg_vcgen_dash.cpp
@@ -58,8 +58,17 @@ void vcgen_dash::add_dash(float dash_len, float gap_len)
 }
 void vcgen_dash::dash_start(float ds)
 {
-    m_dash_start = ds;
-    calc_dash_start(fabs(ds));
+  CHECK_GT(m_total_dash_len, 0);
+  // According to ISO 32000-2:2020 section 8.4.3.6:
+  // If the dash phase is negative, it shall be incremented by twice the sum of
+  // all lengths in the dash array until it is positive.
+  if (ds < 0.0f) {
+    float dash_len_sum = m_total_dash_len * 2;
+    ds += ceil(-ds / dash_len_sum) * dash_len_sum;
+  }
+  CHECK_GE(ds, 0);
+  m_dash_start = ds;
+  calc_dash_start(ds);
 }
 void vcgen_dash::calc_dash_start(float ds)
 {
